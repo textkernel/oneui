222961eb0989f93ae7d35e03ffb5e63da188a4c7
Alex Antipin <antipin@textkernel.nl>
2018-09-25 16:04:02 +0200

    Change identation for webpack config

--- etc/webpack/webpack.config.prod.js
+++
@@ -1,9 +0,0 @@
-const { baseConfig, plugins } =  require('./webpack.config')
-
-module.exports = {
-  ...baseConfig,
-  mode: 'production',
-  plugins: [
-    plugins.hashedModuleIdsPlugin,
-  ],
-}
