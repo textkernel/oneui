5abb351967d50756d8ace449d6fc26bd5952bd54
Alex Antipin <antipin@textkernel.nl>
2018-09-25 19:30:36 +0200

    Add fake storybook playground

---
+++ src/storybook/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Dummy from '../components/Dummy'
+
+render(
+      <div>
+          <h2>Ugly Storybook :/</h2>
+          <div style={{ padding: '10px' }}>
+            <Dummy>
+                I'm boring default Dummy thing...
+            </Dummy>
+          </div>
+          <div style={{ padding: '10px' }}>
+            <Dummy isActive>
+                And I'm so active!
+            </Dummy>
+          </div>
+      </div>,
+    document.getElementById('storybook')
+)
