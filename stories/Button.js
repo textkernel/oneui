9917857d952bbe9045a35965929fb429e51ea855
Eszter Hofmann <hofmann#textkernel.nl>
2018-12-11 17:33:08 +0100

    make knobs in storybook

--- stories/Button.js
+++ stories/Button.js
@@ -4,18 +4,14 @@ import { boolean, number, text, select, withKnobs } from '@storybook/addon-knobs
 import Button from '../src/components/Button';
 import { CONTEXTS, SIZES } from '../src/constants';
 
-const context = select('Context', CONTEXTS, CONTEXTS[1], 'group-id-context');
-const size = select('Size', SIZES, SIZES[1], 'group-id-size');
-const type = select('Type', ['submit', 'button'], 'submit', 'group-id-type');
-
 storiesOf('Button', module)
     .addDecorator(withKnobs)
     .add('button', () => (
         <Button
-            context={context}
-            size={size}
+            context={select('Context', CONTEXTS, CONTEXTS[1])}
+            size={select('Size', SIZES, SIZES[1])}
             block={boolean('Block', false)}
-            type={type}
+            type={select('Type', ['submit', 'button'], 'submit')}
             disabled={boolean('Disabled', false)}
         >
             Click me!
