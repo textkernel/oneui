a3c278ab377acb5e738864e2212c6d9e978f802a
Carlo Bernardini <bernardini@textkernel.nl>
2018-12-12 15:41:52 +0100

    Initial commit ButtonGroup

---
+++ src/components/ButtonGroup/ButtonGroup.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import bem from '../../packages/bem';
+import Button from '../Button';
+import styles from './ButtonGroup.scss';
+import { SIZES } from '../../constants';
+
+const ButtonGroup = ({ children, size, style, block, elem }) => (
+    <div
+        {...block()}
+        style={style}
+    >
+        { React.Children.map(children, button => (
+            React.cloneElement(button, {
+                ...button.props,
+                size,
+                ...elem('Button')
+            })
+        )) }
+    </div>
+);
+
+ButtonGroup.propTypes = {
+    /** The buttons in this group */
+    children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+    /** The size of the button */
+    size: PropTypes.oneOf(SIZES),
+    /** whether or not to show block - level button(full width) */
+    isBlock: PropTypes.bool,
+    /** inline styles */
+    style: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+    /** additional class names */
+    className: PropTypes.string
+};
+
+ButtonGroup.defaultProps = {
+    size: 'normal',
+    isBlock: false,
+    style: null,
+    className: ''
+};
+
+ButtonGroup.propsToMods = ['size', 'isBlock'];
+
+export default bem(styles)(ButtonGroup);
---
+++ stories/ButtonGroup.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+import { boolean, select, text, withKnobs } from '@storybook/addon-knobs'; // eslint-disable-line import/no-extraneous-dependencies
+import ButtonGroup from '../src/components/ButtonGroup';
+import Button from '../src/components/Button';
+import { SIZES } from '../src/constants';
+
+storiesOf('ButtonGroup', module)
+    .addDecorator(withKnobs)
+    .add('ButtonGroup', () => (
+        <ButtonGroup
+            isBlock={boolean('isBlock', false)}
+            size={select('Size', SIZES, SIZES[1])}
+        >
+            <Button
+                context="neutral"
+            >
+                Some
+            </Button>
+            <Button
+                context="neutral"
+            >
+                Button
+            </Button>
+            <Button
+                context="neutral"
+            >
+                Group
+            </Button>
+        </ButtonGroup>
+    ));
