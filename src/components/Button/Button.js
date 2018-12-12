2d7a6008b91ddf487d555ea70357750b313c4ed3
Eszter Hofmann <hofmann#textkernel.nl>
2018-12-12 17:30:38 +0100

    refactore button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,12 +3,22 @@ import PropTypes from 'prop-types';
 import bem from '../../packages/bem';
 import styles from './Button.scss';
 import { CONTEXTS, SIZES } from '../../constants';
+debugger;
+const { block } = bem({
+    name: 'Button',
+    classnames: styles,
+    propsToMods: ['context', 'size', 'isBlock']
+});
 
-const Button = ({ children, disabled, elem, isBlock, onClick, type, block, ...rest }) => (
-    <button {...block()} type={type} onClick={onClick} disabled={disabled} {...rest}>
-        {children}
-    </button>
-);
+const Button = props => {
+    const { children, disabled, isBlock, onClick, type, ...rest } = props;
+
+    return (
+        <button {...block(props)} type={type} onClick={onClick} disabled={disabled} {...rest}>
+            {children}
+        </button>
+    );
+}
 
 Button.propTypes = {
     /** The label of the button */
@@ -36,6 +46,4 @@ Button.defaultProps = {
     onClick: null
 };
 
-Button.propsToMods = ['context', 'size', 'isBlock'];
-
-export default bem(styles)(Button);
+export default Button;
