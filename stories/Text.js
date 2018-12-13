1d92ab3365eae15dab67e0ee117d8e67d06f38d6
Carlo Bernardini <bernardini@textkernel.nl>
2018-12-13 11:40:54 +0100

    Fixed linting issues

--- stories/Text.js
+++ stories/Text.js
@@ -6,10 +6,7 @@ import Text from '../src/components/Text';
 storiesOf('Text', module)
     .addDecorator(withKnobs)
     .add('Text', () => (
-        <Text
-            inline={boolean('Inline text', false)}
-            muted={boolean('Muted text', false)}
-        >
+        <Text inline={boolean('Inline text', false)} muted={boolean('Muted text', false)}>
             {text('Content', 'This is some text content')}
         </Text>
     ));
