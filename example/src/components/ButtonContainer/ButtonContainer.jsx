7dffba645e8a7367cefa939959aa839852363da1
Guilherme <guilhermemrr@gmail.com>
2018-12-13 07:42:47 -0200

    Draft using only one theme

--- example/src/components/ButtonContainer/ButtonContainer.jsx
+++ example/src/components/ButtonContainer/ButtonContainer.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import './ButtonContainer.css'
 
-/**
- * One custom component from a application that uses Nice!, like Search!
- *
- * This component is also customizable since its CSS uses variables from the
- * application theme ( ./public/default-app-theme.css).
- */
 const ButtonContainer = ({ children }) => (
     <div className="button-container">
         {children}
