3b3e517bcd4af52787f49d58fce195edf01bfacf
Carlo Bernardini <bernardini@textkernel.nl>
2018-12-19 14:57:58 +0100

    Split single / multiselect dropdown in two stories

--- stories/Dropdown.js
+++ stories/Dropdown.js
@@ -7,7 +7,7 @@ import { CONTEXTS, SIZES } from '../src/constants';
 
 storiesOf('Dropdown', module)
     .addDecorator(withKnobs)
-    .add('Dropdown', () => {
+    .add('Single-select dropdown', () => {
         const filter = boolean('Filter', false);
         return (
             <Dropdown
@@ -33,7 +33,6 @@ storiesOf('Dropdown', module)
                 label={text('Label', 'Drop me down')}
                 maxHeight={number('Max. height', null)}
                 minWidth={number('Min. width', null)}
-                multiple={boolean('Multiselect', false)}
                 onChange={selection => {
                     // eslint-disable-next-line no-console
                     console.log(selection);
@@ -43,6 +42,58 @@ storiesOf('Dropdown', module)
                     console.log('Dropdown closed');
                 }}
                 size={select('Size', SIZES, SIZES[1])}
+            >
+                <DropdownItem value="1">Some dropdown item</DropdownItem>
+                <DropdownItem value="2">This is a very long item for this dropdown</DropdownItem>
+                <DropdownItem value="3" disabled>
+                    This is a disabled item
+                </DropdownItem>
+                <DropdownItem value="4">Yet another item</DropdownItem>
+                <DropdownItem value="5">Yet another item</DropdownItem>
+                <DropdownItem value="6">Yet another item</DropdownItem>
+                <DropdownItem value="7">Yet another item</DropdownItem>
+                <DropdownItem value="8">Yet another item</DropdownItem>
+                <DropdownItem value="9">Yet another item</DropdownItem>
+            </Dropdown>
+        );
+    })
+    .add('Multi-select dropdown', () => {
+        const filter = boolean('Filter', false);
+        return (
+            <Dropdown
+                context={select('Context', ['link', ...CONTEXTS], CONTEXTS[0])}
+                heading={text('Heading', 'This is a dropdown heading')}
+                disabled={boolean('Disabled', false)}
+                filter={
+                    filter
+                        ? {
+                              matchCase: boolean('Filter: Case sensitive matching', false),
+                              matchDiacritics: boolean('Filter: Match diacritics strictly', false),
+                              matchPosition: select(
+                                  'Filter: Match position',
+                                  ['any', 'start'],
+                                  'any'
+                              ),
+                              noMatchLabel: text('Filter: No matches label', 'No matches found'),
+                              placeholder: text('Filter: Placeholder', 'Filter options...')
+                          }
+                        : null
+                }
+                isBlock={boolean('isBlock', false)}
+                label={text('Label', 'Drop me down')}
+                maxHeight={number('Max. height', null)}
+                minWidth={number('Min. width', null)}
+                multiple
+                onChange={selection => {
+                    // eslint-disable-next-line no-console
+                    console.log(selection);
+                }}
+                onClose={() => {
+                    // eslint-disable-next-line no-console
+                    console.log('Dropdown closed');
+                }}
+                size={select('Size', SIZES, SIZES[1])}
+                value={['1', '4']}
             >
                 <DropdownItem value="1">Some dropdown item</DropdownItem>
                 <DropdownItem value="2">This is a very long item for this dropdown</DropdownItem>
