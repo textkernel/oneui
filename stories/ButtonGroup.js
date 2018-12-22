4f4dcd9c3aff8a233104d7e0cfcb9fe5cf85935b
Carlo Bernardini <bernardini@textkernel.nl>
2018-12-22 16:58:31 +0100

    Use dropdown in ButtonGroup

--- stories/ButtonGroup.js
+++ stories/ButtonGroup.js
@@ -3,6 +3,8 @@ import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-e
 import { boolean, select, withKnobs } from '@storybook/addon-knobs'; // eslint-disable-line import/no-extraneous-dependencies
 import ButtonGroup from '../src/components/ButtonGroup';
 import Button from '../src/components/Button';
+import Dropdown from '../src/components/Dropdown';
+import DropdownItem from '../src/components/Dropdown/DropdownItem';
 import { SIZES } from '../src/constants';
 
 storiesOf('ButtonGroup', module)
@@ -12,5 +14,16 @@ storiesOf('ButtonGroup', module)
             <Button context="neutral">Some</Button>
             <Button context="neutral">Button</Button>
             <Button context="neutral">Group</Button>
+        </ButtonGroup>
+    ))
+    .add('ButtonGroup with dropdown', () => (
+        <ButtonGroup isBlock={boolean('isBlock', false)} size={select('Size', SIZES, SIZES[1])}>
+            <Button context="neutral">Some</Button>
+            <Button context="neutral">Button</Button>
+            <Dropdown context="brand" label="And a dropdown">
+                <DropdownItem value="1">Some item</DropdownItem>
+                <DropdownItem value="2">Another item</DropdownItem>
+                <DropdownItem value="3">More items</DropdownItem>
+            </Dropdown>
         </ButtonGroup>
     ));
