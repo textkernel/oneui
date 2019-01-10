7bd37a8d6241400d4a8c9541da85b87faff6ea8d
Eszter Hofmann <hofmann#textkernel.nl>
2019-01-10 13:48:39 +0100

    elaborate tests

--- src/components/Checkbox/__tests__/Checkbox.spec.js
+++ src/components/Checkbox/__tests__/Checkbox.spec.js
@@ -4,16 +4,27 @@ import Checkbox from '../Checkbox';
 
 describe('<Checkbox> that renders a checkbox', () => {
     it('should render default checkbox correctly', () => {
-        const wrapper = shallow(<Checkbox id="c1">Check this out</Checkbox>);
+        const wrapper = mount(<Checkbox id="c1">Check this out</Checkbox>);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
-
-    it('should add classes when props are changed', () => {
-        const wrapper = shallow(
-            <Checkbox id="c2" disabled>
+    it('should call onChange function when clicked', () => {
+        const onChange = jest.fn();
+        const wrapper = mount(
+            <Checkbox id="c2" onChange={onChange}>
+                Check this out
+            </Checkbox>
+        );
+        wrapper.find('input').simulate('change');
+        expect(onChange).toHaveBeenCalledTimes(1);
+    });
+    it('should rendered disabled checkbox correctly', () => {
+        const wrapper = mount(
+            <Checkbox id="c3" disabled>
                 Useless checkbox
             </Checkbox>
         );
-        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.find('input[disabled]')).toHaveLength(1);
+        expect(wrapper.find('label[disabled]')).toHaveLength(1);
+        expect(wrapper.find('.Text--muted')).toHaveLength(1);
     });
 });
