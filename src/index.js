5a5c51aaa09b98f17a699fade0757ace31d3656f
Carlo Bernardini <bernardini@textkernel.nl>
2019-01-28 13:54:51 +0100

    Remove all Dummy-component files and references to it

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export { default as Dummy } from './components/Dummy';
 export { default as Alert } from './components/Alert';
 export { default as Button } from './components/Button';
 export { default as ButtonGroup } from './components/ButtonGroup';
--- stories/Dummy.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
-import Dummy from '../src/components/Dummy';
-
-storiesOf('Dummy', module).add('with emojis', () => <Dummy> Hoi! </Dummy>);
