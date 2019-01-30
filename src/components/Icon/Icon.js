84c11926b8185c260503624145fa6486438aeb5a
Carlo Bernardini <bernardini@textkernel.nl>
2019-01-30 16:31:58 +0100

    Do not apply context attribute to SVG; make icon title optional; add prop comments

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -11,7 +11,7 @@ const { block, elem } = bem({
 });
 
 const Icon = props => {
-    const { name, size, title, ...rest } = props;
+    const { context, name, size, title, ...rest } = props;
 
     const currentIcon = ICONS[name];
 
@@ -25,7 +25,7 @@ const Icon = props => {
             viewBox={currentIcon.viewBox}
             aria-labelledby="title"
         >
-            <title>{title}</title>
+            {!!title && <title>{title}</title>}
             {currentIcon.paths.map((path, i) => (
                 <path {...elem('path', props)} key={`path_${i}`} d={path} /> // eslint-disable-line react/no-array-index-key
             ))}
@@ -34,16 +34,20 @@ const Icon = props => {
 };
 
 Icon.propTypes = {
+    /** The icon context (e.g. brand, primary, bad, good etc. - defaults to brand) */
     context: PropTypes.oneOf(CONTEXTS),
+    /** Which icon to show */
     name: PropTypes.oneOf(Object.keys(ICONS)).isRequired,
+    /** Size for this icon (aspect ratio is 1:1) */
     size: PropTypes.number,
+    /** Optional icon title */
     title: PropTypes.string
 };
 
 Icon.defaultProps = {
     context: CONTEXTS[1],
     size: 16,
-    title: 'Icon'
+    title: null
 };
 
 export default Icon;
