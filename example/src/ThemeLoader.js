6d79f597f8af91668bba9480632b8980fc2fa30b
Guilherme Marquesini <guilhermemrr@gmail.com>
2019-02-07 17:45:48 +0100

    Resolving promise within onComplete callback

--- example/src/ThemeLoader.js
+++ example/src/ThemeLoader.js
@@ -3,66 +3,68 @@ import cssVarsPonyfill from 'css-vars-ponyfill';
 const THEME_URL_PARAM ='themeId';
 const THEME_CDN_URL   = './';
 
-
 class ThemeLoader {
 
     static load() {
 
         return new Promise(((resolve, reject) => {
-            ThemeLoader.startCssVarsPonyfill();
-            ThemeLoader.applyThemeIfExists();
-            resolve();
+
+
+            /**
+             * Get the parameter `themeId` from the URL in case if it exists, and append
+             * it in a `link` element in the DOM
+             */
+            const applyThemeIfExists = () => {
+
+                const queryString = window.location.search.substr(1);
+
+                if (queryString === '') return;
+
+                queryString.split('&').forEach((item) => {
+
+                    const queryParam = item.split('=');
+
+                    if (queryParam[0] === THEME_URL_PARAM) {
+                        const styleElement= document.createElement('link');
+                        styleElement.type = 'text/css';
+                        styleElement.rel = 'stylesheet';
+                        styleElement.href = `${THEME_CDN_URL}${queryParam[1]}`;
+
+                        const head = document.getElementsByTagName('head')[0]
+                        head.appendChild(styleElement);
+                    }
+
+                });
+
+            };
+
+            /**
+             * Loads the CSS ponyfill.
+             *
+             * In case the property `onlyLegacy` is set to true, it will run only in legacy browsers like IE11.
+             * For test purposes, it's set to true, so can be tested on modern browsers.
+             */
+            const startCssVarsPonyfill = () => {
+                cssVarsPonyfill({
+                    onlyLegacy: false,
+                    onlyVars: true,
+                    watch: true,
+                    onComplete: function (cssText, node, url) {
+                        resolve();
+                    },
+                    onError: function (message, node, xhr, url) {
+                        console.log(message, node, xhr, url);
+                    },
+                });
+
+            };
+
+            startCssVarsPonyfill();
+            applyThemeIfExists();
+
         }));
-
     }
-
-
-    static applyThemeIfExists() {
-        const queryString = window.location.search.substr(1);
-
-        if (queryString === '') return;
-
-        queryString.split('&').forEach((item) => {
-
-            const queryParam = item.split('=');
-
-            if (queryParam[0] === THEME_URL_PARAM) {
-                const styleElement= document.createElement('link');
-                styleElement.type = 'text/css';
-                styleElement.rel = 'stylesheet';
-                styleElement.href = `${THEME_CDN_URL}${queryParam[1]}`;
-
-                const head = document.getElementsByTagName('head')[0]
-                head.appendChild(styleElement);
-            }
-
-        });
-
-    }
-
-
-    static startCssVarsPonyfill() {
-
-        cssVarsPonyfill({
-            onlyLegacy: false, // False, so we can use it on Chrome for test
-            onlyVars: true,
-            watch: true,
-            onBeforeSend: function (xhr, node, url) {
-                console.log('onBeforeSend', url);
-            },
-            onSuccess: function (cssText, node, url) {
-                console.log('onSuccess', url);
-            },
-            onError: function (message, node, xhr, url) {
-                console.log('onError');
-                console.log(message, node, xhr, url);
-            },
-        });
-
-    }
-
-
-};
+}
 
 
 export default ThemeLoader;
