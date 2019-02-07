ff8065d485b9c140959d2714476c70c5c2e34ac6
Guilherme Marquesini <guilhermemrr@gmail.com>
2019-02-07 18:45:27 +0100

    Splitted methods. Handling rejected promise.

--- example/src/ThemeLoader.js
+++ example/src/ThemeLoader.js
@@ -5,65 +5,78 @@ const THEME_CDN_URL   = './';
 
 class ThemeLoader {
 
+
     static load() {
 
-        return new Promise(((resolve, reject) => {
+        return Promise.all([
+            ThemeLoader.applyThemeIfExists(),
+            ThemeLoader.startCssVarsPonyfill(),
+        ]);
 
-
-            /**
-             * Get the parameter `themeId` from the URL in case if it exists, and append
-             * it in a `link` element in the DOM
-             */
-            const applyThemeIfExists = () => {
-
-                const queryString = window.location.search.substr(1);
-
-                if (queryString === '') return;
-
-                queryString.split('&').forEach((item) => {
-
-                    const queryParam = item.split('=');
-
-                    if (queryParam[0] === THEME_URL_PARAM) {
-                        const styleElement= document.createElement('link');
-                        styleElement.type = 'text/css';
-                        styleElement.rel = 'stylesheet';
-                        styleElement.href = `${THEME_CDN_URL}${queryParam[1]}`;
-
-                        const head = document.getElementsByTagName('head')[0]
-                        head.appendChild(styleElement);
-                    }
-
-                });
-
-            };
-
-            /**
-             * Loads the CSS ponyfill.
-             *
-             * In case the property `onlyLegacy` is set to true, it will run only in legacy browsers like IE11.
-             * For test purposes, it's set to true, so can be tested on modern browsers.
-             */
-            const startCssVarsPonyfill = () => {
-                cssVarsPonyfill({
-                    onlyLegacy: false,
-                    onlyVars: true,
-                    watch: true,
-                    onComplete: function (cssText, node, url) {
-                        resolve();
-                    },
-                    onError: function (message, node, xhr, url) {
-                        console.log(message, node, xhr, url);
-                    },
-                });
-
-            };
-
-            startCssVarsPonyfill();
-            applyThemeIfExists();
-
-        }));
     }
+
+
+    /**
+     * Loads the CSS ponyfill.
+     *
+     * In case the property `onlyLegacy` is set to true, it will run only in legacy browsers like IE11.
+     * For test purposes, it's set to true, so can be tested on modern browsers.
+     */
+    static startCssVarsPonyfill() {
+
+        return new Promise((resolve, reject) => {
+
+            cssVarsPonyfill({
+                onlyLegacy: false,
+                onlyVars: true,
+                watch: true,
+                onComplete: function (cssText, node, url) {
+                    resolve();
+                },
+                onError: function (message, node, xhr, url) {
+                    reject(message);
+                }
+            });
+
+        });
+    }
+
+
+    /**
+     * Get the parameter `themeId` from the URL in case if it exists, and append
+     * it in a `link` element in the DOM
+     */
+    static applyThemeIfExists() {
+
+        return new Promise((resolve, reject) => {
+
+            const queryString = window.location.search.substr(1);
+
+            if (queryString === '') resolve();
+
+            queryString.split('&').forEach((item) => {
+
+                const queryParam = item.split('=');
+
+                if (queryParam[0] === THEME_URL_PARAM) {
+                    const styleElement= document.createElement('link');
+                    styleElement.type = 'text/css';
+                    styleElement.rel = 'stylesheet';
+                    styleElement.href = `${THEME_CDN_URL}${queryParam[1]}`;
+                    styleElement.onload = () => resolve();
+                    styleElement.onerror = () => reject();
+
+                    const head = document.getElementsByTagName('head')[0]
+                    head.appendChild(styleElement);
+                }
+
+            });
+
+        });
+
+    }
+
+
 }
 
 
