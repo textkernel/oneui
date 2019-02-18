c60a2dd9bb78ccb6cb7d2c5b4f049fe6621fe360
Eszter Hofmann <hofmann#textkernel.nl>
2019-02-18 16:11:21 +0100

    add support for logo as component in header; use variables for header colors and size;

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,14 +9,24 @@ const { block, elem } = bem({
 });
 
 const Header = props => {
-    const { logoSrc, logoLink, logoTitle, children, ...rest } = props;
+    const { logo, children, ...rest } = props;
+
+    const simpleLogo = logo.link ? (
+        <a href={logo.link}>
+            <img src={logo.src} alt={logo.title} {...elem('logo', props)} />
+        </a>
+    ) : (
+        <img src={logo.src} alt={logo.title || 'our logo'} {...elem('logo', props)} />
+    );
+
+    const renderedLogo = logo.src
+        ? simpleLogo
+        : React.cloneElement(logo, { ...elem('logo', props) });
 
     return (
         <div {...rest} {...block(props)}>
             <div {...elem('wrapper', props)}>
-                <a href={logoLink}>
-                    <img src={logoSrc} alt={logoTitle} {...elem('logo', props)} />
-                </a>
+                {renderedLogo}
                 <div {...elem('menu', props)}>{children}</div>
             </div>
         </div>
@@ -26,19 +36,23 @@ const Header = props => {
 Header.displayName = 'Header';
 
 Header.propTypes = {
-    /** path to the logo source file */
-    logoSrc: PropTypes.string.isRequired,
-    /** path to where the logo should link to if clicked */
-    logoLink: PropTypes.string,
-    /** a name for the logo, it will used as alternative text to the img */
-    logoTitle: PropTypes.string,
+    /** supply a logo ready to be rendered or attributes to build a simple one. */
+    logo: PropTypes.oneOfType([
+        PropTypes.node,
+        PropTypes.shape({
+            /** path to the logo source file */
+            src: PropTypes.string.isRequired,
+            /** path to where the logo should link to if clicked */
+            link: PropTypes.string,
+            /** a name for the logo, it will used as alternative text to the img */
+            title: PropTypes.string
+        })
+    ]).isRequired,
     /** Node(s) to be rendered on the right side of the header */
     children: PropTypes.oneOfType([PropTypes.string, PropTypes.node])
 };
 
 Header.defaultProps = {
-    logoLink: '/',
-    logoTitle: 'website logo',
     children: null
 };
 
--- stories/Header.js
+++ stories/Header.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 import { text, withKnobs } from '@storybook/addon-knobs'; // eslint-disable-line import/no-extraneous-dependencies
-import { Header } from '@textkernel/oneui';
+import { Header, IconTextkernel } from '@textkernel/oneui';
 
 storiesOf('Header', module)
     .addDecorator(withKnobs)
-    .add('Header', () => (
+    .add('Header with simple logo', () => (
         <Header
-            logoSrc={text('Image URL', 'https://www.jobfeed.nl/images/jobfeed-logo.svg')}
-            logoLink={text('URL where click on logo will lead', '/')}
-            logoName={text('Alt text for the logo', 'Jobfeed')}
+            logo={{
+                src: text('Image URL', 'https://www.jobfeed.nl/images/jobfeed-logo.svg'),
+                link: text('URL where click on logo will lead', '/'),
+                title: text('Alt text for the logo', 'Jobfeed')
+            }}
         >
+            {text('Left side', 'This is a placeholder for children')}
+        </Header>
+    ))
+    .add('Header with logo as component', () => (
+        <Header logo={<IconTextkernel />}>
             {text('Left side', 'This is a placeholder for children')}
         </Header>
     ));
