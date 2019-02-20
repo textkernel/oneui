58141853fa741c27f19198f30ed8ff3e90b88d05
Carlo Bernardini <bernardini@textkernel.nl>
2019-02-20 13:28:48 +0100

    DropdownFilter: strip diacritics when comparing strings

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import diacritics from 'diacritics';
+
 export const escapeRegExp = string =>
     // Escape special RegExp characters in user input
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#Using_special_characters
@@ -5,6 +7,6 @@ export const escapeRegExp = string =>
 
 export const isStringMatch = (filter, label) => {
     if (!filter) return true;
-    const re = new RegExp(`(${escapeRegExp(filter || '')})`, 'gi');
-    return (label || '').match(re);
+    const re = new RegExp(`(${escapeRegExp(diacritics.remove(filter || ''))})`, 'gi');
+    return diacritics.remove(label || '').match(re);
 };
