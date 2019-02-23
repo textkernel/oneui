62616b1178416e1b39d5a2565a9fc0a9d148f094
Carlo Bernardini <bernardini@textkernel.nl>
2019-02-23 13:31:53 +0100

    Dropdown: Updated export / import

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,1 +1,5 @@
-export { default } from './Dropdown';
+export { default as Dropdown } from './Dropdown';
+export { default as DropdownFilter } from './DropdownFilter';
+export { default as DropdownGroup } from './DropdownGroup';
+export { default as DropdownItem } from './DropdownItem';
+export { DropdownConsumer } from './DropdownContext';
