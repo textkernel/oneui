492aa41cf5e77dad5ee79be287830bd16f8b904c
Carlo Bernardini <bernardini@textkernel.nl>
2019-02-23 08:52:36 +0100

    Reorganized Tabs exporting

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,1 +1,5 @@
-export { default } from './Tabs';
+export { default as Tab } from './Tab';
+export { default as TabContent } from './TabContent';
+export { default as TabItem } from './TabItem';
+export { default as TabMenu } from './TabMenu';
+export { default as Tabs } from './Tabs';
