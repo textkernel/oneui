cd8d647b468d62f5121421eda44f7c70b61209f0
Carlo Bernardini <carlobernardini@gmail.com>
2019-03-23 13:26:16 +0100

    Added Textkernel theme for Storybook

---
+++ .storybook/oneui.theme.js
@@ -0,0 +1,11 @@
+import { create } from '@storybook/theming';
+import brandImage from './textkernel.svg';
+
+export default create({
+    base: 'light',
+
+    // Branding
+    brandTitle: 'Textkernel OneUI',
+    brandUrl: 'https://textkernel.com',
+    brandImage
+});
