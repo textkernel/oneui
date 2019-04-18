6c5944720b2f91a5994c8ec5b17eae621aaaa798
Carlo Bernardini <carlobernardini@gmail.com>
2019-04-18 10:55:09 +0200

    Content placeholder (#79)
    Adds a ContentPlaceholder component that can be shown while actual content is loading.
    
    * `height`: Variable placeholder height in pixels. When not set, it will inherit lineHeight by default.
    * `width`: Variable placeholder width, relative to the parent container. When not set, it will default to 100%.
    * `duration`: Custom animation duration in seconds. When not set, it will default to 1s.
    * `withoutMargin`: renders the placeholder without its default margin.
    
    The component is to be used inside a block-level parent element (or an element that has a width defined).

---
+++ src/components/ContentPlaceholder/ContentPlaceholder.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import bem from 'bem';
+import styles from './ContentPlaceholder.scss';
+
+const { block, elem } = bem({
+    name: 'ContentPlaceholder',
+    classnames: styles,
+    propsToMods: ['withoutMargin']
+});
+
+const calcDuration = seconds => `${seconds}s`;
+
+const calcMaskWidth = width => ({
+    width: `${100 - Math.max(Math.min(width, 100), 0)}%`
+});
+
+const ContentPlaceholder = props => {
+    const { duration, height, width, withoutMargin, ...rest } = props;
+
+    return (
+        <div
+            {...rest}
+            {...block(props)}
+            style={{
+                animationDuration: calcDuration(duration),
+                height
+            }}
+        >
+            &nbsp;
+            <div {...elem('mask', props)} style={calcMaskWidth(width)} />
+        </div>
+    );
+};
+
+ContentPlaceholder.displayName = 'ContentPlaceholder';
+
+ContentPlaceholder.propTypes = {
+    /** Animation duration in seconds */
+    duration: PropTypes.number,
+    /** Custom height in pixels for the content placeholder */
+    height: PropTypes.number,
+    /** Width of the content placeholder, relative to its parent */
+    width: PropTypes.number,
+    /** Renders placeholder without its default margin */
+    withoutMargin: PropTypes.bool
+};
+
+ContentPlaceholder.defaultProps = {
+    duration: 1,
+    height: null,
+    width: 100,
+    withoutMargin: false
+};
+
+export default ContentPlaceholder;
---
+++ src/components/ContentPlaceholder/__tests__/ContentPlaceholder.spec.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import toJson from 'enzyme-to-json';
+import ContentPlaceholder from '../ContentPlaceholder';
+
+describe('<ContentPlaceholder> that renders a content placeholder', () => {
+    it('should render default placeholder', () => {
+        const wrapper = shallow(<ContentPlaceholder />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.props().style).toEqual({
+            animationDuration: '1s',
+            height: null
+        });
+        expect(wrapper.childAt(1).props().style).toEqual({
+            width: '0%'
+        });
+    });
+
+    it('should affect styles when props are changed', () => {
+        const wrapper = shallow(
+            <ContentPlaceholder duration={3} height={30} width={60} withoutMargin />
+        );
+
+        expect(wrapper.props().style).toEqual({
+            animationDuration: '3s',
+            height: 30
+        });
+
+        expect(wrapper.childAt(1).props().style).toEqual({
+            width: '40%'
+        });
+
+        expect(wrapper.hasClass('ContentPlaceholder--withoutMargin')).toBe(true);
+    });
+});
