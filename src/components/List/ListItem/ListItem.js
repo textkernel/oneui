8403ca5d5f72b287424213d3434af487a11fe5e2
Eszter <hofmann@textkernel.nl>
2019-04-24 12:45:17 +0200

    add disabled option to ListItem (#87)
    Adds disabled option to ListItem where item is not highlighted on hover

--- src/components/List/ListItem/ListItem.js
+++ src/components/List/ListItem/ListItem.js
@@ -7,11 +7,11 @@ import styles from './ListItem.scss';
 const { block, elem } = bem({
     name: 'ListItem',
     classnames: styles,
-    propsToMods: ['isSelected', 'onClick']
+    propsToMods: ['isSelected', 'onClick', 'disabled']
 });
 
 const ListItem = props => {
-    const { children, isSelected, onClick, ...rest } = props;
+    const { children, isSelected, onClick, disabled, ...rest } = props;
     const customBlockMod = { clickable: typeof onClick === 'function' };
 
     return (
@@ -31,16 +31,19 @@ ListItem.displayName = 'ListItem';
 ListItem.propTypes = {
     /** List Items */
     children: PropTypes.node,
+    /** A function to be called if the item is clicked */
+    onClick: PropTypes.func,
     /** Formats this item as selected  */
     isSelected: PropTypes.bool,
-    /** A function to be called if the item is clicked */
-    onClick: PropTypes.func
+    /** Format this item as disabled */
+    disabled: PropTypes.bool
 };
 
 ListItem.defaultProps = {
     children: null,
+    onClick: null,
     isSelected: false,
-    onClick: null
+    disabled: false
 };
 
 export default ListItem;
