18560493cc5377ed8d5b42ac0720fb3852b66bc2
Eszter Hofmann <hofmann#textkernel.nl>
2019-05-15 16:32:39 +0200

    add context to link

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import bem from 'bem';
+import { CONTEXTS } from '../../constants';
 import styles from './Link.scss';
 
 const { block } = bem({
     name: 'Link',
-    classnames: styles
+    classnames: styles,
+    propsToMods: ['context']
 });
 
 const Link = props => {
-    const { children, ...rest } = props;
+    const { children, context, ...rest } = props;
     return (
         <a {...rest} {...block(props)}>
             {children}
@@ -20,8 +22,14 @@ const Link = props => {
 Link.displayName = 'Link';
 
 Link.propTypes = {
+    /** The spinner context (e.g. brand, primary, bad, good etc.) */
+    context: PropTypes.oneOf(['default', 'muted', ...CONTEXTS]),
     /** Link content */
     children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired
+};
+
+Link.defaultProps = {
+    context: 'default'
 };
 
 export default Link;
--- stories/Link.js
+++ stories/Link.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text, withKnobs } from '@storybook/addon-knobs';
+import { select, text, withKnobs } from '@storybook/addon-knobs';
 import { Link } from '@textkernel/oneui';
+import { CONTEXTS } from '@textkernel/oneui/constants';
 
 storiesOf('Link', module)
     .addDecorator(withKnobs)
     .add('Link', () => (
-        <Link target="_blank" href="https://textkernel.com">
+        <Link
+            target="_blank"
+            href="https://textkernel.com"
+            context={select('Context', ['default', 'muted', ...CONTEXTS], 'default')}
+        >
             {text('Link text', 'Click me')}
         </Link>
     ));
