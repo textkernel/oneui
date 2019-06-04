3aac31bfc6787102e815b8afdd57bbb23dac903b
Eszter Hofmann <hofmann#textkernel.nl>
2019-06-04 13:19:24 +0200

    add tests

--- src/components/Icon/__tests__/IconBase.spec.js
+++ src/components/Icon/__tests__/IconBase.spec.js
@@ -17,7 +17,6 @@ describe('<IconBase> that renders an SVG wrapper with all options included', () 
         );
         expect(toJson(wrapper)).toMatchSnapshot();
     });
-
     it('should correct negative sizes', () => {
         const wrapper = shallow(
             <IconBase viewBox="0 0 100 100" size={-1}>
@@ -28,7 +27,6 @@ describe('<IconBase> that renders an SVG wrapper with all options included', () 
         expect(wrapper.find('svg').prop('style').height).toBe(0);
         expect(wrapper.find('svg').prop('style').width).toBe(0);
     });
-
     it('should not apply proportional styles if no size provided', () => {
         const wrapper = shallow(
             <IconBase viewBox="0 0 100 100">
@@ -36,5 +34,25 @@ describe('<IconBase> that renders an SVG wrapper with all options included', () 
             </IconBase>
         );
         expect(wrapper.find('svg').prop('style').width).toBe('1em');
+    });
+    it('should apply proportional styles if size is provided', () => {
+        const mockSize = 48;
+        const wrapper = shallow(
+            <IconBase viewBox="0 0 100 100" size={mockSize}>
+                <circle cx="50" cy="50" r="50" />
+            </IconBase>
+        );
+        expect(wrapper.find('svg').prop('style').height).toBe(mockSize);
+        expect(wrapper.find('svg').prop('style').width).toBe(mockSize);
+    });
+    it('should apply hight only to styles if size is provided with preserveAspectRatio', () => {
+        const mockSize = 48;
+        const wrapper = shallow(
+            <IconBase viewBox="0 0 100 100" size={mockSize} preserveAspectRatio>
+                <circle cx="50" cy="50" r="50" />
+            </IconBase>
+        );
+        expect(wrapper.find('svg').prop('style').height).toBe(mockSize);
+        expect(wrapper.find('svg').prop('style').width).toBe('auto');
     });
 });
