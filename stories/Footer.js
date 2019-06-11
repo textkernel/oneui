571c9813565822158ea93ea2621cdf2ffe8dc21e
Eszter Hofmann <hofmann#textkernel.nl>
2019-06-11 13:08:10 +0200

    add styling to footer link

--- stories/Footer.js
+++ stories/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text, withKnobs } from '@storybook/addon-knobs';
-import { Footer } from '@textkernel/oneui';
+import { Footer, Link } from '@textkernel/oneui';
 
 storiesOf('Footer', module)
     .addDecorator(withKnobs)
     .add('Footer', () => (
         <Footer copyright={text('Alternative copyright', null)}>
             {text('Left side', 'This is a placeholder for children')}
+            <Link href="/"> and a link</Link>
         </Footer>
     ));
