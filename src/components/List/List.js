b9971850407accfc040a97c3f01db3c2c2f9eb72
Ihor Arkhypenko <arkhypenko@MacBook-Pro-3.local>
2019-07-16 16:52:08 +0200

    Add keyboard navigation, cover it with tests

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,12 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import bem from 'bem';
 import ListItem from './ListItem';
 import styles from './List.scss';
+import { LIST_NAVIGATION_DIRECTIONS, ENTER_KEY } from '../../constants';
 
 const { block, elem } = bem({
     name: 'List',
@@ -10,12 +14,76 @@ const { block, elem } = bem({
     propsToMods: ['isDivided']
 });
 
+const isListItem = element => element && element.type !== ListItem && element.type !== 'li';
+const NAVIGATION_STEP_VALUES = {
+    [LIST_NAVIGATION_DIRECTIONS.UP]: -1,
+    [LIST_NAVIGATION_DIRECTIONS.DOWN]: 1
+};
+
 const List = React.forwardRef((props, ref) => {
-    const { children, isDivided, ...rest } = props;
+    const { children, isDivided, onNavigate, onSelect, ...rest } = props;
+    const [selectedIndex, setSelectedIndex] = useState(null);
+
+    const getNextSelectedIndex = keyCode => {
+        const stepValue = NAVIGATION_STEP_VALUES[keyCode];
+        const nextSelectedIndex = selectedIndex + stepValue;
+
+        // Return 0 index if nextSelectedIndex has negative value or selectedIndex hasn't been updated before
+        if (nextSelectedIndex < 0 || selectedIndex === null) {
+            return 0;
+        }
+
+        // Return last React.Children index if nextSelectedIndex is out of the right bound
+        if (nextSelectedIndex >= children.length) {
+            return children.length - 1;
+        }
+
+        // Return nextSelectedIndex without any changes for others cases
+        return nextSelectedIndex;
+    };
+
+    const handleKeyDown = e => {
+        // Update selectedIndex with arrow navigation and make onNavigate function callback
+        if (e.key === LIST_NAVIGATION_DIRECTIONS.UP || e.key === LIST_NAVIGATION_DIRECTIONS.DOWN) {
+            const nextSelectedIndex = getNextSelectedIndex(e.key);
+            e.preventDefault();
+
+            if (selectedIndex !== nextSelectedIndex) {
+                setSelectedIndex(getNextSelectedIndex(e.key));
+
+                if (onNavigate) {
+                    // onNavigate(children[selectedIndex]);
+                    onNavigate(e.key);
+                }
+            }
+        }
+
+        // Imitate onClick event on Enter press and make onSelect function callback
+        if (e.key === ENTER_KEY) {
+            if (
+                children[selectedIndex] &&
+                children[selectedIndex].props &&
+                children[selectedIndex].props.onClick
+            ) {
+                children[selectedIndex].props.onClick(e);
+
+                if (onSelect) {
+                    // onSelect(children[selectedIndex]);
+                    onSelect();
+                }
+            }
+        }
+    };
+
     return (
-        <ul {...rest} ref={ref} {...block(props)}>
-            {React.Children.map(children, child =>
-                child ? React.cloneElement(child, elem('item', props)) : null
+        <ul {...rest} ref={ref} tabIndex="0" onKeyDown={handleKeyDown} {...block(props)}>
+            {React.Children.map(children, (child, index) =>
+                child
+                    ? React.cloneElement(child, {
+                          ...elem('item', props),
+                          isHighlighted: index === selectedIndex
+                      })
+                    : null
             )}
         </ul>
     );
@@ -30,7 +98,7 @@ List.propTypes = {
 
         let error = null;
         React.Children.forEach(prop, child => {
-            if (child && child.type !== ListItem && child.type !== 'li') {
+            if (isListItem(child)) {
                 error = new Error(
                     `'${componentName}' children should be of type 'ListItem' or 'li'.`
                 );
@@ -39,12 +107,18 @@ List.propTypes = {
         return error;
     },
     /** Adds dividing lines between the list items */
-    isDivided: PropTypes.bool
+    isDivided: PropTypes.bool,
+    /** onNavigate function callback */
+    onNavigate: PropTypes.func,
+    /** onSelect function callback */
+    onSelect: PropTypes.func
 };
 
 List.defaultProps = {
     children: null,
-    isDivided: false
+    isDivided: false,
+    onNavigate: null,
+    onSelect: null
 };
 
 export default List;
--- src/components/List/__tests__/List.spec.js
+++ src/components/List/__tests__/List.spec.js
@@ -2,9 +2,19 @@ import React from 'react';
 import toJson from 'enzyme-to-json';
 import ListItem from '../ListItem';
 import List from '../List';
+import { LIST_NAVIGATION_DIRECTIONS } from '../../../constants';
 
 describe('List component', () => {
     let consoleError;
+    let wrapper;
+
+    const itemNumbersArray = [0, 1, 2, 3, 4];
+    const mockOnNavigate = jest.fn();
+    const mockOnSelect = jest.fn();
+    const mockOnClick = jest.fn();
+
+    const navigateUp = () => wrapper.children().simulate('keyDown', { key: 'ArrowUp' });
+    const navigateDown = () => wrapper.children().simulate('keyDown', { key: 'ArrowDown' });
 
     beforeEach(() => {
         consoleError = jest.spyOn(console, 'error').mockImplementationOnce(() => {});
@@ -13,36 +23,92 @@ describe('List component', () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
-    it('should render List correctly', () => {
-        const wrapper = mount(
-            <List>
-                <ListItem>Item 1</ListItem>
-                <ListItem>Item 2</ListItem>
-            </List>
-        );
 
-        expect(toJson(wrapper)).toMatchSnapshot();
-        expect(wrapper.find('ul')).toHaveLength(1);
-        expect(consoleError).not.toHaveBeenCalled();
-    });
-    it('should render correctly with ref', () => {
-        const ref = React.createRef();
-        mount(
-            <List ref={ref}>
-                <ListItem>Item 1</ListItem>
-                <ListItem>Item 2</ListItem>
-            </List>
-        );
+    describe('Initial rendering', () => {
+        it('should render List correctly', () => {
+            wrapper = mount(
+                <List>
+                    <ListItem>Item 1</ListItem>
+                    <ListItem>Item 2</ListItem>
+                </List>
+            );
 
-        expect(ref.current.props.Component.displayName).toEqual('List');
+            expect(toJson(wrapper)).toMatchSnapshot();
+            expect(wrapper.find('ul')).toHaveLength(1);
+            expect(consoleError).not.toHaveBeenCalled();
+        });
+        it('should render correctly with ref', () => {
+            const ref = React.createRef();
+            mount(
+                <List ref={ref}>
+                    <ListItem>Item 1</ListItem>
+                    <ListItem>Item 2</ListItem>
+                </List>
+            );
+
+            expect(ref.current.props.Component.displayName).toEqual('List');
+        });
+        it('should warn if children are not ListItem nor li', () => {
+            mount(
+                <List>
+                    <a href="/">Item 1</a>
+                    <a href="/">Item 2</a>
+                </List>
+            );
+            expect(consoleError).toHaveBeenCalled();
+        });
     });
-    it('should warn if children are not ListItem nor li', () => {
-        mount(
-            <List>
-                <a href="/">Item 1</a>
-                <a href="/">Item 2</a>
-            </List>
-        );
-        expect(consoleError).toHaveBeenCalled();
+
+    describe('Keyboard navigation', () => {
+        beforeEach(() => {
+            wrapper = mount(
+                <List onNavigate={mockOnNavigate} onSelect={mockOnSelect}>
+                    {itemNumbersArray.map(number => (
+                        <ListItem onClick={() => mockOnClick(number)}>Item ${number}</ListItem>
+                    ))}
+                </List>
+            );
+        });
+
+        it('should not have any item highlighted from the start', () => {
+            expect(mockOnNavigate).not.toHaveBeenCalled();
+        });
+
+        it('should update selectedIndex in both directions properly', () => {
+            wrapper.children().simulate('click');
+            navigateDown();
+
+            expect(mockOnNavigate).toBeCalledWith(LIST_NAVIGATION_DIRECTIONS.DOWN);
+
+            navigateDown();
+            navigateUp();
+
+            expect(mockOnNavigate).toBeCalledWith(LIST_NAVIGATION_DIRECTIONS.UP);
+        });
+
+        it('should not let highlighting selectedIndex got out of [0, itemsArray.length] bounds', () => {
+            wrapper.children().simulate('click');
+            navigateUp();
+            navigateUp();
+
+            expect(mockOnNavigate).toHaveBeenCalledTimes(1);
+
+            itemNumbersArray.forEach(() => {
+                navigateDown();
+                navigateDown();
+            });
+
+            expect(mockOnNavigate).toHaveBeenCalledTimes(itemNumbersArray.length);
+        });
+
+        it('should call onSelect callback of the highlighted item', () => {
+            wrapper.children().simulate('click');
+            navigateDown();
+            navigateDown();
+            navigateDown();
+            wrapper.children().simulate('keyDown', { key: 'Enter' });
+
+            expect(mockOnClick).toBeCalledWith(2);
+        });
     });
 });
