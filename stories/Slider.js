0a809bf25062b9f065cab7010df559bf8ae5d22b
Ihor Arkhypenko <arkhypenko@MacBook-Pro-3.local>
2019-07-23 11:21:16 +0200

    Refactor scss file, add knobs

--- stories/Slider.js
+++ stories/Slider.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Slider } from '@textkernel/oneui';
+import { number, withKnobs } from '@storybook/addon-knobs';
 
-storiesOf('Slider', module).add(
-    'Slider',
-    () => <Slider min={0} max={100} defaultValue={10} onChange={value => console.log(value)} />,
-    {
-        info: {
-            text: `
-            ## Usage information
-            This component is a wrapper around [rc-slider](https://github.com/react-component/slider).
-            
-            Full list of props available to pass you can find [here](https://github.com/react-component/slider#api).`
+storiesOf('Slider', module)
+    .addDecorator(withKnobs)
+    .add(
+        'Slider',
+        () => (
+            <Slider
+                min={number('Min value', 0)}
+                max={number('Max value', 100)}
+                step={number('Step', 1)}
+                defaultValue={10}
+                onChange={value => console.log(value)}
+            />
+        ),
+        {
+            info: {
+                text: `
+                ## Usage information
+                This component is a wrapper around [rc-slider](https://github.com/react-component/slider).
+                
+                Full list of props available to pass you can find [here](https://github.com/react-component/slider#api).`
+            }
         }
-    }
-);
+    );
