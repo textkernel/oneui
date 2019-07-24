b7a8250f89a05670390c7ba8c9b6ed1ecbb18b6b
Eszter <hofmann@textkernel.nl>
2019-07-24 16:28:10 +0200

    feat(component): new Autosuggest, FieldWrapper and MarkedText components (#137)

---
+++ src/components/Autosuggest/__mocks__/suggestions.js
@@ -0,0 +1,13 @@
+export const SUGGESTIONS = [
+    { name: 'Sun' },
+    { name: 'Moon' },
+    { name: 'Mercury' },
+    { name: 'Venus' },
+    { name: 'Earth' },
+    { name: 'Mars' },
+    { name: 'Jupiter' },
+    { name: 'Saturn' },
+    { name: 'Neptun' }
+];
+
+export const SUGGESTION_TO_STRING = item => (item ? item.name : '');
