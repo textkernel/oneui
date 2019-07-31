f9f2ae899989f36fe2e9e0904fc80d16399d38df
Ihor Arkhypenko <arkhypenko@MacBook-Pro-3.local>
2019-07-31 11:19:08 +0200

    Add LocationAutocomplete component

---
+++ stories/LocationAutocomplete.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { LocationAutocomplete } from '@textkernel/oneui';
+
+storiesOf('Atoms|LocationAutocomplete', module).add('LocationAutocomplete', () => (
+    <LocationAutocomplete />
+));
