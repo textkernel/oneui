eb154055c0a8f2920585b26b5f3f8925c8a8733f
Eszter Hofmann <hofmann#textkernel.nl>
2019-08-05 11:26:01 +0200

    simplify storybook

--- stories/LocationAutocomplete.js
+++ stories/LocationAutocomplete.js
@@ -3,5 +3,5 @@ import { storiesOf } from '@storybook/react';
 import { LocationAutocomplete } from '@textkernel/oneui';
 
 storiesOf('Atoms|LocationAutocomplete', module).add('LocationAutocomplete', () => (
-    <LocationAutocomplete apiKey="AIzaSyC-93hDRAa77VYQUxwJHYKeytwSqKT3fLk" />
+    <LocationAutocomplete apiKey="" />
 ));
