417787b60104d77189428d09f7cdd15a4956c950
Eszter Hofmann <hofmann#textkernel.nl>
2019-08-06 15:08:29 +0200

    get API key via propmt in storybook

--- stories/LocationAutocomplete.js
+++ stories/LocationAutocomplete.js
@@ -8,19 +8,31 @@ storiesOf('Organisms|LocationAutocomplete', module)
     .add(
         'LocationAutocomplete',
         () => {
+            let apiKey = '';
             const onSelectionChange = value => {
                 console.log('onSelectionChange was called with:');
                 console.log(value);
             };
 
             const onError = error => {
-                console.log('an error was cought');
-                console.log(error);
+                if (error === 'REQUEST_DENIED') {
+                    // eslint-disable-next-line no-alert
+                    window.alert(
+                        'For this component to work, please reload the page and provide a valid API key'
+                    );
+                } else {
+                    console.log(`Google places API responded with: ${error}`);
+                }
             };
+
+            if (!window.google) {
+                // eslint-disable-next-line no-alert
+                apiKey = window.prompt('Please provide a Google API key');
+            }
 
             return (
                 <LocationAutocomplete
-                    apiKey=""
+                    apiKey={apiKey}
                     inputPlaceholder={text('Input field placeholder', 'Enter a city or region...')}
                     noSuggestionsPlaceholder={text(
                         'No suggestions found text',
