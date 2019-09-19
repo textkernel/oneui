a0e3a204d41826057264a1830cf25f3fa41d1a5a
Oleksii Mukiienko <mukiienko@textkernel.nl>
2019-09-19 13:32:38 +0200

    feat(Drawer): improvements Drawer component
    BREAKING CHANGES: changed Drawer API
    
    renamed triggeredIsExpanded > toggleDrawer

--- src/components/Drawer/__tests__/Drawer.spec.js
+++ src/components/Drawer/__tests__/Drawer.spec.js
@@ -2,50 +2,107 @@ import React from 'react';
 import toJson from 'enzyme-to-json';
 import Drawer from '../Drawer';
 
+jest.mock('react-transition-group', () => {
+    const FakeTransition = jest.fn(({ children }) => children({ state: 'entered' }));
+    return { Transition: FakeTransition };
+});
+
 describe('Drawer', () => {
     it('should render correctly', () => {
-        const wrapper = shallow(<Drawer title="some title">some text</Drawer>);
+        const wrapper = mount(<Drawer title="some title">some text</Drawer>);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
-    it('should pass initial open status', () => {
-        const wrapper = shallow(
-            <Drawer initialIsOpen title="some title">
+    it('should pass initial expand status', () => {
+        const wrapper = mount(
+            <Drawer initialIsExpanded title="some title">
                 some text
             </Drawer>
         );
 
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isOpen')).toBe(true);
-        expect(wrapper.find('.Drawer__extendButton').hasClass('Drawer__extendButton--isOpen')).toBe(
-            true
-        );
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(true);
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
+        expect(
+            wrapper.find('.Drawer__expandButton').hasClass('Drawer__expandButton--isExpanded')
+        ).toBe(true);
     });
 
     it('should pass initial close status', () => {
-        const wrapper = shallow(<Drawer title="some title">some text</Drawer>);
+        const wrapper = mount(<Drawer title="some title">some text</Drawer>);
 
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isOpen')).toBe(false);
-        expect(wrapper.find('.Drawer__extendButton').hasClass('Drawer__extendButton--isOpen')).toBe(
-            false
-        );
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(true);
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(false);
+        expect(
+            wrapper.find('.Drawer__expandButton').hasClass('Drawer__expandButton--isExpanded')
+        ).toBe(false);
     });
 
-    it('should open and close correctly', () => {
-        const wrapper = shallow(<Drawer title="some title">some text</Drawer>);
-        // Open Drawer
-        wrapper.find('.Drawer__extendButton').simulate('click');
+    it('should expand and close correctly', () => {
+        const wrapper = mount(<Drawer title="some title">some text</Drawer>);
+        // Expand Drawer
+        wrapper.find('.Drawer__expandButton').simulate('click');
 
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isOpen')).toBe(true);
-        expect(wrapper.find('.Drawer__extendButton').hasClass('Drawer__extendButton--isOpen')).toBe(
-            true
-        );
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(true);
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
+        expect(
+            wrapper.find('.Drawer__expandButton').hasClass('Drawer__expandButton--isExpanded')
+        ).toBe(true);
 
         // Close Drawer
-        wrapper.find('.Drawer__extendButton').simulate('click');
+        wrapper.find('.Drawer__expandButton').simulate('click');
 
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isOpen')).toBe(false);
-        expect(wrapper.find('.Drawer__extendButton').hasClass('Drawer__extendButton--isOpen')).toBe(
-            false
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(true);
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(false);
+        expect(
+            wrapper.find('.Drawer__expandButton').hasClass('Drawer__expandButton--isExpanded')
+        ).toBe(false);
+    });
+
+    it('should be hidden when isShown is false', () => {
+        const wrapper = mount(
+            <Drawer isShown={false} title="some title">
+                some text
+            </Drawer>
         );
+
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(false);
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
+    });
+
+    it('should be still hidden when drawer expanted and isShown is false', () => {
+        const wrapper = mount(
+            <Drawer isShown={false} isExpanded title="some title">
+                some text
+            </Drawer>
+        );
+
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(false);
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
+    });
+
+    it('should be expand then isExpanded is true', () => {
+        const wrapper = mount(
+            <Drawer isExpanded title="some title">
+                some text
+            </Drawer>
+        );
+
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(true);
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
+    });
+
+    it('should fire callback function correctly on click expand/close button', () => {
+        const onClickMock = jest.fn();
+        const wrapper = mount(
+            <Drawer onClick={onClickMock} title="some title">
+                some text
+            </Drawer>
+        );
+
+        wrapper.find('.Drawer__expandButton').simulate('click');
+
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(false);
+        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(true);
+        expect(onClickMock).toHaveBeenCalled();
     });
 });
--- stories/Drawer.js
+++ stories/Drawer.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 import { Drawer } from '@textkernel/oneui';
 
 storiesOf('Atoms|Drawer', module)
     .addDecorator(withKnobs)
-    .add('Drawer', () => (
-        <Drawer
-            initialIsOpen
-            title={text('Drawer title', '12/13 processed (2 need review, 1 failed)')}
-        >
-            {text(
-                'Content',
-                `
+    .add(
+        'Drawer',
+        () => (
+            <Drawer
+                isShown={boolean('isShown', true)}
+                isExpanded={boolean('isExpanded', true)}
+                title={text('Drawer title', '12/13 processed (2 need review, 1 failed)')}
+                onClick={
+                    boolean('Use onClick prop', false)
+                        ? () => console.log('onClick callback has been called')
+                        : undefined
+                }
+            >
+                {text(
+                    'Content',
+                    `
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Ipsum Ipsum
             Ipsum has been the industrys standard dummy text ever since the 1500s, w unknown unknown
             unknown printer took a galley of type and scrambled it to make a type spe has survived
@@ -28,6 +36,15 @@ storiesOf('Atoms|Drawer', module)
             Ipsum passages, recently with desktop publishing software like Aldus PageMaker including
             versions of Lorem Ipsum.
             `
-            )}
-        </Drawer>
-    ));
+                )}
+            </Drawer>
+        ),
+        {
+            info: {
+                text: `
+                    ## Usage information
+                    Drawer can be used like an uncontrolled and controlled component.
+                `,
+            },
+        }
+    );
