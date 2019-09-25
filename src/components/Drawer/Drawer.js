94a306f366a11fb244ed51b36a065e7933fb5177
mukiienko <alex.mukienko@gmail.com>
2019-09-25 12:16:35 +0200

    feat(Drawer): improvements Drawer component (#171)
    BREAKING CHANGE: changed Drawer API

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,41 +1,109 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import { Transition } from 'react-transition-group';
 import bem from 'bem';
 import { IoIosArrowUp } from 'react-icons/io';
+import { ENTER_KEY } from '../../constants';
 import styles from './Drawer.scss';
+
+const TRANSITION_DURATION = {
+    enter: 10,
+    exit: 300,
+};
 
 const { block, elem } = bem({
     name: 'Drawer',
     classnames: styles,
-    propsToMods: ['isOpen'],
+    propsToMods: ['isShownAndExpanded', 'isShownAndClosed', 'isExpanded'],
 });
 
 const Drawer = props => {
-    const { title, initialIsOpen, children, ...rest } = props;
-    const [isOpen, setIsOpen] = useState(initialIsOpen);
+    const {
+        title,
+        isShown,
+        isExpanded: isExpandedProps,
+        initialIsExpanded,
+        onClick,
+        children,
+        ...rest
+    } = props;
+    const [isExpanded, setIsExpanded] = useState(isExpandedProps || initialIsExpanded);
 
-    function handleClick() {
-        setIsOpen(!isOpen);
+    useEffect(() => {
+        if (isExpandedProps !== null) {
+            setIsExpanded(isExpandedProps);
+        }
+    }, [isExpandedProps]);
+
+    function getStatus() {
+        const STATUS_LIST = {
+            IS_EXPANDED: 'isShownAndExpanded',
+            IS_CLOSED: 'isShownAndClosed',
+            IS_HIDDEN: '',
+        };
+
+        if (isShown) {
+            return isExpanded ? STATUS_LIST.IS_EXPANDED : STATUS_LIST.IS_CLOSED;
+        }
+
+        return STATUS_LIST.IS_HIDDEN;
     }
 
-    return (
-        <div {...block({ isOpen })} {...rest}>
-            <header {...elem('head', props)}>
-                <h4 {...elem('headTitle', props)}>{title}</h4>
-                <div {...elem('headButtons', props)}>
-                    <button
-                        {...elem('extendButton', { isOpen })}
-                        type="button"
+    function toggleDrawer() {
+        if (onClick) {
+            onClick();
+        } else {
+            setIsExpanded(!isExpanded);
+        }
+    }
+
+    function handleClick(e) {
+        e.preventDefault();
+        toggleDrawer();
+    }
+
+    function handleKeyPress(e) {
+        e.preventDefault();
+
+        if (e.key === ENTER_KEY) {
+            toggleDrawer();
+        }
+    }
+
+    return ReactDOM.createPortal(
+        <Transition appear unmountOnExit in={isShown} timeout={TRANSITION_DURATION}>
+            {state => (
+                <div
+                    {...block({ [getStatus()]: state !== 'entering' })}
+                    aria-hidden={!isShown}
+                    {...rest}
+                >
+                    <header
+                        {...elem('head', props)}
+                        role="button"
+                        tabIndex="-1"
+                        onKeyPress={handleKeyPress}
                         onClick={handleClick}
                     >
-                        <IoIosArrowUp {...elem('extendIcon', props)} />
-                    </button>
+                        <h4 {...elem('headTitle', props)}>{title}</h4>
+                        <div {...elem('headButtons', props)}>
+                            <button
+                                {...elem('expandButton', { isExpanded })}
+                                type="button"
+                                onClick={handleClick}
+                            >
+                                <IoIosArrowUp {...elem('expandIcon', props)} />
+                            </button>
+                        </div>
+                    </header>
+                    <section {...elem('body', props)} aria-hidden={!(isShown && isExpanded)}>
+                        {children}
+                    </section>
                 </div>
-            </header>
-            <section {...elem('body', props)} aria-hidden={!isOpen}>
-                {children}
-            </section>
-        </div>
+            )}
+        </Transition>,
+        document.body
     );
 };
 
@@ -43,13 +111,22 @@ Drawer.propTypes = {
     /** Drawer title */
     title: PropTypes.string.isRequired,
     /** Set open/close status for initial state */
-    initialIsOpen: PropTypes.bool,
+    initialIsExpanded: PropTypes.bool,
+    /** Control visibility Drawer component */
+    isShown: PropTypes.bool,
+    /** Set open/close status for state */
+    isExpanded: PropTypes.bool,
+    /** Fired after click open/close button */
+    onClick: PropTypes.func,
     /** Node(s) to be rendered inside the container */
     children: PropTypes.node.isRequired,
 };
 
 Drawer.defaultProps = {
-    initialIsOpen: false,
+    initialIsExpanded: false,
+    isShown: true,
+    isExpanded: null,
+    onClick: null,
 };
 
 Drawer.displayName = 'Drawer';
