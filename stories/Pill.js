4c93e9dc31d148e558c9c8c9c6bd8fed00532627
Eszter Hofmann <hofmann#textkernel.nl>
2019-10-03 13:01:29 +0200

    style adjustment; more complete story;

--- stories/Pill.js
+++ stories/Pill.js
@@ -6,15 +6,29 @@ import { PillButton } from '@textkernel/oneui';
 storiesOf('Molecules|Pill', module)
     .addDecorator(withKnobs)
     .add('PillButton', () => (
-        <PillButton
-            isOpen={boolean('Pill is open', false)}
-            togglePopup={() => {
-                console.log('togglePopup has been called');
-            }}
-            onClear={() => {
-                console.log('onClear has been called');
-            }}
-            name={text('Name of the pill', 'Pill name')}
-            content={text('Label for pill content', 'This pill is used')}
-        />
+        <div style={{ display: 'flex' }}>
+            <PillButton
+                isOpen={boolean('Pill is open', false)}
+                togglePopup={() => {
+                    console.log('togglePopup has been called');
+                }}
+                onClear={() => {
+                    console.log('onClear has been called');
+                }}
+                name={text('Name of the pill', 'Pill name')}
+                content={text('Label for pill content', 'This pill is used')}
+            />
+            &nbsp;&nbsp;
+            <PillButton
+                isOpen={boolean('Pill is open 2', false)}
+                togglePopup={() => {
+                    console.log('togglePopup 2 has been called');
+                }}
+                onClear={() => {
+                    console.log('onClear 2 has been called');
+                }}
+                name={text('Name of the pill 2', 'Pill 2')}
+                content={text('Label for pill content 2', '')}
+            />
+        </div>
     ));
