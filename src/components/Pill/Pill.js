99ec373b83f77f53339984de2d125398388727a3
Eszter Hofmann <hofmann#textkernel.nl>
2019-10-09 17:32:02 +0200

    add support for external refs

--- src/components/Pill/Pill.js
+++ src/components/Pill/Pill.js
@@ -11,8 +11,18 @@ const { block } = bem({
 });
 
 const Pill = props => {
-    const { children, onClear, name, content } = props;
-    const dropdownRef = React.createRef();
+    const {
+        onClear,
+        name,
+        ref,
+        content,
+        children,
+        dropdownRef: dropdownRefFromProps,
+        ...rest
+    } = props;
+
+    const buttonRef = ref || React.createRef();
+    const dropdownRef = dropdownRefFromProps || React.createRef();
 
     // eslint-disable-next-line react/display-name, react/prop-types
     const buttonRenderer = ({ setPopupVisibility, isOpen }) => {
@@ -27,22 +37,24 @@ const Pill = props => {
                 isOpen={isOpen}
                 toggleDropdown={toggleDropdown}
                 onClear={onClear}
+                {...rest}
             />
         );
     };
 
-    const dropdownRenderer = ({ setPopupVisibility, ref }) =>
+    const dropdownRenderer = ({ setPopupVisibility, ref: refFromPopup }) =>
         children({
-            ref,
+            ref: refFromPopup,
             close: () => setPopupVisibility(false),
             innerPadding: 'var(--spacing-3x)',
-            ...block(props),
+            ...block({}),
         });
 
     return (
         <PopupBase
             anchorRenderer={buttonRenderer}
             popupRenderer={dropdownRenderer}
+            anchorRef={buttonRef}
             popupRef={dropdownRef}
         />
     );
@@ -54,7 +66,10 @@ Pill.propTypes = {
     /** The dropdown content renderer function. It is called with:
      *   * close {function} that closes the dropdown
      *   * className {string} that should be applied to the container
-     * It should return a single div */
+     *   * innerPadding {string} that can be applied inside the component to set consistent padding
+     * NOTE: The returned element should support refForward, but should not have it set.
+     *   If you need to access the ref, pass the ref with dropdownRef prop (see below)
+     */
     children: PropTypes.func.isRequired,
     /** a function to be called to clear the pill/filter content */
     onClear: PropTypes.func.isRequired,
@@ -62,10 +77,16 @@ Pill.propTypes = {
     name: PropTypes.string.isRequired,
     /** label describing the content of an active filter/pill */
     content: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    /** ref for pill button */
+    ref: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+    /** ref for dropdown */
+    dropdownRef: PropTypes.object, // eslint-disable-line react/forbid-prop-types
 };
 
 Pill.defaultProps = {
     content: null,
+    ref: null,
+    dropdownRef: null,
 };
 
 export default Pill;
