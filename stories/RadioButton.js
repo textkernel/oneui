cde1b6c8b42d7a026451d91d99a0fc73872a6f9e
Eszter Hofmann <hofmann#textkernel.nl>
2019-10-31 15:03:46 +0100

    remove redundant code from story

--- stories/RadioButton.js
+++ stories/RadioButton.js
@@ -12,26 +12,10 @@ storiesOf('Molecules|RadioButton', module)
                 console.log('onChange was called with event.target.value: ', e.target.value)
             }
         >
-            <RadioButton
-                disabled={boolean('Disabled 1', false)}
-                value="option1"
-                id="radio-1"
-                onChange={e => {
-                    const { value } = e.target;
-                    console.log(`Radio value changed to ${value}`, e);
-                }}
-            >
+            <RadioButton disabled={boolean('Disabled 1', false)} value="option1" id="radio-1">
                 {text('RadioButton 1 label', 'Option 1')}
             </RadioButton>
-            <RadioButton
-                disabled={boolean('Disabled 2', false)}
-                value="option2"
-                id="radio-2"
-                onChange={e => {
-                    const { value } = e.target;
-                    console.log(`Radio value changed to ${value}`, e);
-                }}
-            >
+            <RadioButton disabled={boolean('Disabled 2', false)} value="option2" id="radio-2">
                 {text('RadioButton 2 label', 'Option 2')}
             </RadioButton>
         </RadioButtonGroup>
