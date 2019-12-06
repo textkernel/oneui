0c4c6b4df537af270a66bde9bb48efc7417494df
Ihor Arkhypenko <arkhypenko@MacBook-Pro-3.local>
2019-12-06 10:43:26 +0100

    Change story file extension

---
+++ stories/Tooltip.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, select, text } from '@storybook/addon-knobs';
+import { Tooltip } from '@textkernel/oneui';
+import { POPUP_PLACEMENTS } from '../src/constants';
+
+storiesOf('Molecules|Tooltip', module)
+    .addDecorator(withKnobs)
+    .add('Tooltip', () => (
+        <div style={{ display: 'table', margin: '0 auto' }}>
+            <Tooltip
+                placement={select('Placement', POPUP_PLACEMENTS, 'bottom')}
+                content={text('Tooltip text', 'this is my tooltip text')}
+            >
+                hover here to see the tooltip
+            </Tooltip>
+        </div>
+    ));
