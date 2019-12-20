dc3d18ba27f01fed9ec562011b114af1fd9bba52
Eszter Hofmann <hofmann#textkernel.nl>
2019-12-20 13:55:34 +0100

    elaborate example

--- stories/Tabs.tsx
+++ stories/Tabs.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
-import { TabItem, TabsBar } from '@textkernel/oneui';
+import { TabItem, TabsBar, Tooltip } from '@textkernel/oneui';
 import withStore from '../src/packages/storybook/withStore';
 
 storiesOf('Atoms|Tabs', module)
@@ -69,15 +69,18 @@ storiesOf('Atoms|Tabs', module)
                 onSelect={handleSelect}
                 isBlock={boolean('Equally spaced items', false)}
             >
-                {tabs.map(tab => (
-                    <TabItem
-                        tabId={tab}
-                        key={tab}
-                        disabled={boolean(`Tab ${tab} is disabled`, false)}
-                    >
-                        {text(`Tab${tab} label`, `${tab}. tab`)}
-                    </TabItem>
-                ))}
+                <TabItem tabId={1} key={1} disabled={boolean(`Tab 1 is disabled`, false)}>
+                    {text(`Tab 1 label`, `Simple tab`)}
+                </TabItem>
+                <TabItem tabId={2} key={2} disabled={boolean(`Tab 2 is disabled`, false)}>
+                    {text(`Tab 2 label`, `Tab with styled count`)}
+                    <span style={{ color: 'grey', fontWeight: 400 }}> (2)</span>
+                </TabItem>
+                <TabItem tabId={3} key={3} disabled={boolean(`Tab 3 is disabled`, false)}>
+                    <Tooltip content="some additional information">
+                        {text(`Tab 3 label`, `Tab with Tooltip`)}
+                    </Tooltip>
+                </TabItem>
             </TabsBar>
         );
     });
