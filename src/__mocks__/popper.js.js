f173e698658eac14dc13b174d66836ea815b8cc8
Ihor Arkhypenko <arkhypenko@MacBook-Pro-3.local>
2020-02-11 10:42:21 +0100

    Refactor code

--- src/__mocks__/popper.js.js
+++ src/__mocks__/popper.js.js
@@ -1,9 +1,5 @@
-import { placements } from '@popperjs/core';
-
 // eslint-disable-next-line import/no-default-export
 export default class Popper {
-    static placements = placements;
-
     constructor() {
         return {
             destroy: () => {},
