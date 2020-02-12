6c06befee2fe55610861b9cd37509ebfad5558d0
Igor <jagger173@gmail.com>
2020-02-12 17:30:50 +0100

    build(deps): migrate popper.js to v2 (#230)
    * Update popper library

--- src/__mocks__/popper.js.js
+++ src/__mocks__/popper.js.js
@@ -1,9 +1,5 @@
-import PopperJs from 'popper.js';
-
 // eslint-disable-next-line import/no-default-export
 export default class Popper {
-    static placements = PopperJs.placements;
-
     constructor() {
         return {
             destroy: () => {},
