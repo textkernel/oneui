1572e4f511e4ac7b82f82d93208a3b843992a39f
Igor <jagger173@gmail.com>
2020-02-26 15:53:59 +0100

    feat(Field): Add new component (#235)

---
+++ stories/Field.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, withKnobs } from '@storybook/addon-knobs';
+import { Field, Input } from '@textkernel/oneui';
+
+storiesOf('Molecules|Field', module)
+    .addDecorator(withKnobs)
+    .add('Field with label', () => (
+        <Field labelText={text('Label', 'Some label')}>
+            <Input />
+        </Field>
+    ));
