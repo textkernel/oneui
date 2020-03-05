e98f2bfa89f050711ceea451c20da8453f7bddc9
Ihor Arkhypenko <arkhypenko@MacBook-Pro-3.local>
2020-03-05 15:23:12 +0100

    fix: fix webpack config

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved */
 import * as React from 'react';
 import Tippy, { TippyProps } from '@tippy.js/react';
-import 'tippy.js/dist/tippy.css';
-import 'tippy.js/animations/shift-toward.css';
+import 'tippy.js/dist/tippy.css?external';
+import 'tippy.js/animations/shift-toward.css?external';
 
 export const Tooltip: React.FC<TippyProps> = props => {
     const { content, children, ...rest } = props;
