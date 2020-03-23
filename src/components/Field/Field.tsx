d67a405bcb56c82157e35a54213748f9006361b1
Carlo Bernardini <bernardini@textkernel.nl>
2020-03-23 13:25:20 +0100

    fix(Field): Add rest parameter (#240)
    Adds `...rest` parameter to `label` element to apply additional attributes

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import * as React from 'react';
 import { bem } from '../../utils';
 import styles from './Field.scss';
 
 const { block, elem } = bem('Field', styles);
 
-type Props = {
+interface Props extends React.HTMLAttributes<HTMLLabelElement> {
     /** Label text for the input */
     labelText: string;
     /** Component to be rendered inside the field */
     children: React.ReactNode;
-};
+}
 
-export const Field: React.FC<Props> = ({ labelText, children }) => (
+export const Field: React.FC<Props> = ({ labelText, children, ...rest }) => (
     // eslint-disable-next-line jsx-a11y/label-has-associated-control
-    <label {...block()}>
+    <label {...rest} {...block()}>
         <p {...elem('label')}>{labelText}</p>
         {children}
     </label>
