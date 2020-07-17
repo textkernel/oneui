77db9d02da6a22176cb27e6feae91417c258039a
Eszter <hofmann@textkernel.nl>
2020-07-17 15:15:07 +0200

    feat(Link): add context muted option (#333)

--- stories/Link.tsx
+++ stories/Link.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text, withKnobs } from '@storybook/addon-knobs';
+import { text, select, withKnobs } from '@storybook/addon-knobs';
 import { Link } from '@textkernel/oneui';
 
 storiesOf('Atoms|Link', module)
     .addDecorator(withKnobs)
     .add('Link', () => (
-        <Link target="_blank" href="https://textkernel.com">
+        <Link
+            target="_blank"
+            href="https://textkernel.com"
+            context={select('Context', ['muted', 'brand'], 'brand')}
+        >
             {text('Link text', 'Click me')}
         </Link>
     ));
