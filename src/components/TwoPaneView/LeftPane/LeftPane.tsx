c144812965533cd2d18dcd95927a73541e8551da
Eszter Hofmann <hofmann@textkernel.nl>
2020-07-21 12:59:23 +0200

    revert declaration style changes

--- src/components/TwoPaneView/LeftPane/LeftPane.tsx
+++ src/components/TwoPaneView/LeftPane/LeftPane.tsx
@@ -11,7 +11,7 @@ export interface Props extends React.HTMLAttributes<HTMLDivElement> {
 
 const { block } = bem('LeftPane', styles);
 
-export const LeftPane = React.forwardRef<HTMLDivElement, Props>(
+export const LeftPane: React.FC<Props> = React.forwardRef(
     (props, ref): React.ReactElement => {
         const { children, ...rest } = props;
 
