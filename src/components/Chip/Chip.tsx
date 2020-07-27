26900cd5ead2f99489a7989a9d4ae8a92da67b1a
Eszter <hofmann@textkernel.nl>
2020-07-27 09:47:14 +0200

    Fix(Tooltip): allow most of our components to be wrapped in Tooltip (#340)

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -5,18 +5,20 @@ import styles from './Chip.scss';
 interface Props extends React.HTMLAttributes<HTMLSpanElement> {
     /** The content of the chip: it is expected to be short text possibly accompanied by an icon */
     children: NotEmptyReactNode;
+    /** Ref to access the span element */
+    ref?: React.RefObject<HTMLSpanElement>;
 }
 
 const { block } = bem('Chip', styles);
 
-export const Chip: React.FC<Props> = (props) => {
+export const Chip: React.FC<Props> = React.forwardRef((props, ref) => {
     const { children, ...rest } = props;
 
     return (
-        <span {...rest} {...block(props)}>
+        <span ref={ref} {...rest} {...block(props)}>
             {children}
         </span>
     );
-};
+});
 
 Chip.displayName = 'Chip';
