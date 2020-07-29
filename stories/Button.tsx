27e232d1d34a72e311c1dde4a4fddacb7211262b
Ihor Arkhypenko <arkhypenko@cas-c01.p.nl01.textkernel.net>
2020-07-29 14:02:52 +0200

    feat: move Button with an anchor to a separate story page

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -18,7 +18,22 @@ storiesOf('Atoms|Button', module)
             onClick={(e) => {
                 console.log('Clicked button', e);
             }}
-            href={text('Href (will render an anchor)', '')}
+        >
+            {text('Button label', 'Click me!')}
+        </Button>
+    ))
+    .add('Button as a link', () => (
+        <Button
+            target="_blank"
+            context={select('Context', ['link', ...CONTEXTS], CONTEXTS[0])}
+            href={text('Href URL', 'https://github.com/textkernel/oneui')}
+            size={select('Size', SIZES, SIZES[1])}
+            isBlock={boolean('isBlock', false)}
+            isInline={boolean('isInline', false)}
+            disabled={boolean('Disabled', false)}
+            onClick={(e) => {
+                console.log('Clicked button', e);
+            }}
         >
             {text('Button label', 'Click me!')}
         </Button>
