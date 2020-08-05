f5c6988e15066bc13722c77e10d1f4fe80ceae56
Eszter Hofmann <hofmann@textkernel.nl>
2020-08-05 10:48:22 +0200

    remove prop definitions that only duplicate rc-slider

--- src/components/Sliders/RangeSlider/RangeSlider.tsx
+++ src/components/Sliders/RangeSlider/RangeSlider.tsx
@@ -3,27 +3,10 @@ import { Range, RangeProps } from 'rc-slider';
 import '../Slider.scss';
 
 /** See technical notes in Slider.tsx */
-export interface Props extends RangeProps {
-    /** Set initial range of slider [lower, higher] */
-    defaultValue: [number, number];
-    /** The minimum value of the slider */
-    min: number;
-    /** The maximum value of the slider */
-    max: number;
-    /**
-     * Value to be added or subtracted on each step the slider makes.
-     * Must be greater than zero, and max - min should be evenly divisible by the step value.
-     */
-    step: number;
-}
-
-export const RangeSlider: React.FC<Props> = (props) => <Range {...props} />;
+export const RangeSlider: React.FC<RangeProps> = (props) => <Range {...props} />;
 
 RangeSlider.displayName = 'RangeSlider';
 
 RangeSlider.defaultProps = {
     defaultValue: [0, 100],
-    min: 0,
-    max: 100,
-    step: 1,
 };
