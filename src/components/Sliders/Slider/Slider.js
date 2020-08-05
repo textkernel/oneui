334933562c5b871939f5a847ffa2c539fbfbbb16
Eszter Hofmann <hofmann@textkernel.nl>
2020-08-05 14:48:01 +0200

    Revert "move to TS"
    This reverts commit 3bc3d9dd00e18e69b547b6f068160836b5563351.

---
+++ src/components/Sliders/Slider/Slider.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import RCSlider from 'rc-slider/lib/Slider';
+import '../Slider.scss';
+
+export const Slider = React.forwardRef((props, ref) => <RCSlider {...props} ref={ref} />);
+
+Slider.displayName = 'Slider';
+
+Slider.propTypes = {
+    /** Set initial value of slider */
+    defaultValue: PropTypes.number,
+    /** The minimum value of the slider */
+    min: PropTypes.number,
+    /** The maximum value of the slider */
+    max: PropTypes.number,
+    /**
+     * Value to be added or subtracted on each step the slider makes.
+     * Must be greater than zero, and max - min should be evenly divisible by the step value.
+     */
+    step: PropTypes.number,
+};
+
+Slider.defaultProps = {
+    defaultValue: 0,
+    min: 0,
+    max: 100,
+    step: 1,
+};
