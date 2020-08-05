31277a269f1748d4aa8d9e93bf9e21a169a4d8d8
Eszter Hofmann <hofmann@textkernel.nl>
2020-08-05 14:47:33 +0200

    Revert "remove prop definitions that only duplicate rc-slider"
    This reverts commit f5c6988e15066bc13722c77e10d1f4fe80ceae56.

--- src/components/Sliders/Slider/Slider.tsx
+++ src/components/Sliders/Slider/Slider.tsx
@@ -2,12 +2,33 @@ import * as React from 'react';
 import RCSlider, { SliderProps } from 'rc-slider';
 import '../Slider.scss';
 
-// Technical notes:
-// * rc-slider seems not to support forward ref with RefObject,
-//   see: https://github.com/react-component/slider/blob/80b0bf61b412d5db06241610ed5e6340a159a2f6/src/common/createSlider.tsx#L246
-// * version 9.3.0 is not JSX/TSX compatible
-//   see: https://github.com/react-component/slider/issues/656
+/** Technical notes:
+ * rc-slider seems not to support forward ref with RefObject,
+ *    see: https://github.com/react-component/slider/blob/80b0bf61b412d5db06241610ed5e6340a159a2f6/src/common/createSlider.tsx#L246
+ * version 9.3.0 is not TS compatible
+ *    see: https://github.com/react-component/slider/issues/656
+ */
+export interface Props extends Omit<SliderProps, 'ref'> {
+    /** Set initial value of slider */
+    defaultValue: number;
+    /** The minimum value of the slider */
+    min: number;
+    /** The maximum value of the slider */
+    max: number;
+    /**
+     * Value to be added or subtracted on each step the slider makes.
+     * Must be greater than zero, and max - min should be evenly divisible by the step value.
+     */
+    step: number;
+}
 
-export const Slider: React.FC<SliderProps> = (props) => <RCSlider {...props} />;
+export const Slider: React.FC<Props> = (props) => <RCSlider {...props} />;
 
 Slider.displayName = 'Slider';
+
+Slider.defaultProps = {
+    defaultValue: 0,
+    min: 0,
+    max: 100,
+    step: 1,
+};
