d9e4a8ecf0ad2817dcbb2d1d7e1d1eabf56dc815
Eszter <hofmann@textkernel.nl>
2020-09-01 11:08:45 +0200

    feat(Modal): add prop to toggle modal position to scrollable/fixed (#377)

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,12 +1,38 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import ReactModal from 'react-modal';
 import { bem } from '../../utils';
 import styles from './Modal.scss';
 
+interface Props extends ReactModal.Props {
+    /** elements to be rendered within the modal */
+    children: ReactNode;
+    /** The state of the modal */
+    isOpen: boolean;
+    /** A title for the modal that will be used by screenreaders */
+    contentLabel: string;
+    /** A function to be called when the modal is closed */
+    onRequestClose?: (
+        event: React.MouseEvent<Element, MouseEvent> | React.KeyboardEvent<Element>
+    ) => void;
+    /** When true the content of the modal will be scrollable, but the modal itself will stayed fixed inside the viewport */
+    isPositionFixed?: boolean;
+    /** Additional class to be applied to the content part */
+    className?: string;
+    /** Additional class to be applied to the overlay */
+    overlayClassName?: string;
+    /** Additional class to be applied to the portal */
+    portalClassName?: string;
+}
+
+interface Modal<P> extends React.FunctionComponent<P> {
+    setAppElement: (appElement: string | HTMLElement) => void;
+}
+
 const { block, elem } = bem('Modal', styles);
 
-export const Modal = (props) => {
+export const Modal: Modal<Props> = (props) => {
+    const overlayRef = React.useRef<HTMLDivElement | null>(null);
+
     const {
         children,
         isOpen,
@@ -19,6 +45,7 @@ export const Modal = (props) => {
     } = props;
 
     const { className: portalClass } = block({ className: portalClassName, ...rest });
+    const { className: bodyOpenClass } = elem('body', props);
     const { className: overlayClass } = elem('overlay', {
         ...props,
         elemClassName: overlayClassName,
@@ -29,12 +56,24 @@ export const Modal = (props) => {
     const { className: contentEnteredClass } = elem('content--entered', props);
     const { className: contentExitedClass } = elem('content--exited', props);
 
+    const setOverlayRef = (node) => {
+        overlayRef.current = node;
+    };
+
+    // Make sure overlay is scrolled at top after opening
+    const onAfterOpen = () => {
+        if (overlayRef.current) {
+            overlayRef.current.scrollTop = 0;
+        }
+    };
+
     return (
         <ReactModal
             isOpen={isOpen}
             contentLabel={contentLabel}
             onRequestClose={onRequestClose}
             closeTimeoutMS={300}
+            bodyOpenClassName={bodyOpenClass}
             portalClassName={portalClass}
             overlayClassName={{
                 base: overlayClass,
@@ -46,6 +85,8 @@ export const Modal = (props) => {
                 afterOpen: contentEnteredClass,
                 beforeClose: contentExitedClass,
             }}
+            overlayRef={setOverlayRef}
+            onAfterOpen={onAfterOpen}
             {...rest}
         >
             {children}
@@ -63,26 +104,6 @@ Modal.setAppElement = (selector) => {
 
 Modal.displayName = 'Modal';
 
-Modal.propTypes = {
-    /** elements to be rendered within the modal */
-    children: PropTypes.node.isRequired,
-    /** The state of the modal */
-    isOpen: PropTypes.bool.isRequired,
-    /** A title for the modal that will be used by screenreaders */
-    contentLabel: PropTypes.string.isRequired,
-    /** A function to be called when the modal is closed */
-    onRequestClose: PropTypes.func,
-    /** Additional class to be applied to the content part */
-    className: PropTypes.string,
-    /** Additional class to be applied to the overlay */
-    overlayClassName: PropTypes.string,
-    /** Additional class to be applied to the portal */
-    portalClassName: PropTypes.string,
-};
-
 Modal.defaultProps = {
-    onRequestClose: null,
-    className: '',
-    overlayClassName: null,
-    portalClassName: null,
+    isPositionFixed: false,
 };
---
+++ stories/Modal.tsx
@@ -0,0 +1,154 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, withKnobs } from '@storybook/addon-knobs';
+import { Modal, Button } from '@textkernel/oneui';
+import { StoreInjector } from '../src/packages/storybook/withStore';
+
+storiesOf('Atoms|Modal', module)
+    .addDecorator(withKnobs)
+    .addParameters(
+        StoreInjector.withStore({
+            isOpen: false,
+        })
+    )
+    .add(
+        'Modal that scrolls',
+        (storyContext) => {
+            const store = storyContext?.parameters.getStore();
+            const onClose = () => {
+                store.set({ isOpen: false });
+                console.log('Modal was requested to be closed.');
+            };
+            return (
+                <div>
+                    <Button onClick={() => store.set({ isOpen: true })}>Open Modal</Button>
+                    <Modal
+                        isOpen={store.get('isOpen')}
+                        contentLabel={text(
+                            'Label for content visible to screenreaders',
+                            'My superb modal'
+                        )}
+                        onRequestClose={onClose}
+                    >
+                        <h1>A heading</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                            sollicitudin, orci cursus sodales blandit, mi elit semper est, ac
+                            pretium ipsum dui in erat. In hac habitasse platea dictumst. Ut
+                            scelerisque odio a sapien maximus posuere. Integer fringilla, metus id
+                            semper dapibus, elit libero laoreet nibh, molestie aliquet massa velit
+                            nec est. Morbi euismod libero eget enim rutrum ullamcorper. Curabitur
+                            consequat a urna a varius. Duis et nulla ultrices lorem pulvinar varius.
+                            Nullam sed hendrerit magna. Aliquam molestie mauris justo, nec luctus
+                            lectus viverra eu. Integer mi lorem, imperdiet dictum nisi quis, dictum
+                            egestas est. Ut ut semper lectus, eget aliquam nibh. Donec vestibulum
+                            consequat felis in aliquet.
+                        </p>
+                        <p>
+                            Sed metus eros, sodales sed ante sed, pharetra finibus ipsum. Ut quis
+                            dolor quis metus ultrices lacinia. Donec sed metus consequat, aliquet
+                            libero in, pellentesque quam. Nullam nec aliquam massa. Morbi eu purus
+                            at nulla sagittis imperdiet. Pellentesque aliquet tincidunt tellus, vel
+                            dapibus erat feugiat quis. Praesent non ipsum lectus.
+                        </p>
+                        <p>
+                            Aliquam tincidunt sem non risus vulputate, laoreet fringilla erat
+                            congue. Pellentesque at magna ultricies, pharetra lacus et, venenatis
+                            nunc. Sed et posuere erat, eget faucibus sem. Duis vulputate mauris
+                            vitae felis porttitor lobortis. Vivamus eget egestas enim. Sed id sem
+                            faucibus, feugiat dolor vitae, pharetra turpis. Suspendisse consequat
+                            laoreet magna, vitae scelerisque odio. Pellentesque habitant morbi
+                            tristique senectus et netus et malesuada fames ac turpis egestas.
+                        </p>
+                    </Modal>
+                </div>
+            );
+        },
+        {
+            info: {
+                text: `
+                ## Usage information
+
+                This component is a wrapper around [react-modal](http://reactcommunity.org/react-modal/#usage).
+
+                * You can pass other props according to their definition, apart from classes.
+                * For accessibility reasons you need to __initialise__ modal use in your app.
+                For more info see [app element](http://reactcommunity.org/react-modal/accessibility/#app-element) in thier documentation.
+                To do this call the following function once:
+
+                > __Modal.setAppElement__(appElementSelector);
+                `,
+            },
+        }
+    )
+    .add(
+        'Modal with fixed position',
+        (storyContext) => {
+            const store = storyContext?.parameters.getStore();
+            const onClose = () => {
+                store.set({ isOpen: false });
+                console.log('Modal was requested to be closed.');
+            };
+            return (
+                <div>
+                    <Button onClick={() => store.set({ isOpen: true })}>Open Modal</Button>
+                    <Modal
+                        isOpen={store.get('isOpen')}
+                        contentLabel={text(
+                            'Label for content visible to screenreaders',
+                            'My superb modal'
+                        )}
+                        onRequestClose={onClose}
+                        isPositionFixed
+                    >
+                        <h1>A heading</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                            sollicitudin, orci cursus sodales blandit, mi elit semper est, ac
+                            pretium ipsum dui in erat. In hac habitasse platea dictumst. Ut
+                            scelerisque odio a sapien maximus posuere. Integer fringilla, metus id
+                            semper dapibus, elit libero laoreet nibh, molestie aliquet massa velit
+                            nec est. Morbi euismod libero eget enim rutrum ullamcorper. Curabitur
+                            consequat a urna a varius. Duis et nulla ultrices lorem pulvinar varius.
+                            Nullam sed hendrerit magna. Aliquam molestie mauris justo, nec luctus
+                            lectus viverra eu. Integer mi lorem, imperdiet dictum nisi quis, dictum
+                            egestas est. Ut ut semper lectus, eget aliquam nibh. Donec vestibulum
+                            consequat felis in aliquet.
+                        </p>
+                        <p>
+                            Sed metus eros, sodales sed ante sed, pharetra finibus ipsum. Ut quis
+                            dolor quis metus ultrices lacinia. Donec sed metus consequat, aliquet
+                            libero in, pellentesque quam. Nullam nec aliquam massa. Morbi eu purus
+                            at nulla sagittis imperdiet. Pellentesque aliquet tincidunt tellus, vel
+                            dapibus erat feugiat quis. Praesent non ipsum lectus.
+                        </p>
+                        <p>
+                            Aliquam tincidunt sem non risus vulputate, laoreet fringilla erat
+                            congue. Pellentesque at magna ultricies, pharetra lacus et, venenatis
+                            nunc. Sed et posuere erat, eget faucibus sem. Duis vulputate mauris
+                            vitae felis porttitor lobortis. Vivamus eget egestas enim. Sed id sem
+                            faucibus, feugiat dolor vitae, pharetra turpis. Suspendisse consequat
+                            laoreet magna, vitae scelerisque odio. Pellentesque habitant morbi
+                            tristique senectus et netus et malesuada fames ac turpis egestas.
+                        </p>
+                    </Modal>
+                </div>
+            );
+        },
+        {
+            info: {
+                text: `
+                ## Usage information
+
+                This component is a wrapper around [react-modal](http://reactcommunity.org/react-modal/#usage).
+
+                * You can pass other props according to their definition, apart from classes.
+                * For accessibility reasons you need to __initialise__ modal use in your app.
+                For more info see [app element](http://reactcommunity.org/react-modal/accessibility/#app-element) in thier documentation.
+                To do this call the following function once:
+
+                > __Modal.setAppElement__(appElementSelector);
+                `,
+            },
+        }
+    );
