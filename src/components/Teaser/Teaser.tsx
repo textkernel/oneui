d9de975f63e46d74d3751c9d4800357a2284cfbe
Alina Gorodetskaya <some.name.ag@gmail.com>
2020-10-26 14:35:33 +0100

    feat: add statuses to Teaser
    - add statuses array
    - add fixed height
    - implement 3 rows architecture where:
      1. `title` + `location`
      2. `subTitle` (or `details` if no `subTitle`)
      3. `details` (if there is `subTitle`) + statuses (always on the last row in the right)

--- src/components/Teaser/Teaser.tsx
+++ src/components/Teaser/Teaser.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 import { bem } from '../../utils';
 import { Text } from '../Text';
+import { Tooltip } from '../Tooltip';
 import styles from './Teaser.scss';
+
+type Status = {
+    label: string;
+    tooltip: string;
+};
 
 interface Props extends Omit<React.HTMLAttributes<HTMLDivElement>, 'title'> {
     /** The title of entity */
@@ -12,16 +18,28 @@ interface Props extends Omit<React.HTMLAttributes<HTMLDivElement>, 'title'> {
     location?: SingleReactNode;
     /** Details related to the entity */
     details?: SingleReactNode;
+    /** Statuses of entity containing status label and tooltip message */
+    statuses?: Status[];
 }
 
 const { block, elem } = bem('Teaser', styles);
 
 export const Teaser: React.FC<Props> = (props) => {
-    const { title, subTitle, location, details, ...rest } = props;
+    const { title, subTitle, location, details, statuses, ...rest } = props;
 
+    const subTitleElement = (
+        <Text inline context="accent" {...elem('subTitle', props)}>
+            {subTitle}
+        </Text>
+    );
+    const detailsElement = (
+        <Text inline context="muted" {...elem('details', props)}>
+            {details}
+        </Text>
+    );
     return (
         <div {...rest} {...block(props)}>
-            <div {...elem('titleLine', props)}>
+            <div {...elem('line', props)}>
                 <Text inline context="brand" {...elem('title', props)}>
                     {title}
                 </Text>
@@ -31,16 +49,31 @@ export const Teaser: React.FC<Props> = (props) => {
                     </Text>
                 )}
             </div>
-            {subTitle && (
-                <Text inline context="accent" {...elem('subTitle', props)}>
-                    {subTitle}
-                </Text>
-            )}
-            {details && (
-                <Text inline context="muted">
-                    {details}
-                </Text>
-            )}
+            <div {...elem('line', props)}>
+                {subTitle && subTitleElement}
+                {!subTitle && details && detailsElement}
+            </div>
+            <div {...elem('line', props)}>
+                {subTitle && details && detailsElement}
+                {statuses && statuses.length > 0 && (
+                    <div {...elem('statuses', props)}>
+                        {statuses.map(({ label, tooltip }, index) => (
+                            <span key={label}>
+                                <Tooltip content={tooltip}>
+                                    <Text inline context="muted" size="small">
+                                        {label}
+                                    </Text>
+                                </Tooltip>
+                                {index < statuses.length - 1 && (
+                                    <Text inline context="muted" size="small">
+                                        {' & '}
+                                    </Text>
+                                )}
+                            </span>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
--- stories/Teaser.tsx
+++ stories/Teaser.tsx
@@ -1,18 +1,56 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { object, text, withKnobs } from '@storybook/addon-knobs';
 import { Teaser } from '@textkernel/oneui';
 
 storiesOf('Molecules|Teaser', module)
     .addDecorator(withKnobs)
-    .add('Teaser', () => {
+    .add('With all fields', () => {
         const title = text('Title', 'My first job');
         return (
             <Teaser
                 title={<span title={title}>{title}</span>}
                 subTitle={text('SubTitle', 'Awsome inc.')}
                 location={text('Location', 'Melbourne')}
-                details={text('Details', 'It was posted here, yesterday')}
+                details={text('Details', 'It was posted here')}
+                statuses={[
+                    object('Viewed status', {
+                        label: 'Viewed',
+                        tooltip: 'Viewed one day ago',
+                    }),
+                    object('Imported status', {
+                        label: 'Imported',
+                        tooltip: 'Imported two days ago',
+                    }),
+                ]}
+            />
+        );
+    })
+    .add('With one status', () => {
+        const title = text('Title', 'My first job');
+        return (
+            <Teaser
+                title={<span title={title}>{title}</span>}
+                subTitle={text('SubTitle', 'Awsome inc.')}
+                location={text('Location', 'Melbourne')}
+                details={text('Details', 'It was posted here')}
+                statuses={[
+                    object('Viewed status', {
+                        label: 'Viewed',
+                        tooltip: 'Viewed one day ago',
+                    }),
+                ]}
+            />
+        );
+    })
+    .add('Without statuses', () => {
+        const title = text('Title', 'My first job');
+        return (
+            <Teaser
+                title={<span title={title}>{title}</span>}
+                subTitle={text('SubTitle', 'Awsome inc.')}
+                location={text('Location', 'Melbourne')}
+                details={text('Details', 'It was posted here')}
             />
         );
     });
