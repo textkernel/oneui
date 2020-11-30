e57e9282856e6d0b334b1df02d88ba491d48e8fa
Eszter Hofmann <hofmann@textkernel.nl>
2020-11-30 11:26:22 +0100

    lint

--- src/components/WeightedResultBar/__tests__/WeightedResultBarLoader.spec.js
+++ src/components/WeightedResultBar/__tests__/WeightedResultBarLoader.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import toJson from 'enzyme-to-json';
 import { WeightedResultBarLoader } from '../WeightedResultBarLoader';
 
 describe('WeightedResultBarLoader', () => {
