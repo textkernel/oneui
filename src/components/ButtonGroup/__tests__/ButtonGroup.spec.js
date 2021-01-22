72c961413ba6598892b7b787ddb7b7d575636c9a
Carlo Bernardini <bernardini@textkernel.nl>
2021-01-22 17:38:18 +0100

    fix(ButtonGroup): add classnames to style first and last button

--- src/components/ButtonGroup/__tests__/ButtonGroup.spec.js
+++ src/components/ButtonGroup/__tests__/ButtonGroup.spec.js
@@ -2,6 +2,8 @@ import React from 'react';
 import toJson from 'enzyme-to-json';
 import { ButtonGroup } from '../ButtonGroup';
 import { Button } from '../../Buttons';
+import { Dropdown } from '../../Dropdown';
+import { ListItem } from '../../List/ListItem';
 
 describe('<ButtonGroup> that renders a button', () => {
     it('should render default button correctly', () => {
@@ -19,6 +21,19 @@ describe('<ButtonGroup> that renders a button', () => {
             <ButtonGroup size="large" isBlock>
                 <Button>A button</Button>
                 <Button>Another button</Button>
+            </ButtonGroup>
+        );
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should support mixed element types', () => {
+        const wrapper = mount(
+            <ButtonGroup size="large" isBlock>
+                <Button>A button</Button>
+                <Button href="#">An anchor</Button>
+                <Dropdown button={<Button>A dropdown button</Button>} placement="bottom-end">
+                    <ListItem key="some-key">A list item</ListItem>
+                </Dropdown>
             </ButtonGroup>
         );
         expect(toJson(wrapper)).toMatchSnapshot();
