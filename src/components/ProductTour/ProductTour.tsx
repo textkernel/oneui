8c25a73fdb67a0a0ecbfce1c3937164432c66cb1
Eszter Hofmann <hofmann@textkernel.nl>
2021-02-24 12:03:43 +0100

    implementation

--- src/components/ProductTour/ProductTour.tsx
+++ src/components/ProductTour/ProductTour.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
 import { bem } from '../../utils';
+import { ENTER_KEY } from '../../constants';
+import { Button } from '../Buttons';
+import { Checkbox } from '../Checkbox';
+import { Modal, ModalProps } from '../Modal';
 import styles from './ProductTour.scss';
 
-interface Props extends React.HTMLAttributes<HTMLDivElement> {
+interface Props extends ModalProps {
     /** Should the tour be shown */
     isOpen: boolean;
     /** An array of ReactElement where each element is one slide */
-    children: React.ReactElement[];
+    children: React.ReactElement | React.ReactElement[];
     /** The label for the checkbox. If undefined or empty, checkbox will not be shown */
     checkboxLabel?: string;
-    /** Cancel button label - required if there are is then 1 slide */
+    /** Cancel button label - required if there is more then 1 slide */
     cancelLabel?: string;
-    /** Continue button label - required if there are is then 1 slide */
+    /** Continue button label - required if there is more then 1 slide */
     continueLabel?: string;
     /** Finish button label */
     finishLabel: string;
@@ -24,7 +28,124 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 const { block, elem } = bem('ProductTour', styles);
 
 export const ProductTour: React.FC<Props> = (props) => {
-    return null;
+    const {
+        isOpen,
+        children,
+        checkboxLabel,
+        cancelLabel,
+        continueLabel,
+        finishLabel,
+        onCancel,
+        onFinished,
+        ...rest
+    } = props;
+
+    /** index for currently shown slide; 0 indexed */
+    const [currentSlide, setCurrentSlide] = React.useState(0);
+    /** state of the checkbox */
+    const [isChecked, setIsChecked] = React.useState(false);
+    const nOfSlides = React.Children.count(children);
+
+    if (nOfSlides > 1 && (!cancelLabel || !continueLabel)) {
+        // eslint-disable-next-line no-console
+        console.warn(
+            'ProductTour component was called with incomplete props. When there are 2 or more slides in the tour, you need to include both `cancelLabel` and `continueLabel` as props'
+        );
+    }
+
+    const isLastSlide = currentSlide === nOfSlides - 1;
+
+    const handleCheckboxClick = () => {
+        setIsChecked(!isChecked);
+    };
+
+    const handleDotClick = (i: number) => {
+        setCurrentSlide(i);
+    };
+
+    const handleDotKeyDown = (e: React.KeyboardEvent<HTMLElement>, i: number) => {
+        if (e.key === ENTER_KEY) {
+            handleDotClick(i);
+        }
+    };
+
+    const handleCancel = () => {
+        onCancel?.(isChecked);
+    };
+
+    const handleNext = () => {
+        setCurrentSlide(currentSlide + 1);
+    };
+
+    const handleFinish = () => {
+        onFinished(isChecked);
+    };
+
+    const handledOverlayClick = () => {
+        if (isLastSlide) {
+            handleFinish();
+        } else {
+            handleCancel();
+        }
+    };
+
+    return (
+        <Modal
+            isOpen={isOpen}
+            isPositionFixed
+            {...rest}
+            {...block()}
+            onRequestClose={handledOverlayClick}
+        >
+            <section {...elem('slide')}>
+                {nOfSlides === 1 ? children : children[currentSlide]}
+            </section>
+            <div {...elem('footer')}>
+                <div {...elem('checkbox')}>
+                    {checkboxLabel && (
+                        <Checkbox id="hide" checked={isChecked} onChange={handleCheckboxClick}>
+                            {checkboxLabel}
+                        </Checkbox>
+                    )}
+                </div>
+                {nOfSlides > 1 && (
+                    <div>
+                        {[...Array(nOfSlides)].map((v, i) => {
+                            const isNotActive = i !== currentSlide;
+                            return (
+                                <div
+                                    key={i} // eslint-disable-line react/no-array-index-key
+                                    role="button"
+                                    tabIndex={isNotActive ? 0 : undefined}
+                                    onClick={isNotActive ? () => handleDotClick(i) : undefined}
+                                    onKeyDown={
+                                        isNotActive ? (e) => handleDotKeyDown(e, i) : undefined
+                                    }
+                                    {...elem('dot', { isActive: !isNotActive })}
+                                />
+                            );
+                        })}
+                    </div>
+                )}
+                <div {...elem('buttons')}>
+                    {isLastSlide ? (
+                        <Button context="primary" onClick={handleFinish}>
+                            {finishLabel}
+                        </Button>
+                    ) : (
+                        <>
+                            <Button context="link" onClick={handleCancel}>
+                                {cancelLabel || ''}
+                            </Button>
+                            <Button context="brand" onClick={handleNext}>
+                                {continueLabel || ''}
+                            </Button>
+                        </>
+                    )}
+                </div>
+            </div>
+        </Modal>
+    );
 };
 
 ProductTour.displayName = 'ProductTour';
--- stories/ProductTour.tsx
+++ stories/ProductTour.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
 import { ProductTour } from '@textkernel/oneui';
+import { slides } from '../src/components/ProductTour/__mocks__/slides';
 
 storiesOf('Organisms|ProductTour', module)
     .addDecorator(withKnobs)
@@ -13,65 +14,21 @@ storiesOf('Organisms|ProductTour', module)
             console.log(`onFinished was called with ${isChecked.toString()}`);
         };
 
-        const slides = [
-            <div>
-                <h1>Slide 1</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>,
-            <div>
-                <h1>Slide 2</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>,
-            <div>
-                <h1>Slide 3</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>,
-            <div>
-                <h1>Slide 4</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>,
-        ];
         const numberOfSlides = select('Number of slides', [1, 2, 3, 4], 4);
 
         return (
             <ProductTour
                 isOpen={boolean('Show tour', true)}
-                checkboxLabel={text('Checkbox label', "Don't show again")}
+                checkboxLabel={text('Checkbox label', 'Do not show again')}
                 cancelLabel={text('Cancel button label', 'Skip tour')}
-                continueLabel={text('Continue buttom label', 'Next')}
-                finishLabel={text('Finishe buttom label', 'Finish')}
+                continueLabel={text('Continue button label', 'Next')}
+                finishLabel={text('Finish button label', 'Finish')}
                 onCancel={handleCancel}
                 onFinished={handleFinished}
+                contentLabel={text('Title for screen readers', 'Super cool tour')}
+                ariaHideApp={false} // suppress consol warnings from Modal
             >
-                {slides.slice(0, numberOfSlides)}
+                {numberOfSlides === 1 ? slides[0] : slides.slice(0, numberOfSlides)}
             </ProductTour>
         );
     });
