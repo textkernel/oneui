72fab679469d65e6be5a21ddeacfa18ef7813858
Carlo Bernardini <bernardini@textkernel.nl>
2021-06-30 16:00:17 +0200

    feat(WeightedResultBar): merge WeightedResultBarLoader (#553)
    Merge WeightedResultBarLoader into WeightedResultBar. Instead of having a separate component, the loading state is now handled by the isLoading prop on WeightedResultBar. The random width of Content Placeholders is now fixed in between renders.
    
    BREAKING CHANGE: The WeightedResultBarLoader component is removed. Use the prop isLoading on WeightedResultBar instead.

--- stories/WeightedResultBar.tsx
+++ stories/WeightedResultBar.tsx
@@ -1,30 +1,32 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, number } from '@storybook/addon-knobs';
-import { WeightedResultBar, WeightedResultBarLoader, Button } from '@textkernel/oneui';
+import { withKnobs, number, boolean } from '@storybook/addon-knobs';
+import { WeightedResultBar, Button } from '@textkernel/oneui';
 
 storiesOf('Molecules|WeightedResultBar', module)
     .addDecorator(withKnobs)
     .add('WeightedResultBar', () => {
+        const loading = boolean('Loading', false);
         return (
             <div style={{ width: 500 }}>
                 <WeightedResultBar
                     percentage={number('Percentage', 100)}
                     count={number('Count', 123)}
                     context="primary"
+                    isLoading={loading}
                 >
                     Repair and Maintenance Technician
                 </WeightedResultBar>
-                <WeightedResultBar percentage={76} count={94}>
+                <WeightedResultBar percentage={76} count={94} isLoading={loading}>
                     Sales Manager
                 </WeightedResultBar>
-                <WeightedResultBar percentage={64} count={79}>
+                <WeightedResultBar percentage={64} count={79} isLoading={loading}>
                     Software Engineer
                 </WeightedResultBar>
-                <WeightedResultBar percentage={64} count={79}>
+                <WeightedResultBar percentage={64} count={79} isLoading={loading}>
                     Operations Manager
                 </WeightedResultBar>
-                <WeightedResultBar percentage={58} count={72}>
+                <WeightedResultBar percentage={58} count={72} isLoading={loading}>
                     Business Development Manager
                 </WeightedResultBar>
             </div>
@@ -62,17 +64,6 @@ storiesOf('Molecules|WeightedResultBar', module)
                 <WeightedResultBar percentage={58} count={<ClickableCount count={72} />}>
                     Business Development Manager
                 </WeightedResultBar>
-            </div>
-        );
-    })
-    .add('WeightedResultBarLoader', () => {
-        return (
-            <div style={{ width: 500 }}>
-                <WeightedResultBarLoader />
-                <WeightedResultBarLoader />
-                <WeightedResultBarLoader />
-                <WeightedResultBarLoader />
-                <WeightedResultBarLoader />
             </div>
         );
     });
