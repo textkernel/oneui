3254c8e16d020265b61b724cf4a68d7ce2eb73d2
Eszter Hofmann <hofmann@textkernel.nl>
2021-09-14 16:55:46 +0200

    add tests

---
+++ src/components/AlertConfirm/__tests__/Confirm.spec.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import toJson from 'enzyme-to-json';
+import { Confirm } from '../AlertConfirm';
+
+describe('Confirm', () => {
+    const mockOnAccept = jest.fn();
+    const mockOnCancel = jest.fn();
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = mount(
+            <Confirm
+                isOpen
+                acceptButton={{ onClick: mockOnAccept, label: 'OK' }}
+                cancelButton={{ onClick: mockOnCancel, label: 'Cancel' }}
+                ariaHideApp={false}
+            >
+                Body of the confirm
+            </Confirm>
+        );
+    });
+
+    it('should render correctly', () => {
+        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.find('Heading')).toHaveLength(0);
+        expect(wrapper.find('Button')).toHaveLength(2);
+    });
+    it('should render correctly with title', () => {
+        wrapper.setProps({ title: 'Title' });
+        wrapper.update();
+
+        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.find('Heading')).toHaveLength(1);
+    });
+    it('should call onAccept cb when button is clicked', () => {
+        wrapper.find('Button').at(1).simulate('click');
+        expect(mockOnAccept).toHaveBeenCalledTimes(1);
+    });
+    it('should call onAccept cb when button is clicked', () => {
+        wrapper.find('Button').at(0).simulate('click');
+        expect(mockOnCancel).toHaveBeenCalledTimes(1);
+    });
+});
