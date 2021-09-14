24c55c86e6c7d6ccdef1ea32bf8ba620e4766623
Eszter Hofmann <hofmann@textkernel.nl>
2021-09-14 10:56:00 +0200

    don't use example component in story

--- stories/Dialogs.js
+++ stories/Dialogs.js
@@ -3,61 +3,65 @@ import { storiesOf } from '@storybook/react';
 import { text, withKnobs } from '@storybook/addon-knobs';
 import { Alert, Button } from '@textkernel/oneui';
 
-const ExampleAlert = (props) => {
-    const { children, type, ...rest } = props;
-    const [showAlert, setShowAlert] = React.useState(false);
-
-    const handleButtonClick = () => {
-        setShowAlert(true);
-    };
-
-    const handleAccept = () => {
-        setShowAlert(false);
-        console.log('Dialog has been accepted');
-    };
-
-    const handleCancel = () => {
-        setShowAlert(false);
-        console.log('Dialog has been cancelled');
-    };
-
-    const buttonText =
-        type === 'alert' ? 'Click me to see an alert' : 'Click me to see a confirm dialog';
-    const alertProps = { ...rest, showAlert, onAccept: handleAccept };
-    if (type === 'confirm') {
-        alertProps.onCancel = handleCancel;
-    }
-
-    return (
-        <>
-            <Button onClick={handleButtonClick}>{buttonText}</Button>
-            <Alert {...alertProps}>{children}</Alert>
-        </>
-    );
-};
-
 storiesOf('Molecules|Dialogs', module)
     .addDecorator(withKnobs)
     .add('Alert', () => {
+        const [showAlert, setShowAlert] = React.useState(false);
+
+        const handleButtonClick = () => {
+            setShowAlert(true);
+        };
+
+        const handleAccept = () => {
+            setShowAlert(false);
+            console.log('Dialog has been accepted');
+        };
+
         return (
-            <ExampleAlert
-                acceptLabel={text('Accept button text', 'Dismiss')}
-                title={text('Title', 'Hi there!')}
-                type="alert"
-            >
-                {text('Content', 'This is some information for you')}
-            </ExampleAlert>
+            <>
+                <Button onClick={handleButtonClick}>Click me to see an alert</Button>
+                <Alert
+                    showAlert={showAlert}
+                    onAccept={handleAccept}
+                    acceptLabel={text('Accept button text', 'Ok')}
+                    title={text('Title', 'Hi there!')}
+                >
+                    {text('Content', 'This is some information for you.')}
+                </Alert>
+            </>
         );
     })
     .add('Confirm', () => {
+        const [showAlert, setShowAlert] = React.useState(false);
+
+        const handleButtonClick = () => {
+            setShowAlert(true);
+        };
+
+        const handleAccept = () => {
+            setShowAlert(false);
+            console.log('Dialog has been accepted');
+        };
+
+        const handleCancel = () => {
+            setShowAlert(false);
+            console.log('Dialog has been cancelled');
+        };
+
         return (
-            <ExampleAlert
-                acceptLabel={text('Accept button text', 'Ok')}
-                cancelLabel={text('Accept button text', 'Cancel')}
-                title={text('Title', 'Hi there!')}
-                type="confirm"
-            >
-                {text('Content', 'This is some information for you. Do you want to proceed?')}
-            </ExampleAlert>
+            <>
+                <Button onClick={handleButtonClick}>Click me to see an alert</Button>
+                <Alert
+                    showAlert={showAlert}
+                    onAccept={handleAccept}
+                    onCancel={handleCancel}
+                    acceptLabel={text('Accept button text', 'Ok')}
+                    cancelLabel={text('Accept button text', 'Cancel')}
+                    title={text('Title', 'Hi there!')}
+                    type="confirm"
+                >
+                    {text('Content', 'This is some information for you. Do you want to proceed?')}
+                </Alert>
+            </>
         );
     });
