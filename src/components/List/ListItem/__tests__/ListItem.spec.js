0118ed39f75227800554832d2f33de99f898c8c0
Eszter Hofmann <hofmann@textkernel.nl>
2021-10-15 12:18:03 +0200

    add test for ListItem changes

--- src/components/List/ListItem/__tests__/ListItem.spec.js
+++ src/components/List/ListItem/__tests__/ListItem.spec.js
@@ -30,12 +30,29 @@ describe('ListItem component', () => {
 
         expect(wrapper.find('.ListItem--clickable')).toHaveLength(1);
     });
-
     it('should call onClick function when clicked', () => {
         const onClick = jest.fn();
         const wrapper = mount(<ListItem onClick={onClick}>An item</ListItem>);
         wrapper.find('div').simulate('click');
 
         expect(onClick).toHaveBeenCalledTimes(1);
+    });
+    it('should render correctly when disabled', () => {
+        const wrapper = mount(<ListItem disabled>An item</ListItem>);
+        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.find('li').prop('className')).toBe('ListItem ListItem--disabled');
+        expect(wrapper.find('li').prop('disabled')).toBeUndefined();
+    });
+    it('should set disabled prop on li when required', () => {
+        const wrapper = mount(
+            <ListItem disabled passDisabledToLi>
+                An item
+            </ListItem>
+        );
+        expect(wrapper.find('li').prop('disabled')).toBeTruthy();
+
+        wrapper.setProps({ disabled: false });
+        wrapper.update();
+        expect(wrapper.find('li').prop('disabled')).toBeFalsy();
     });
 });
