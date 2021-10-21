e3b5e231bdc5eed4ae4c152e72de227b84ae57c5
Konstantin Lebejko <liabeika@textkernel.nl>
2021-10-21 17:53:16 +0200

    fix: removed unused inport

--- src/components/Dropdown/__tests__/Dropdown.spec.js
+++ src/components/Dropdown/__tests__/Dropdown.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 import toJson from 'enzyme-to-json';
 import { Dropdown } from '../Dropdown';
 import { Button } from '../../Buttons';
