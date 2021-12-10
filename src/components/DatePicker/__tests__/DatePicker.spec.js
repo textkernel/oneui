6547af5f8fb0fbfc2cb5b846225866eedc2872da
Eszter Hofmann <hofmann@textkernel.nl>
2021-12-10 10:49:52 +0100

    extend tests a bit

--- src/components/DatePicker/__tests__/DatePicker.spec.js
+++ src/components/DatePicker/__tests__/DatePicker.spec.js
@@ -5,12 +5,19 @@ import { DatePicker } from '../DatePicker';
 describe('DatePicker', () => {
     const YEARS = [2016, 2025];
     let wrapper;
+    const handleChange = jest.fn();
 
     beforeEach(() => {
-        wrapper = mount(<DatePicker yearsRange={YEARS} />);
+        wrapper = mount(<DatePicker yearsRange={YEARS} onChange={handleChange} />);
     });
 
     it('should render correctly', () => {
         expect(toJson(wrapper)).toMatchSnapshot();
+    });
+    it('should call onChange function with Date object', () => {
+        // A simplistic regression test for the library
+        wrapper.find('input').simulate('change', { target: { value: '11/1/2021' } });
+        expect(handleChange).toHaveBeenCalledTimes(1);
+        expect(handleChange.mock.calls[0][0] instanceof Date).toBeTruthy();
     });
 });
