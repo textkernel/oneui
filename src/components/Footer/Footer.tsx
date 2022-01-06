f317524e7ee8a86e63294486f770e8c76a3372ac
Alex <alex@antipin.com>
2022-01-06 09:33:44 +0100

    fix(Footer): add year prop to Footer component to make it deterministic (#210)
    Hardcoded `new Date().getFullYear();` caused tests to fail each beginning of a new year. Added prop for specifying year, defaulting to current year.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,18 +7,21 @@ import { LogoTextkernel } from '../Icon';
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
     /** supply copyright text ready to be rendered instead of the default one */
     copyright?: ReactNode;
+    /** Current year to be rendered as a part of copyright note */
+    year?: number;
     /** Node(s) to be rendered on the right side of the header */
     children?: ReactNode;
 }
 
+const currentYear = new Date().getFullYear();
 const { block, elem } = bem('Footer', styles);
 
 export const Footer: React.FC<Props> = (props) => {
-    const { copyright, children, ...rest } = props;
+    const { copyright, year = currentYear, children, ...rest } = props;
 
     const tkCopyright = (
         <span>
-            {`\u00a9 ${new Date().getFullYear()} `}
+            {year && `\u00a9 ${year} `}
             <LogoTextkernel {...elem('logo', props)} margin="left" />
         </span>
     );
