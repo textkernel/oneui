32ce67eeee463945913694b8e78ded9b5d49b4ec
Eszter Hofmann <hofmann@textkernel.nl>
2022-04-15 14:27:46 +0200

    hook for listenting to outer clicks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useDebounce } from './useDebounce';
 export { usePrevious } from './usePrevious';
 export { useBrowserTabVisibilityChange } from './useBrowserTabVisibilityChange';
+export { useOuterClick } from './useOuterClick';
