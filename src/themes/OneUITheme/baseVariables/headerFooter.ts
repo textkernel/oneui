f63252059e738c120603421766073ca5fee31e8f
Oleksii Mukiienko <mukiienko@textkernel.nl>
2022-04-22 12:13:30 +0200

    test: removed default

--- src/themes/OneUITheme/baseVariables/headerFooter.ts
+++ src/themes/OneUITheme/baseVariables/headerFooter.ts
@@ -1,14 +1,14 @@
 export const headerFooterVariables = {
     // Header variables
-    '--header-background-color': 'var(--color-background) !default',
-    '--header-foreground-color': 'var(--color-foreground) !default',
+    '--header-background-color': 'var(--color-background)',
+    '--header-foreground-color': 'var(--color-foreground)',
     '--header-height': '50px',
     '--header-content-height': 'calc(var(--header-height) - 10px)',
     // Footer variables
-    '--footer-background-color': 'var(--color-accent) !default',
-    '--footer-foreground-color': 'var(--color-background) !default',
-    '--footer-link-color': 'var(--color-neutral-60) !default',
-    '--footer-height': '100px !default',
+    '--footer-background-color': 'var(--color-accent)',
+    '--footer-foreground-color': 'var(--color-background)',
+    '--footer-link-color': 'var(--color-neutral-60)',
+    '--footer-height': '100px',
     // navigation bar
-    '--navigation-height': '48px !default',
+    '--navigation-height': '48px',
 };
