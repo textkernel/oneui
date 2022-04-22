dafa6dc8bec7c45d450ab14d8c0af9d8793fdf51
Eszter Hofmann <hofmann@textkernel.nl>
2022-04-22 11:42:09 +0200

    fix css in Checkbox with none-string child

--- stories/Checkbox.tsx
+++ stories/Checkbox.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean, text, withKnobs } from '@storybook/addon-knobs';
-import { Checkbox } from '@textkernel/oneui';
+import { Checkbox, Text } from '@textkernel/oneui';
 
 storiesOf('Molecules|Checkbox', module)
     .addDecorator(withKnobs)
@@ -24,5 +24,16 @@ storiesOf('Molecules|Checkbox', module)
             defaultChecked
         >
             {text('Checkbox label', 'Select me!')}
+        </Checkbox>
+    ))
+    .add('Checkbox with not just string as label', () => (
+        <Checkbox
+            disabled={boolean('Disabled', false)}
+            id={text('Id', 'checkbox-1')}
+            onChange={(e) => console.log('Checkbox state changed', e)}
+        >
+            <Text inline style={{ color: 'turquoise' }} className="test-class">
+                {text('Checkbox label', 'Select me!')}
+            </Text>
         </Checkbox>
     ));
