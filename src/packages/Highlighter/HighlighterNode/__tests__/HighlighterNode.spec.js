a8a1dae480c8d7beb2d0ea1eb33de75653fa581f
ahalyha <halyha@textkernel.nl>
2022-05-25 16:22:52 +0300

    Revert "style: change new colors"
    This reverts commit 558509bd5ee07e4465c4f06074948f5c71ff3b4c.

--- src/packages/Highlighter/HighlighterNode/__tests__/HighlighterNode.spec.js
+++ src/packages/Highlighter/HighlighterNode/__tests__/HighlighterNode.spec.js
@@ -18,15 +18,15 @@ describe('HighlighterNode', () => {
     describe('#generateColor()', () => {
         it('should generate color', () => {
             const color = HighlighterNode.generateColor(1);
-            expect(color).toMatch('#D6F481');
+            expect(color).toMatch('#90EE90');
         });
 
-        it('should generate 12 distinct colors', () => {
+        it('should generate 11 distinct colors', () => {
             const numbers = Array.from({ length: 100 }, (e, i) => i);
             const colors = numbers.map((i) => HighlighterNode.generateColor(i));
             const amountDifferentColors = intersection(colors).length;
 
-            expect(amountDifferentColors).toBe(12);
+            expect(amountDifferentColors).toBe(11);
         });
     });
     describe('#applyStylesForNode()', () => {
