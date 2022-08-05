2489cd6de975d79c43f6ca64e05506c7af877261
Eszter Hofmann <hofmann@textkernel.nl>
2022-08-05 16:46:56 +0200

    Footer

--- stories/Footer.tsx
+++ stories/Footer.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import { text, withKnobs } from '@storybook/addon-knobs';
 import { Footer, Link } from '@textkernel/oneui';
 
-storiesOf('Molecules/Footer', module)
-    .addDecorator(withKnobs)
-    .add('Footer', () => (
-        <Footer copyright={text('Alternative copyright', '') || undefined}>
-            {text('Left side', 'This is a placeholder for children')}
-            <Link href="/"> and a link</Link>
-        </Footer>
-    ));
+export default {
+    title: 'Molecules/Footer',
+    component: Footer,
+};
+
+export const _Footer = ({ children, ...args }) => (
+    <Footer {...args}>
+        {children}
+        <Link href="/"> and a link</Link>
+    </Footer>
+);
+_Footer.args = {
+    children: 'This is a placeholder for children',
+};
