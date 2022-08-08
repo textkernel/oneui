e11f1ee8d9620add5db658d0d08bedde35d3bd43
Eszter Hofmann <hofmann@textkernel.nl>
2022-08-08 11:42:22 +0200

    Link

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -2,20 +2,18 @@ import * as React from 'react';
 import { bem } from '../../utils';
 import styles from './Link.scss';
 
-interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+export interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
     /** Link content */
     children: NotEmptyReactNode;
     /** Color context */
     context?: 'brand' | 'muted';
-    /** Ref to access the anchor */
-    ref?: React.RefObject<HTMLAnchorElement>;
     /** Do not underline text on hover */
     dontDecorateOnHover?: boolean;
 }
 
 const { block } = bem('Link', styles);
 
-export const Link: React.FC<Props> = React.forwardRef((props, ref) => {
+export const Link = React.forwardRef<HTMLAnchorElement, Props>((props, ref) => {
     const { children, context, dontDecorateOnHover, ...rest } = props;
     return (
         <a ref={ref} {...rest} {...block(props)}>
