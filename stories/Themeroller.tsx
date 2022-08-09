7041f9938a56a160eb6aae112f9a52ebb4fb1d78
Eszter Hofmann <hofmann@textkernel.nl>
2022-08-09 13:48:34 +0200

    Themeroller

--- stories/Themeroller.tsx
+++ stories/Themeroller.tsx
@@ -1,33 +1,39 @@
 import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
 import OneUI, { Themeroller, THEMEROLLER_CONFIG } from '@textkernel/oneui';
 
-storiesOf('Theme/Themeroller', module)
-    .addDecorator(withKnobs)
-    .add('Theme builder', () => {
-        const handleChange = (themeResult) => {
-            sessionStorage.setItem(OneUI.THEME_ELEMENT_ID, JSON.stringify(themeResult));
-        };
-        let activeTheme;
-        let hasActiveTheme;
-        try {
-            activeTheme = JSON.parse(sessionStorage.getItem(OneUI.THEME_ELEMENT_ID) || '' || '');
-            hasActiveTheme = Object.keys(activeTheme.theme.cssVariables).length;
-        } catch (err) {
-            console.error(err);
-        }
-        return (
-            <Themeroller
-                inputLabel="Theme name"
-                resetLabel="Reset to defaults"
-                resetActiveLabel={hasActiveTheme ? 'Revert to active theme' : undefined}
-                fileLabel="Import file"
-                downloadLabel="Download"
-                downloadTooltipLabel="Please specify the Theme name"
-                config={THEMEROLLER_CONFIG}
-                activeTheme={activeTheme}
-                onChange={handleChange}
-            />
-        );
-    });
+export default {
+    title: 'Theme/Themeroller',
+    component: Themeroller,
+};
+
+const handleChange = (themeResult) => {
+    sessionStorage.setItem(OneUI.THEME_ELEMENT_ID, JSON.stringify(themeResult));
+};
+
+export const ThemeBuilder = (args) => {
+    let activeTheme;
+    let hasActiveTheme;
+    try {
+        activeTheme = JSON.parse(sessionStorage.getItem(OneUI.THEME_ELEMENT_ID) || '' || '');
+        hasActiveTheme = Object.keys(activeTheme.theme.cssVariables).length;
+    } catch (err) {
+        console.error(err);
+    }
+    return (
+        <Themeroller
+            {...args}
+            resetActiveLabel={hasActiveTheme ? 'Revert to active theme' : undefined}
+            activeTheme={activeTheme}
+        />
+    );
+};
+ThemeBuilder.storyName = 'Theme builder';
+ThemeBuilder.args = {
+    inputLabel: 'Theme name',
+    resetLabel: 'Reset to defaults',
+    fileLabel: 'Import file',
+    downloadLabel: 'Download',
+    downloadTooltipLabel: 'Please specify the Theme name',
+    config: THEMEROLLER_CONFIG,
+    onChange: handleChange,
+};
