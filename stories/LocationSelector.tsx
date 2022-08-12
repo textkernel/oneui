1dfa11e11a2469cd43f3f3aeea99ab1cc8ea7805
Eszter Hofmann <hofmann@textkernel.nl>
2022-08-12 12:46:06 +0200

    LocationSelector

--- stories/LocationSelector.tsx
+++ stories/LocationSelector.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import { number, text, select, boolean, withKnobs } from '@storybook/addon-knobs';
-import { LocationSelector } from '@textkernel/oneui';
+import { Story } from '@storybook/react';
+import { LocationSelector, LocationSelectorProps } from '@textkernel/oneui';
 import { ensureApiKey } from './utils/ensureApiKey';
-import { StoreInjector } from '../src/packages/storybook/withStore';
 import NL_PATHS from './static/gadm36_NLD_0.json';
 
 const selectedLocations = [
@@ -27,234 +25,160 @@ const selectedLocations = [
     },
 ];
 
-storiesOf('Organisms/LocationSelector', module)
-    .addDecorator(withKnobs)
-    .addParameters(
-        StoreInjector.withStore({
-            selectedLocations: [],
-            radiusUnits: 'km',
-            withoutLocationCards: false,
-        })
-    )
-    .add('Basic component', () => {
-        const apiKey = ensureApiKey();
+export default {
+    title: 'Organisms/LocationSelector',
+    component: LocationSelector,
+    argTypes: {
+        useDefaultHighlight: { control: 'boolean' },
+    },
+};
 
-        return (
-            <LocationSelector
-                className="test-class"
-                apiKey={apiKey}
-                selectedLocations={selectedLocations}
-                country={text('country', 'NL')}
-                language={text('Language', 'EN')}
-                region={text('Region', 'NL')}
-                initialMapAddress={text('Initial map address', '')}
-                radiusUnits="km"
-                renderRadiusLabel={(r) => `+ ${r} km`}
-                hasRadius={boolean('Use radius search', true)}
-                minRadius={number('Min radius', 1)}
-                maxRadius={number('Max radius', 100)}
-                radiusStep={number('Radius steps', 1)}
-                modalContentLabel={text(
-                    'Modal content title for screen readers',
-                    'Location selection'
-                )}
-                inputPlaceholder={text('Input placeholder', 'Location...')}
-                modalInputPlaceholder={text('Input placeholder inside the modal', '')}
-                noSuggestionsPlaceholder="noSuggestionsPlaceholder"
-                selectionPlaceholder="selectionPlaceholder"
-                doneLabel={text('Label for Done button', 'Done')}
-                clearLabel={text('Label for clear button', 'Clear')}
-                onAddLocation={(location) =>
-                    console.log('onAddLocation was called with:', location)
-                }
-                onUpdateLocation={() => console.log('onUpdateLocation was called')}
-                onRemoveLocation={() => console.log('onRemoveLocation was called')}
-                onRemoveAllLocations={() => console.log('onRemoveAllLocations was called')}
-                onLocationAutocompleteError={() =>
-                    console.log('onLocationAutocompleteError was called')
-                }
-                defaultHighlight={
-                    boolean('Add default highlight area', true)
-                        ? (NL_PATHS as GeoJSON.GeoJsonObject)
-                        : undefined
-                }
-            />
-        );
-    })
-    // eslint-disable-next-line
-    .add('Example single select implementation', (_args: unknown, { parameters }: any) => {
-        const apiKey = ensureApiKey();
-        const store = parameters.getStore();
+export const Basic = ({ useDefaultHighlight, ...args }) => {
+    const apiKey = ensureApiKey();
 
-        const handleAddLocation = (location) => {
-            console.log('onAddLocation was called with:', location);
-            store.set({ selectedLocations: [location] });
-        };
+    return (
+        <LocationSelector
+            {...(args as LocationSelectorProps)}
+            apiKey={apiKey}
+            defaultHighlight={
+                args.useDefaultHighlight ? (NL_PATHS as GeoJSON.GeoJsonObject) : undefined
+            }
+        />
+    );
+};
+Basic.args = {
+    className: 'test-class',
+    selectedLocations,
+    country: 'NL',
+    language: 'EN',
+    region: 'NL',
+    initialMapAddress: '',
+    radiusUnits: 'km',
+    renderRadiusLabel: (r) => `+ ${r} km`,
+    hasRadius: true,
+    minRadius: 1,
+    maxRadius: 100,
+    radiusStep: 1,
+    modalContentLabel: 'Location selection',
+    inputPlaceholder: 'Location...',
+    modalInputPlaceholder: '',
+    noSuggestionsPlaceholder: 'noSuggestionsPlaceholder',
+    selectionPlaceholder: 'selectionPlaceholder',
+    doneLabel: 'Done',
+    clearLabel: 'Clear',
+    useDefaultHighlight: true,
+};
 
-        const handleUpdateLocation = (id, radius) => {
-            console.log('onUpdateLocation was called', id, radius);
-            const newSelection = store.get('selectedLocations').map((loc) => {
-                if (loc.id === id) {
-                    loc.radius = radius; // eslint-disable-line no-param-reassign
-                }
-                return loc;
-            });
-            store.set({ selectedLocations: newSelection });
-        };
+const Template: Story<LocationSelectorProps & { useDefaultHighlight: boolean }> = ({
+    useDefaultHighlight,
+    ...args
+}) => {
+    const apiKey = ensureApiKey();
+    const [selectedLocations, setSelectedLocations] = React.useState<any[]>([]);
 
-        const handleRemoveLocation = (id) => {
-            console.log('onRemoveLocation was called', id);
-            store.set({
-                selectedLocations: store.get('selectedLocations').filter((l) => l.id !== id),
-            });
-        };
+    const handleAddLocation = (location) => {
+        console.log('onAddLocation was called with:', location);
+        setSelectedLocations([location]);
+    };
 
-        const handleRemoveAllLocations = () => {
-            console.log('onRemoveAllLocations was called');
-            store.set({ selectedLocations: [] });
-        };
+    const handleUpdateLocation = (id, radius) => {
+        console.log('onUpdateLocation was called', id, radius);
+        const newSelection = selectedLocations.map((loc) => {
+            if (loc.id === id) {
+                loc.radius = radius; // eslint-disable-line no-param-reassign
+            }
+            return loc;
+        });
+        setSelectedLocations(newSelection);
+    };
 
-        const getPlaceholder = () => {
-            const selection = store.get('selectedLocations');
-            const selectedLocationsText = selection
-                .map((item) => item.structured_formatting.main_text)
-                .join(', ');
-            return selectedLocationsText || null;
-        };
+    const handleRemoveLocation = (id) => {
+        console.log('onRemoveLocation was called', id);
+        setSelectedLocations(selectedLocations.filter((l) => l.id !== id));
+    };
 
-        return (
-            <LocationSelector
-                apiKey={apiKey}
-                withoutLocationCards
-                selectedLocations={store.get('selectedLocations')}
-                region={text('Region', 'NL')}
-                country={text('country', 'NL')}
-                language={text('Language', 'EN')}
-                initialMapAddress={text('Initial map address', 'Netherlands')}
-                radiusUnits={select('Radius units', ['km', 'mi'], 'km')}
-                renderRadiusLabel={(r) => `+ ${r} km`}
-                hasRadius={boolean('Has radius', true)}
-                minRadius={number('Min radius', 1)}
-                maxRadius={number('Max radius', 100)}
-                radiusDefaultValue={number('Radius default value', 5)}
-                radiusStep={number('Radius steps', 1)}
-                placeTypes={['(regions)']}
-                openOnEnterPress={boolean('Open on Enter press', true)}
-                shouldGetAddressInfo={boolean('Fetch address information', false)}
-                showCountryInSuggestions={boolean('Show country in suggestions', true)}
-                modalContentLabel={text(
-                    'Modal content title for screen readers',
-                    'Location selection'
-                )}
-                inputPlaceholder={text('Input placeholder', 'Location...')}
-                modalInputPlaceholder={text('Input placeholder inside the modal', '')}
-                noSuggestionsPlaceholder="noSuggestionsPlaceholder"
-                selectionPlaceholder={getPlaceholder()}
-                doneLabel={text('Label for Done button', 'Done')}
-                clearLabel={text('Label for clear button', 'Clear')}
-                onAddLocation={handleAddLocation}
-                onUpdateLocation={handleUpdateLocation}
-                onRemoveLocation={handleRemoveLocation}
-                onRemoveAllLocations={handleRemoveAllLocations}
-                onBlur={() => {
-                    console.log('onBlur was called');
-                }}
-                onLocationAutocompleteError={() =>
-                    console.log('onLocationAutocompleteError was called')
-                }
-                defaultHighlight={
-                    boolean('Add default highlight area', true)
-                        ? (NL_PATHS as GeoJSON.GeoJsonObject)
-                        : undefined
-                }
-            />
-        );
-    })
-    // eslint-disable-next-line
-    .add('Example multi select implementation', (_args: unknown, { parameters }: any) => {
-        const apiKey = ensureApiKey();
-        const store = parameters.getStore();
+    const handleRemoveAllLocations = () => {
+        console.log('onRemoveAllLocations was called');
+        setSelectedLocations([]);
+    };
 
-        const handleAddLocation = (location) => {
-            console.log('onAddLocation was called with:', location);
-            store.set({ selectedLocations: [...store.get('selectedLocations'), location] });
-        };
+    const getPlaceholder = () => {
+        const selectedLocationsText = selectedLocations
+            .map((item) => item.structured_formatting.main_text)
+            .join(', ');
+        return selectedLocationsText || undefined;
+    };
 
-        const handleUpdateLocation = (id, radius) => {
-            console.log('onUpdateLocation was called', id, radius);
-            const newSelection = store.get('selectedLocations').map((loc) => {
-                if (loc.id === id) {
-                    loc.radius = radius; // eslint-disable-line no-param-reassign
-                }
-                return loc;
-            });
-            store.set({ selectedLocations: newSelection });
-        };
+    return (
+        <LocationSelector
+            {...args}
+            apiKey={apiKey}
+            selectedLocations={selectedLocations}
+            selectionPlaceholder={getPlaceholder()}
+            onAddLocation={handleAddLocation}
+            onUpdateLocation={handleUpdateLocation}
+            onRemoveLocation={handleRemoveLocation}
+            onRemoveAllLocations={handleRemoveAllLocations}
+            onBlur={() => {
+                console.log('onBlur was called');
+            }}
+            onLocationAutocompleteError={() =>
+                console.log('onLocationAutocompleteError was called')
+            }
+            defaultHighlight={useDefaultHighlight ? (NL_PATHS as GeoJSON.GeoJsonObject) : undefined}
+        />
+    );
+};
 
-        const handleRemoveLocation = (id) => {
-            console.log('onRemoveLocation was called', id);
-            store.set({
-                selectedLocations: store.get('selectedLocations').filter((l) => l.id !== id),
-            });
-        };
+export const SingleSelect = Template.bind({});
+SingleSelect.args = {
+    withoutLocationCards: true,
+    region: 'NL',
+    country: 'NL',
+    language: 'EN',
+    initialMapAddress: 'Netherlands',
+    radiusUnits: 'km',
+    renderRadiusLabel: (r) => `+ ${r} km`,
+    hasRadius: true,
+    minRadius: 1,
+    maxRadius: 100,
+    radiusDefaultValue: 5,
+    radiusStep: 1,
+    placeTypes: ['(regions)'],
+    openOnEnterPress: true,
+    shouldGetAddressInfo: false,
+    showCountryInSuggestions: true,
+    modalContentLabel: 'Location selection',
+    inputPlaceholder: 'Location...',
+    modalInputPlaceholder: '',
+    noSuggestionsPlaceholder: 'noSuggestionsPlaceholder',
+    doneLabel: 'Done',
+    clearLabel: 'Clear',
+    useDefaultHighlight: true,
+};
 
-        const handleRemoveAllLocations = () => {
-            console.log('onRemoveAllLocations was called');
-            store.set({ selectedLocations: [] });
-        };
-
-        const getPlaceholder = () => {
-            const selection = store.get('selectedLocations');
-            const selectedLocationsText = selection
-                .map((item) => item.structured_formatting.main_text)
-                .join(', ');
-            return selectedLocationsText || null;
-        };
-
-        return (
-            <LocationSelector
-                apiKey={apiKey}
-                selectedLocations={store.get('selectedLocations')}
-                region={text('Region', 'NL')}
-                country={text('country', 'NL')}
-                language={text('Language', 'EN')}
-                initialMapAddress={text('Initial map address', 'Netherlands')}
-                radiusUnits={select('Radius units', ['km', 'mi'], 'km')}
-                renderRadiusLabel={(r) => `+ ${r} km`}
-                hasRadius={boolean('Use radius search', true)}
-                minRadius={number('Min radius', 1)}
-                maxRadius={number('Max radius', 100)}
-                radiusDefaultValue={number('Radius default value', 5)}
-                radiusStep={number('Radius steps', 1)}
-                placeTypes={['(regions)']}
-                openOnEnterPress={boolean('Open on Enter press', true)}
-                shouldGetAddressInfo={boolean('Fetch address information', false)}
-                showCountryInSuggestions={boolean('Show country in suggestions', true)}
-                modalContentLabel={text(
-                    'Modal content title for screen readers',
-                    'Location selection'
-                )}
-                inputPlaceholder={text('Input placeholder', 'Location...')}
-                modalInputPlaceholder={text('Input placeholder inside the modal', '')}
-                noSuggestionsPlaceholder="noSuggestionsPlaceholder"
-                selectionPlaceholder={getPlaceholder()}
-                doneLabel={text('Label for Done button', 'Done')}
-                clearLabel={text('Label for clear button', 'Clear')}
-                onAddLocation={handleAddLocation}
-                onUpdateLocation={handleUpdateLocation}
-                onRemoveLocation={handleRemoveLocation}
-                onRemoveAllLocations={handleRemoveAllLocations}
-                onBlur={() => {
-                    console.log('onBlur was called');
-                }}
-                onLocationAutocompleteError={() =>
-                    console.log('onLocationAutocompleteError was called')
-                }
-                defaultHighlight={
-                    boolean('Add default highlight area', true)
-                        ? (NL_PATHS as GeoJSON.GeoJsonObject)
-                        : undefined
-                }
-            />
-        );
-    });
+export const MultiSelect = Template.bind({});
+MultiSelect.args = {
+    region: 'NL',
+    country: 'NL',
+    language: 'EN',
+    initialMapAddress: 'Netherlands',
+    radiusUnits: 'km',
+    renderRadiusLabel: (r) => `+ ${r} km`,
+    hasRadius: true,
+    minRadius: 1,
+    maxRadius: 100,
+    radiusDefaultValue: 5,
+    radiusStep: 1,
+    placeTypes: ['(regions)'],
+    openOnEnterPress: true,
+    shouldGetAddressInfo: false,
+    showCountryInSuggestions: true,
+    modalContentLabel: 'Location selection',
+    inputPlaceholder: 'Location...',
+    modalInputPlaceholder: '',
+    noSuggestionsPlaceholder: 'noSuggestionsPlaceholder',
+    doneLabel: 'Done',
+    clearLabel: 'Clear',
+    useDefaultHighlight: true,
+};
