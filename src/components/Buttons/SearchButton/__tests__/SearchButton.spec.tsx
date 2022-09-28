8b6b7ca4af1a1c5b71b4de0208fa21808fabbb45
André Nogueira <nogueira@textkernel.nl>
2022-09-28 09:40:47 +0100

    test: remove not needed type prop since it is optional

--- src/components/Buttons/SearchButton/__tests__/SearchButton.spec.tsx
+++ src/components/Buttons/SearchButton/__tests__/SearchButton.spec.tsx
@@ -5,19 +5,19 @@ import { SearchButton } from '../SearchButton';
 
 describe('<SearchButton> that renders a search button', () => {
     it('should render default button correctly', () => {
-        const wrapper = mount(<SearchButton type="button" />);
+        const wrapper = mount(<SearchButton />);
         expect(toJson(wrapper)).toMatchSnapshot();
         expect(wrapper.find('button')).toHaveLength(1);
     });
     it('should call click callback correctly', () => {
         const onClickMock = jest.fn();
-        const wrapper = mount(<SearchButton type="button" onClick={onClickMock} />);
+        const wrapper = mount(<SearchButton onClick={onClickMock} />);
         wrapper.find('button').simulate('click');
         expect(onClickMock).toHaveBeenCalled();
     });
     it('should render a button with a label correctly', () => {
         const label = 'Search';
-        const wrapper = mount(<SearchButton type="button">{label}</SearchButton>);
+        const wrapper = mount(<SearchButton>{label}</SearchButton>);
         expect(toJson(wrapper)).toMatchSnapshot();
         expect(wrapper.find('button').text()).toEqual(label);
     });
