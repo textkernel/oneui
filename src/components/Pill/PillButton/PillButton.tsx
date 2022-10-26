047a4c6fb60273b76afdc20460f24cb6c3d3cf5b
Pavel Kirvel <kirvel@textkernel.nl>
2022-10-26 10:21:01 +0300

    refactor: migrate PopupBase, Pill, PillButton, PillDropdown to TS

---
+++ src/components/Pill/PillButton/PillButton.tsx
@@ -0,0 +1,120 @@
+import * as React from 'react';
+import { IoIosArrowDown, IoMdClose } from 'react-icons/io';
+import { bem } from '../../../utils';
+import { ENTER_KEY } from '../../../constants';
+import styles from './PillButton.scss';
+
+export interface Props {
+    /** Wether the dropdown is open or closed */
+    isOpen?: boolean;
+    /** If pill is in default state, meaning it has content but cannot be reset. */
+    isContentDefault?: boolean;
+    /** a function to be called when dropdown should be toggled */
+    toggleDropdown: () => void;
+    /** a function to be called to clear the pill/filter content */
+    onClear: () => void;
+    /** name describing the pill/filter */
+    name: string;
+    /** label describing the content of an active filter/pill */
+    content?: string | null; // PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+}
+
+const { block, elem } = bem('PillButton', styles);
+
+export const PillButton = React.forwardRef<HTMLElement, Props>(
+    (
+        {
+            isOpen = false,
+            isContentDefault = false,
+            toggleDropdown,
+            onClear,
+            name,
+            content = null,
+            ...rest
+        },
+        ref
+    ) => {
+        const isActive = !!content;
+        const propsForBem = { isOpen, isActive };
+
+        const labelRef = React.createRef();
+        const pillRef = React.createRef();
+        const [pillMinWidth, setPillMinWidth] = React.useState(0);
+
+        // make sure label is not longer then pill
+        React.useLayoutEffect(() => {
+            if (isActive) {
+                const labelEl = labelRef.current as HTMLElement;
+                const { width: labelWidth } = labelEl.getBoundingClientRect();
+                const pillEl = pillRef.current as HTMLElement;
+                const { width: pillWidth } = pillEl.getBoundingClientRect();
+
+                if (labelWidth > pillWidth) {
+                    setPillMinWidth(labelWidth);
+                }
+            }
+        }, [isActive, labelRef, pillMinWidth, pillRef]);
+
+        let buttonIcon = <IoIosArrowDown {...elem('arrowIcon', propsForBem)} />;
+        let isButtonClickable = false;
+
+        if (isActive && !isOpen && !isContentDefault) {
+            buttonIcon = <IoMdClose />;
+            isButtonClickable = true;
+        }
+
+        const buttonClick = isButtonClickable
+            ? (e) => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  onClear();
+              }
+            : undefined;
+
+        const handleKeyDownOnButton = isButtonClickable
+            ? (e) => {
+                  if (e.key === ENTER_KEY) {
+                      if (buttonClick) {
+                          buttonClick(e);
+                      }
+                  }
+              }
+            : undefined;
+
+        const handleKeyDownOnPill = (e) => {
+            if (e.key === ENTER_KEY) {
+                e.preventDefault();
+                toggleDropdown();
+            }
+        };
+
+        return (
+            <div ref={ref} {...rest} {...block({ ...propsForBem, ...rest })}>
+                <div ref={labelRef} {...elem('label', propsForBem)}>
+                    {isActive && name}
+                </div>
+                <div
+                    ref={pillRef}
+                    {...elem('pill', propsForBem)}
+                    style={pillMinWidth ? { minWidth: pillMinWidth } : undefined}
+                    onClick={toggleDropdown}
+                    onKeyDown={handleKeyDownOnPill}
+                    tabIndex="0"
+                    role="button"
+                >
+                    <span {...elem('pillLabel', propsForBem)}>{content || name}</span>
+                    <button
+                        type="button"
+                        {...elem('button', propsForBem)}
+                        onClick={buttonClick}
+                        onKeyDown={handleKeyDownOnButton}
+                    >
+                        {buttonIcon}
+                    </button>
+                </div>
+            </div>
+        );
+    }
+);
+
+PillButton.displayName = 'PillButton';
---
+++ src/components/PopupBase/PopupBase.tsx
@@ -0,0 +1,182 @@
+import * as React from 'react';
+import { ReactElement, useEffect, useLayoutEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { createPopper, Instance } from '@popperjs/core';
+import { ESCAPE_KEY, PopupPlacement } from '../../constants';
+
+export type PopupBaseRenderer = ({ setPopupVisibility, isOpen }) => ReactElement | null;
+
+export interface Props {
+    /**
+     * Function, that returns an element that triggers popup.
+     * It will be called with a single object as argument that contains:
+     *      * setPopupVisibility {function} - can be called with true/false to show/hide the popup
+     *      * isOpen {boolean} - the current state of the popup
+     * NOTE: The returned element should support refForward, but should not have it set.
+     *     If you need to access the ref, pass the ref with anchorRef prop (see below)
+     */
+    anchorRenderer: PopupBaseRenderer;
+    /**
+     * Function, that returns popup element.
+     * It will be called with a single object as argument that contains:
+     *      * setPopupVisibility {function} - can be called with true/false to show/hide the popup
+     *      * isOpen {boolean} - the current state of the popup
+     * NOTE: The returned element should support refForward, but should not have it set.
+     *     If you need to access the ref, pass the ref with popupRef prop (see below)
+     */
+    popupRenderer: PopupBaseRenderer;
+    /** ref object for anchor */
+    anchorRef?: any;
+    /** ref object for popup */
+    popupRef?: any;
+    /** placement of the popup dialog relative to anchor */
+    placement?: PopupPlacement;
+    /** additional Popper options. See https://popper.js.org/docs/v2/constructors/#options */
+    popperOptions?: object;
+    /** To render the popup in a portal. Useful if the anchor element has overflow hidden and similar cases */
+    renderInPortal?: boolean;
+    /** a function to be called when the popup closes */
+    onClose?: () => void; // ???
+}
+const anchorRefInit = React.createRef();
+const popupRefInit = React.createRef();
+let popper: Instance | undefined;
+
+export const PopupBase: React.FC<Props> = ({
+    anchorRenderer,
+    popupRenderer,
+    anchorRef = anchorRefInit,
+    popupRef = popupRefInit,
+    placement = 'bottom-start',
+    renderInPortal = false,
+    onClose,
+    popperOptions = {},
+}) => {
+    const [isOpen, setIsOpen] = React.useState<boolean>(false);
+
+    const close = () => {
+        if (isOpen) {
+            if (onClose) {
+                onClose();
+            }
+            setIsOpen(false);
+        }
+    };
+
+    const wasPopupClicked = (event) => {
+        if (event.composedPath && anchorRef.current && popupRef.current) {
+            return Array.from(event.composedPath()).some((node) => {
+                // Must be Element node
+                if ((node as Element).nodeType === 1) {
+                    return popupRef.current.contains(node as Element);
+                }
+                return false;
+            });
+        }
+        return false;
+    };
+
+    const wasAnchorClicked = (event) => {
+        if (anchorRef.current) {
+            return anchorRef.current.contains(event.target);
+        }
+        return false;
+    };
+
+    const handleWindowClick = (event) => {
+        if (isOpen) {
+            if (!wasPopupClicked(event) && !wasAnchorClicked(event)) {
+                close();
+            }
+        }
+    };
+
+    const handleKeyPress = (event) => {
+        if (event.key === ESCAPE_KEY) {
+            close();
+        }
+    };
+
+    const destroyPopperInstance = () => {
+        if (popper) {
+            popper.destroy();
+            popper = undefined;
+        }
+    };
+
+    const createPopperInstance = () => {
+        if (anchorRef.current && popupRef.current) {
+            destroyPopperInstance();
+            popper = createPopper(anchorRef.current, popupRef.current, {
+                placement,
+                ...popperOptions,
+            });
+        }
+    };
+
+    useLayoutEffect(() => {
+        if (isOpen) {
+            createPopperInstance();
+        } else {
+            destroyPopperInstance();
+        }
+    });
+
+    useEffect(() => {
+        document.addEventListener('click', handleWindowClick);
+        document.addEventListener('keydown', handleKeyPress, false);
+
+        return () => {
+            document.removeEventListener('click', handleWindowClick);
+            document.removeEventListener('keydown', handleKeyPress, false);
+        };
+    });
+
+    const setPopupVisibility = (shouldBeOpen) => {
+        if (shouldBeOpen !== isOpen) {
+            setIsOpen(shouldBeOpen);
+        }
+    };
+
+    const getArgs = () => {
+        return {
+            setPopupVisibility,
+            isOpen,
+        };
+    };
+
+    const renderAnchor = () => {
+        const anchorElem = anchorRenderer(getArgs());
+        // @ts-ignore
+        return React.cloneElement(anchorElem, { ref: anchorRef });
+    };
+
+    const renderPopup = () => {
+        if (isOpen) {
+            const popupElem = popupRenderer(getArgs());
+            if (!popupElem) {
+                return null;
+            }
+
+            const popupElemWithProps = React.cloneElement(popupElem, {
+                ref: popupRef,
+                'data-popup': 'true',
+            });
+
+            return renderInPortal
+                ? ReactDOM.createPortal(popupElemWithProps, document.body)
+                : popupElemWithProps;
+        }
+
+        return null;
+    };
+
+    return (
+        <>
+            {renderAnchor()}
+            {renderPopup()}
+        </>
+    );
+};
+
+PopupBase.displayName = 'PopupBase';
