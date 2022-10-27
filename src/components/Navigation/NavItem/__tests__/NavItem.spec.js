4a3ffba905945e34ad82aa46bedc12f7f4597703
Eszter Hofmann <hofmann@textkernel.nl>
2022-10-27 11:02:58 +0200

    feat(NavItem): support react-router v6

--- src/components/Navigation/NavItem/__tests__/NavItem.spec.js
+++ src/components/Navigation/NavItem/__tests__/NavItem.spec.js
@@ -13,6 +13,27 @@ describe('NavItem that renders a single navigation item', () => {
         expect(wrapper.find('a').props().className).toEqual('NavItem');
         expect(toJson(wrapper)).toMatchSnapshot();
     });
+    it('should throw when more then 1 child is added', () => {
+        // suppress expected console errors
+        jest.spyOn(console, 'error').mockImplementation((err) => {
+            if (
+                err.includes('NavItem should have a single child only') ||
+                err.includes('The above error occurred in the')
+            ) {
+                return;
+            }
+            console.error(err);
+        });
+
+        expect(() =>
+            mount(
+                <NavItem>
+                    <a href="/">item</a>
+                    <a href="/">extra</a>
+                </NavItem>
+            )
+        ).toThrow();
+    });
     it('should add active class', () => {
         const wrapper = mount(
             <NavItem active>
@@ -30,29 +51,36 @@ describe('NavItem that renders a single navigation item', () => {
         );
         expect(wrapper.find('a').props().className).toEqual('NavItem NavItem--pullRight');
     });
-    it('should add activeClassName class', () => {
-        const wrapper = mount(
-            <BrowserRouter>
-                <NavItem useActiveClass>
-                    <NavLink to="/else">item</NavLink>
-                </NavItem>
-            </BrowserRouter>
-        );
-        expect(wrapper.find('a').props().className).toEqual('NavItem');
-        expect(wrapper.find('NavLink').props().activeClassName).toEqual('NavItem__active');
-    });
-    it('should throw when more then 1 child is added', () => {
-        // suppress expected console errors
-        jest.spyOn(console, 'error').mockImplementationOnce(() => undefined);
-        jest.spyOn(console, 'error').mockImplementationOnce(() => undefined);
+    describe('when supporting React Router NavLink', () => {
+        it('should add activeClassName class for react-router version 5', () => {
+            // suppress error related to the test actually using version 6
+            jest.spyOn(console, 'error').mockImplementation((err) => {
+                if (err.includes('Warning: React does not recognize the')) {
+                    return;
+                }
+                console.error(err);
+            });
 
-        expect(() =>
-            mount(
-                <NavItem>
-                    <a href="/">item</a>
-                    <a href="/">extra</a>
-                </NavItem>
-            )
-        ).toThrow();
+            const wrapper = mount(
+                <BrowserRouter>
+                    <NavItem useActiveClass>
+                        <NavLink to="/else">item</NavLink>
+                    </NavItem>
+                </BrowserRouter>
+            );
+            expect(wrapper.find('a').props().className).toEqual('NavItem');
+            expect(wrapper.find('NavLink').props().activeClassName).toEqual('NavItem__active');
+        });
+        it('should make className a function for react-router version 6', () => {
+            const wrapper = mount(
+                <BrowserRouter>
+                    <NavItem useActiveClass routerVersion={6}>
+                        <NavLink to="/else">item</NavLink>
+                    </NavItem>
+                </BrowserRouter>
+            );
+            expect(wrapper.find('a').props().className).toEqual('NavItem');
+            expect(typeof wrapper.find('NavLink').props().className).toBe('function');
+        });
     });
 });
