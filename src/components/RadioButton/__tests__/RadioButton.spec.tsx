4cc0c065a54ad1d4a63ce999b58a0f2a6ec8b901
Eszter Hofmann <hofmann@textkernel.nl>
2022-12-22 16:27:15 +0100

    fix(RadioButton): wrongly styled when disabled

--- src/components/RadioButton/__tests__/RadioButton.spec.tsx
+++ src/components/RadioButton/__tests__/RadioButton.spec.tsx
@@ -31,6 +31,7 @@ describe('<RadioButton> that renders a radio button', () => {
                 Useless radio button
             </RadioButton>
         );
+        expect(toJson(wrapper)).toMatchSnapshot();
         expect(wrapper.find('input[disabled]')).toHaveLength(1);
         expect(wrapper.find('.Text--context_muted')).toHaveLength(1);
     });
