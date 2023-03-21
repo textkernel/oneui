6f5d6058a240a583a0d04fa197f9780c287c3904
Pavel Kirvel <kirvel@textkernel.nl>
2023-03-21 09:04:03 +0300

    feat: changes after code review

--- scripts/measure-enzyme-to-rtl-tests-ratio/measure-enzyme-to-rtl-tests-ratio.js
+++ scripts/measure-enzyme-to-rtl-tests-ratio/measure-enzyme-to-rtl-tests-ratio.js
@@ -1,82 +1,77 @@
-const { spawn } = require('child_process');
+const { execSync } = require('child_process');
 
-const API_URL = 'https://sheetdb.io/api/v1/g3uyrf24ruo1v';
-const ENZYME_COLUMN = 'enzyme';
-const RTL_COLUMN = 'rtl';
-const ID_PATTERN = 'id-';
-const NOT_VALID_ID_PATTERN = 'id';
+const API_URL = 'https://sheetdb.io/api/v1';
 
-async function getColumnData(columnName) {
-    const columnData = await fetch(API_URL)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            return data;
-        })
-        .catch((error) => {
-            console.error('SheetDB data fetch error: ', error);
-        });
+async function insertRow(url, insertData) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            data: {
+                insertData,
+            },
+        }),
+    });
 
-    for (let i = 0; i < columnData.length; i += 1) {
-        const currentColumnData = columnData[i];
-        if (columnName === ENZYME_COLUMN && currentColumnData.enzyme.includes(ID_PATTERN)) {
-            return currentColumnData.enzyme;
-        }
-        if (columnName === RTL_COLUMN && currentColumnData.rtl.includes(ID_PATTERN)) {
-            return currentColumnData.rtl;
-        }
+    if (response.status < 200 || response.status >= 300) {
+        const message = await response.text();
+        throw new Error(`Unable to insert a row. Request failed: ${message}`);
     }
 
-    return NOT_VALID_ID_PATTERN;
+    return response.json();
 }
 
-async function insertCellValue(testsCount, columnName) {
-    const cellId = await getColumnData(columnName);
-    if (cellId !== NOT_VALID_ID_PATTERN) {
-        const url = `${API_URL}/${columnName}/${cellId}`;
-        const valueCellInset = `${testsCount}`;
-        const insertData =
-            columnName === ENZYME_COLUMN
-                ? {
-                      enzyme: valueCellInset,
-                  }
-                : {
-                      rtl: valueCellInset,
-                  };
-        await fetch(url, {
-            method: 'PATCH',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                data: {
-                    insertData,
-                },
-            }),
-        });
-    }
+async function saveMetricsToSpreadsheet({ total, rtl }) {
+    const url = `${API_URL}/${process.env.SHEET_DB_TOKEN}`;
+    const date = new Date();
+    const day = date.getUTCDate();
+    const month = date.getUTCMonth() + 1;
+    const year = date.getUTCFullYear();
+    await insertRow(url, {
+        date: `${day}/${month}/${year}`,
+        enzyme: total - rtl,
+        rtl,
+        progress: `=${rtl}/${total}`,
+    });
 }
 
 async function execute() {
-    const enzymeCommand =
-        'find ./src -mindepth 1 -type f \\( -iname \\*.spec.js -o -iname \\*.spec.jsx  -iname \\*.spec.ts -iname \\*.spec.tsx \\) | wc -l\n';
-    const enzymeTests = spawn(enzymeCommand, {
-        shell: true,
-    });
-    enzymeTests.stdout.on('data', (data) => {
-        insertCellValue(data, ENZYME_COLUMN);
-    });
+    const findAllTestFilesCommand =
+        'find ./src -mindepth 1 -type f \\( -iname \\*.spec.js -o -iname \\*.spec.jsx -o -iname \\*.spec.ts -o -iname \\*.spec.tsx \\) | sort --unique';
 
-    const rtlCommand =
-        'find ./src -mindepth 1 -type f \\( -iname "*.spec.js" -o -iname "*.spec.jsx" -o -iname "*.spec.ts" -iname "*.spec.tsx" \\) | xargs grep -m 1 "from \'@testing-library/react\'" | wc -l\n';
-    const rtlTests = spawn(rtlCommand, {
-        shell: true,
-    });
-    rtlTests.stdout.on('data', (data) => {
-        insertCellValue(data, RTL_COLUMN);
-    });
+    const totalTests = execSync(findAllTestFilesCommand);
+    const rtlTests = execSync(
+        `${findAllTestFilesCommand} | xargs grep -E "from '@testing-library/react'|from 'react-test-renderer'" | sort --unique`
+    );
+
+    const totalMetric = totalTests
+        .toString('utf-8')
+        .split('\n')
+        .filter((item) => item.startsWith('./src/')).length;
+    const rtlMetric = rtlTests
+        .toString('utf-8')
+        .split('\n')
+        .filter((item) => item.startsWith('./src/')).length;
+
+    console.log(
+        `Metrics were extracted successfully. enzymeMetric = ${totalMetric}; rtlMetric = ${rtlMetric}`
+    );
+
+    try {
+        await saveMetricsToSpreadsheet({
+            total: totalMetric,
+            rtl: rtlMetric,
+        });
+        console.log('Metrics were saved successfully');
+    } catch (error) {
+        console.error(`Failed while saving the metrics: ${error.message}`);
+        process.exit(1);
+    }
 }
+
+(async () => execute())();
 
 execute();
