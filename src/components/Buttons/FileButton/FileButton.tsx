85b4fb5c5f19f54729ea2327318638a3a382849a
ahalyha <103436400+ahalyha@users.noreply.github.com>
2023-04-11 11:07:38 +0200

    fix: remove context (#936)
    * fix: remove context
    
    * fix: address review points

--- src/components/Buttons/FileButton/FileButton.tsx
+++ src/components/Buttons/FileButton/FileButton.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import { bem } from '../../../utils';
 import styles from './FileButton.scss';
-import { Context, Size } from '../../../constants';
+import { Size } from '../../../constants';
 import { NotEmptySingleReactNode } from '../../../customTypes/types';
 
 export interface Props extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
     /** The label of the button */
     children: NotEmptySingleReactNode;
-    /** The button context (e.g. info, danger, success etc. - defaults to neutral) */
-    context?: Context | 'link';
     /** The size of the button */
     size?: Size;
     /** Whether or not to show block-level button (full width) */
@@ -23,24 +21,12 @@ const { block } = bem('Button', styles);
 
 export const FileButton = React.forwardRef<HTMLInputElement, Props>(
     (
-        {
-            children,
-            context = 'neutral',
-            disabled = false,
-            isBlock = false,
-            isInline = false,
-            size = 'normal',
-            ...rest
-        },
+        { children, disabled = false, isBlock = false, isInline = false, size = 'normal', ...rest },
         ref
     ) => {
         return (
             // eslint-disable-next-line jsx-a11y/label-has-associated-control
-            <label
-                ref={ref}
-                {...block({ context, size, isBlock, isInline, ...rest })}
-                disabled={disabled}
-            >
+            <label ref={ref} {...block({ size, isBlock, isInline, ...rest })} disabled={disabled}>
                 <input {...rest} hidden type="file" />
                 {children}
             </label>
