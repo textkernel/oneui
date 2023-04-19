643c36dfa98e92f6ddf91d7140a9c9a7a476c30a
Eszter Hofmann <hofmann@textkernel.nl>
2023-04-19 17:10:56 +0200

    Migrate Search button

--- src/components/Buttons/SearchButton/__tests__/SearchButton.spec.tsx
+++ src/components/Buttons/SearchButton/__tests__/SearchButton.spec.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import toJson from 'enzyme-to-json';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { SearchButton } from '../SearchButton';
 
 describe('<SearchButton> that renders a search button', () => {
     it('should render default button correctly', () => {
-        const wrapper = mount(<SearchButton />);
-        expect(toJson(wrapper)).toMatchSnapshot();
-        expect(wrapper.find('button')).toHaveLength(1);
+        const view = render(<SearchButton />);
+        expect(view.asFragment()).toMatchSnapshot();
+        expect(screen.getByRole('button')).toBeInTheDocument();
     });
-    it('should call click callback correctly', () => {
+    it('should call click callback correctly', async () => {
         const onClickMock = jest.fn();
-        const wrapper = mount(<SearchButton onClick={onClickMock} />);
-        wrapper.find('button').simulate('click');
+        const user = userEvent.setup();
+        render(<SearchButton onClick={onClickMock} />);
+        await user.click(screen.getByRole('button'));
         expect(onClickMock).toHaveBeenCalled();
     });
     it('should render a button with a label correctly', () => {
         const label = 'Search';
-        const wrapper = mount(<SearchButton>{label}</SearchButton>);
-        expect(toJson(wrapper)).toMatchSnapshot();
-        expect(wrapper.find('button').text()).toEqual(label);
+        const view = render(<SearchButton>{label}</SearchButton>);
+        expect(view.asFragment()).toMatchSnapshot();
+        expect(screen.getByText(label)).toBeInTheDocument();
     });
 });
