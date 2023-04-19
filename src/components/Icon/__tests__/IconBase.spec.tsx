ece1208d9de8bd256d8b6e63da5c038117ddcac3
Pavel Kirvel <kirvel@textkernel.nl>
2023-04-19 15:59:25 +0300

    feat: migrated Icon tests to TypeScript and RTL

--- src/components/Icon/__tests__/IconBase.spec.tsx
+++ src/components/Icon/__tests__/IconBase.spec.tsx
@@ -1,52 +1,61 @@
 import React from 'react';
-import toJson from 'enzyme-to-json';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { IconBase } from '../IconBase';
 
 describe('<IconBase> that renders an SVG wrapper with all options included', () => {
     it('should render a default icon', () => {
-        const wrapper = shallow(
+        const wrapper = render(
             <IconBase viewBox="0 0 100 100" title="Icon base" size={15} margin="right" isPrimary>
                 <circle cx="50" cy="50" r="50" />
             </IconBase>
         );
-        expect(toJson(wrapper)).toMatchSnapshot();
+
+        expect(wrapper.asFragment()).toMatchSnapshot();
     });
     it('should correct negative sizes', () => {
-        const wrapper = shallow(
+        const wrapper = render(
             <IconBase viewBox="0 0 100 100" size={-1}>
                 <circle cx="50" cy="50" r="50" />
             </IconBase>
         );
-        expect(wrapper.find('svg')).not.toContain('title');
-        expect(wrapper.find('svg').prop('style').height).toBe(0);
-        expect(wrapper.find('svg').prop('style').width).toBe(0);
+        expect(wrapper.asFragment()).toMatchSnapshot();
+        const img = screen.getByRole('img');
+
+        expect(img).toBeInTheDocument();
+        expect(img).not.toHaveAttribute('title');
+        expect(img).toHaveAttribute('style', 'width: 0px; height: 0px;');
     });
     it('should not apply proportional styles if no size provided', () => {
-        const wrapper = shallow(
+        const wrapper = render(
             <IconBase viewBox="0 0 100 100">
                 <circle cx="50" cy="50" r="50" />
             </IconBase>
         );
-        expect(wrapper.find('svg').prop('style').width).toBe('1em');
+        expect(wrapper.asFragment()).toMatchSnapshot();
+
+        expect(screen.getByRole('img')).toHaveAttribute('style', 'width: 1em;');
     });
     it('should apply proportional styles if size is provided', () => {
         const mockSize = 48;
-        const wrapper = shallow(
+        const wrapper = render(
             <IconBase viewBox="0 0 100 100" size={mockSize}>
                 <circle cx="50" cy="50" r="50" />
             </IconBase>
         );
-        expect(wrapper.find('svg').prop('style').height).toBe(mockSize);
-        expect(wrapper.find('svg').prop('style').width).toBe(mockSize);
+        expect(wrapper.asFragment()).toMatchSnapshot();
+
+        expect(screen.getByRole('img')).toHaveAttribute('style', 'width: 48px; height: 48px;');
     });
     it('should apply hight only to styles if size is provided with preserveAspectRatio', () => {
         const mockSize = 48;
-        const wrapper = shallow(
+        const wrapper = render(
             <IconBase viewBox="0 0 100 100" size={mockSize} preserveAspectRatio>
                 <circle cx="50" cy="50" r="50" />
             </IconBase>
         );
-        expect(wrapper.find('svg').prop('style').height).toBe(mockSize);
-        expect(wrapper.find('svg').prop('style').width).toBe('auto');
+        expect(wrapper.asFragment()).toMatchSnapshot();
+
+        expect(screen.getByRole('img')).toHaveAttribute('style', 'width: auto; height: 48px;');
     });
 });
