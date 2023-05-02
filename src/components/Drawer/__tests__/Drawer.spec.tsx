ad87dbe18e93343e46d4513b34cad4ae3f5be82d
Pavel Kirvel <kirvel@textkernel.nl>
2023-05-02 11:37:48 +0300

    test: migrate Drawer tests, mock

--- src/components/Drawer/__tests__/Drawer.spec.tsx
+++ src/components/Drawer/__tests__/Drawer.spec.tsx
@@ -1,108 +1,105 @@
 import React from 'react';
-import toJson from 'enzyme-to-json';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 import { Drawer } from '../Drawer';
 
 jest.mock('react-transition-group', () => {
     const FakeTransition = jest.fn(({ children }) => children({ state: 'entered' }));
     return { Transition: FakeTransition };
 });
+const onClickMock = jest.fn();
 
 describe('Drawer', () => {
     it('should render correctly', () => {
-        const wrapper = mount(<Drawer title="some title">some text</Drawer>);
-        expect(toJson(wrapper)).toMatchSnapshot();
+        const view = render(<Drawer title="some title">some text</Drawer>);
+
+        expect(view.container).toMatchSnapshot();
+        expect(screen.getByRole('heading', { name: 'some title' })).toBeInTheDocument();
     });
 
     it('should pass initial expand status', () => {
-        const wrapper = mount(
-            <Drawer initialIsExpanded title="some title">
+        render(
+            <Drawer onClick={onClickMock} initialIsExpanded title="some title">
                 some text
             </Drawer>
         );
+        const expandButton = screen.getByRole('button', { name: '' });
 
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(true);
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
-        expect(
-            wrapper.find('.Drawer__expandButton').hasClass('Drawer__expandButton--isExpanded')
-        ).toBe(true);
+        expect(expandButton).toBeInTheDocument();
+        expect(onClickMock).not.toHaveBeenCalled();
     });
 
     it('should pass initial close status', () => {
-        const wrapper = mount(<Drawer title="some title">some text</Drawer>);
-
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(true);
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(false);
-        expect(
-            wrapper.find('.Drawer__expandButton').hasClass('Drawer__expandButton--isExpanded')
-        ).toBe(false);
-    });
-
-    it('should expand and close correctly', () => {
-        const wrapper = mount(<Drawer title="some title">some text</Drawer>);
-        // Expand Drawer
-        wrapper.find('.Drawer__expandButton').simulate('click');
-
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(true);
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
-        expect(
-            wrapper.find('.Drawer__expandButton').hasClass('Drawer__expandButton--isExpanded')
-        ).toBe(true);
-
-        // Close Drawer
-        wrapper.find('.Drawer__expandButton').simulate('click');
-
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(true);
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(false);
-        expect(
-            wrapper.find('.Drawer__expandButton').hasClass('Drawer__expandButton--isExpanded')
-        ).toBe(false);
-    });
-
-    it('should be hidden when isShown is false', () => {
-        const wrapper = mount(
-            <Drawer isShown={false} title="some title">
-                some text
-            </Drawer>
-        );
-
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(false);
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
-    });
-
-    it('should be still hidden when drawer expanted and isShown is false', () => {
-        const wrapper = mount(
-            <Drawer isShown={false} isExpanded title="some title">
-                some text
-            </Drawer>
-        );
-
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(false);
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
-    });
-
-    it('should be expand then isExpanded is true', () => {
-        const wrapper = mount(
-            <Drawer isExpanded title="some title">
-                some text
-            </Drawer>
-        );
-
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(true);
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
-    });
-
-    it('should fire callback function correctly on click expand/close button', () => {
-        const onClickMock = jest.fn();
-        const wrapper = mount(
+        render(
             <Drawer onClick={onClickMock} title="some title">
                 some text
             </Drawer>
         );
 
-        wrapper.find('.Drawer__expandButton').simulate('click');
+        expect(onClickMock).not.toHaveBeenCalled();
+    });
 
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(false);
-        expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(true);
-        expect(onClickMock).toHaveBeenCalled();
+    it('should expand and close correctly', async () => {
+        const user = userEvent.setup();
+        render(
+            <Drawer onClick={onClickMock} title="some title">
+                some text
+            </Drawer>
+        );
+        const expandButton = screen.getByRole('button', { name: '' });
+        // Expand Drawer
+        await user.click(expandButton);
+
+        // Close Drawer
+        await user.click(expandButton);
+
+        expect(onClickMock).toHaveBeenCalledTimes(2);
+    });
+
+    it('should be hidden when isShown is false', () => {
+        render(
+            <Drawer isShown={false} title="some title">
+                some text
+            </Drawer>
+        );
+
+        // expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(false);
+        // expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
+    });
+
+    it('should be still hidden when drawer expanded and isShown is false', () => {
+        render(
+            <Drawer isShown={false} isExpanded title="some title">
+                some text
+            </Drawer>
+        );
+
+        // expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(false);
+        // expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
+    });
+
+    it('should be expand then isExpanded is true', () => {
+        render(
+            <Drawer isExpanded title="some title">
+                some text
+            </Drawer>
+        );
+
+        // expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndExpanded')).toBe(true);
+        // expect(wrapper.find('.Drawer').hasClass('Drawer--isShownAndClosed')).toBe(false);
+    });
+
+    it('should fire callback function correctly on click expand/close button', async () => {
+        const user = userEvent.setup();
+        render(
+            <Drawer onClick={onClickMock} title="some title">
+                some text
+            </Drawer>
+        );
+        const expandButton = screen.getByRole('button', { name: '' });
+        await user.click(expandButton);
+
+        expect(onClickMock).toHaveBeenCalledTimes(1);
     });
 });
