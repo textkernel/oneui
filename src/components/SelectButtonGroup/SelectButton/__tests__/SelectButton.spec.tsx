d9a2b1798d020e114346b2053ea6eb35ef0e15ec
Pavel Kirvel <kirvel@textkernel.nl>
2023-05-11 19:01:04 +0300

    test: migrate SelectButton, SelectButtonGroup tests

---
+++ src/components/SelectButtonGroup/SelectButton/__tests__/SelectButton.spec.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { SelectButton } from '../SelectButton';
+
+describe('SelectButton', () => {
+    let view;
+    const onChangeMock = jest.fn();
+
+    beforeEach(() => {
+        view = render(
+            <SelectButton
+                value="button 1"
+                onChange={onChangeMock}
+                size="large"
+                isEqualWidth
+                isSelected
+            >
+                Option 1
+            </SelectButton>
+        );
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('should render nothing if no children are provided', () => {
+        view.rerender(
+            <SelectButton
+                value="button 1"
+                onChange={onChangeMock}
+                size="large"
+                isEqualWidth
+                isSelected
+            />
+        );
+
+        expect(view.container).toMatchSnapshot();
+        expect(screen.queryByRole('button')).not.toBeInTheDocument();
+    });
+
+    it('should render children when it is 0', () => {
+        view.rerender(
+            <SelectButton
+                value="button 1"
+                onChange={onChangeMock}
+                size="large"
+                isEqualWidth
+                isSelected
+            >
+                {0}
+            </SelectButton>
+        );
+
+        expect(view.continaer).toMatchSnapshot();
+        expect(screen.getByRole('button')).toBeInTheDocument();
+    });
+
+    it('should render correctly', () => {
+        expect(view.container).toMatchSnapshot();
+    });
+
+    it('should call onChange when clicked', async () => {
+        const user = userEvent.setup();
+        await user.click(screen.getByRole('button'));
+
+        expect(onChangeMock).toHaveBeenCalledWith('button 1');
+    });
+
+    it.skip('should call onChange when Enter key was pressed on it', async () => {
+        const user = userEvent.setup();
+        await user.keyboard('[ENTER]');
+
+        expect(onChangeMock).toHaveBeenCalled();
+    });
+
+    it('should not call onChange when other key was pressed on it', async () => {
+        const user = userEvent.setup();
+        await user.keyboard('[b]');
+
+        expect(onChangeMock).not.toHaveBeenCalled();
+    });
+});
---
+++ src/components/SelectButtonGroup/SelectButtonGroup/__tests__/SelectButtonGroup.spec.tsx
@@ -0,0 +1,240 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { SelectButtonGroup } from '../SelectButtonGroup';
+import { SelectButton } from '../../SelectButton';
+
+describe('SelectButtonGroup', () => {
+    let view;
+    let getButton;
+    const onChangeMock = jest.fn();
+    const defaultProps = {
+        defaultValue: ['1'],
+        onChange: onChangeMock,
+    };
+    const rerenderView = (props) => {
+        view.rerender(
+            <SelectButtonGroup {...defaultProps} {...props}>
+                <SelectButton value="1" key="1">
+                    Option 1
+                </SelectButton>
+                <SelectButton value="2" key="2">
+                    Option 2
+                </SelectButton>
+                <SelectButton value="3" key="3">
+                    Option 3
+                </SelectButton>
+            </SelectButtonGroup>
+        );
+    };
+
+    beforeEach(() => {
+        view = render(
+            <SelectButtonGroup {...defaultProps}>
+                <SelectButton value="1" key="1">
+                    Option 1
+                </SelectButton>
+                <SelectButton value="2" key="2">
+                    Option 2
+                </SelectButton>
+                <SelectButton value="3" key="3">
+                    Option 3
+                </SelectButton>
+            </SelectButtonGroup>
+        );
+
+        getButton = (number) => screen.getAllByRole('button')[number];
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('should render correctly', () => {
+        expect(view.container).toMatchSnapshot();
+    });
+
+    describe('Single select mode', () => {
+        it('should not change isSelect prop of children on initial render', () => {
+            expect(getButton(0)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).not.toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+        });
+
+        it('should switch the selected option if a not-selected option is clicked', async () => {
+            const user = userEvent.setup();
+            await user.click(getButton(1));
+
+            expect(getButton(0)).not.toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+        });
+
+        it('should allow empty selection if field is not required', async () => {
+            const user = userEvent.setup();
+            await user.click(getButton(0));
+
+            expect(getButton(0)).not.toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).not.toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+        });
+
+        it('should not allow empty selection if field is required', async () => {
+            const newProps = {
+                isRequired: true,
+            };
+            rerenderView(newProps);
+            const user = userEvent.setup();
+            await user.click(getButton(0));
+
+            expect(getButton(0)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).not.toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+        });
+
+        it('should call onChange with correct parameters', async () => {
+            const user = userEvent.setup();
+            await user.click(getButton(1));
+
+            expect(onChangeMock).toHaveBeenCalledWith(['2']);
+
+            await user.click(getButton(0));
+
+            expect(onChangeMock).toHaveBeenLastCalledWith(['1']);
+
+            await user.click(getButton(0));
+
+            expect(onChangeMock).toHaveBeenLastCalledWith([]);
+        });
+    });
+
+    describe('Multi select mode', () => {
+        beforeEach(() => {
+            const newProps = {
+                isMultiselect: true,
+            };
+            rerenderView(newProps);
+        });
+
+        it('should not change isSelect prop of children on initial render', () => {
+            expect(getButton(0)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).not.toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+        });
+
+        it('should switch to selected if a not-selected option is clicked, without changing others', async () => {
+            const user = userEvent.setup();
+            await user.click(getButton(1));
+
+            expect(getButton(0)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+
+            await user.click(getButton(2));
+
+            expect(getButton(0)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).toHaveClass('SelectButton SelectButton--isSelected');
+        });
+
+        it('should switch to not-selected if a selected option is clicked, without changing others', async () => {
+            const user = userEvent.setup();
+            await user.click(getButton(1));
+
+            expect(getButton(0)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+
+            await user.click(getButton(1));
+
+            expect(getButton(0)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).not.toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+
+            await user.click(getButton(1));
+            await user.click(getButton(0));
+
+            expect(getButton(0)).not.toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+        });
+
+        it.skip('should not allow deletion of last value if field is required', async () => {
+            const newProps = {
+                isRequired: true,
+            };
+            rerenderView(newProps);
+            const user = userEvent.setup();
+            await user.click(getButton(0));
+
+            expect(getButton(0)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).not.toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+
+            await user.click(getButton(1));
+
+            expect(getButton(0)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+
+            await user.click(getButton(0));
+
+            expect(getButton(0)).not.toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+
+            await user.click(getButton(1));
+
+            expect(getButton(0)).not.toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(1)).toHaveClass('SelectButton SelectButton--isSelected');
+            expect(getButton(2)).not.toHaveClass('SelectButton SelectButton--isSelected');
+        });
+
+        it('should call onChange with correct parameters', async () => {
+            const user = userEvent.setup();
+            await user.click(getButton(1));
+            expect(onChangeMock).toHaveBeenLastCalledWith(['1', '2']);
+            expect(onChangeMock).toBeCalled();
+
+            await user.click(getButton(2));
+
+            expect(onChangeMock).toBeCalled();
+            expect(onChangeMock).toHaveBeenLastCalledWith(['1', '2', '3']);
+
+            await user.click(getButton(1));
+
+            expect(onChangeMock).toBeCalled();
+            expect(onChangeMock).toHaveBeenLastCalledWith(['1', '3']);
+
+            await user.click(getButton(0));
+            await user.click(getButton(2));
+
+            expect(onChangeMock).toHaveBeenLastCalledWith([]);
+        });
+    });
+
+    describe('Controlled behavior', () => {
+        beforeEach(() => {
+            const newProps = {
+                defaultValue: undefined,
+                value: ['1'],
+            };
+            rerenderView(newProps);
+        });
+
+        it('should render correctly', () => {
+            expect(view.container).toMatchSnapshot();
+        });
+
+        it('should call onChange with correct parameters', async () => {
+            const user = userEvent.setup();
+
+            expect(getButton(0)).toHaveClass('SelectButton SelectButton--isSelected');
+
+            await user.click(getButton(1));
+
+            expect(onChangeMock).toHaveBeenCalled();
+        });
+    });
+});
