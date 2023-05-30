7db4fcdef264407a7c97f629bea94c30fec088e9
Pavel Kirvel <kirvel@textkernel.nl>
2023-05-30 08:25:50 +0300

    test: code refactoring

--- src/components/Sliders/Slider/__tests__/Slider.spec.tsx
+++ src/components/Sliders/Slider/__tests__/Slider.spec.tsx
@@ -1,22 +1,23 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-// import { Slider } from '../Slider';
+import { Slider } from '../Slider';
 
 describe('Slider component', () => {
-    // const defaultAriaMax = '100';
-    // const defaultAriaMin = '0';
-    // const defaultAriaDisabled = 'false';
-    // const defaultAriaNow = '0';
+    const defaultAriaMax = '100';
+    const defaultAriaMin = '0';
+    const defaultAriaDisabled = 'false';
+    const defaultAriaNow = '0';
 
     it('should render component correctly', () => {
-        // const view = render(<Slider />);
-        // const slider = screen.getByRole('slider');
-        // expect(view.container).toMatchSnapshot();
-        // expect(slider).toBeInTheDocument();
-        // expect(slider).toHaveAttribute('aria-valuemax', defaultAriaMax);
-        // expect(slider).toHaveAttribute('aria-valuemin', defaultAriaMin);
-        // expect(slider).toHaveAttribute('aria-disabled', defaultAriaDisabled);
-        // expect(slider).toHaveAttribute('aria-valuenow', defaultAriaNow);
+        const view = render(<Slider />);
+        const slider = screen.getByRole('slider');
+
+        expect(view.container).toMatchSnapshot();
+        expect(slider).toBeInTheDocument();
+        expect(slider).toHaveAttribute('aria-valuemax', defaultAriaMax);
+        expect(slider).toHaveAttribute('aria-valuemin', defaultAriaMin);
+        expect(slider).toHaveAttribute('aria-disabled', defaultAriaDisabled);
+        expect(slider).toHaveAttribute('aria-valuenow', defaultAriaNow);
     });
 });
