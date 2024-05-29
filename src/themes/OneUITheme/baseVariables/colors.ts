4cc798de3d2deea0b6ab42c61d738ca11c2c57ff
Eszter Hofmann <hofmann@textkernel.nl>
2024-05-29 09:47:44 +0200

    remove bright and muted colors

--- src/themes/OneUITheme/baseVariables/colors.ts
+++ src/themes/OneUITheme/baseVariables/colors.ts
@@ -13,30 +13,6 @@ export const colorVariables = {
     '--color-success': '#5cb85c',
     '--color-warning': '#ffb819',
     '--color-danger': '#f42534',
-
-    '--color-bright-1': '#142459',
-    '--color-bright-2': '#507D9B',
-    '--color-bright-3': '#F6C85F',
-    '--color-bright-4': '#6F4D7C',
-    '--color-bright-5': '#9CD766',
-    '--color-bright-6': '#CA472F',
-    '--color-bright-7': '#820401',
-    '--color-bright-8': '#EE9A3A',
-    '--color-bright-9': '#E7E34E',
-    '--color-bright-10': '#D9DADD',
-
-    '--color-muted-1': '#AFF5E2',
-    '--color-muted-2': '#D6F481',
-    '--color-muted-3': '#FFF16B',
-    '--color-muted-4': '#FFD571',
-    '--color-muted-5': '#FFC4A6',
-    '--color-muted-6': '#E5DAC9',
-    '--color-muted-7': '#ECCBFF',
-    '--color-muted-8': '#FFC5EB',
-    '--color-muted-9': '#FCC1CF',
-    '--color-muted-10': '#C4D7FC',
-    '--color-muted-11': '#ADDDFF',
-    '--color-muted-12': '#75DFFB',
 };
 
 export const computedColorVariables = (baseVariables: CssVars): CssVars => {
