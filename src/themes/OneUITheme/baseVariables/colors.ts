95b87dbf57eea1bf2f89c18a64d0267812c8b3d6
Eszter Hofmann <hofmann@textkernel.nl>
2024-05-29 12:31:19 +0200

    add AI and Accent colors

--- src/themes/OneUITheme/baseVariables/colors.ts
+++ src/themes/OneUITheme/baseVariables/colors.ts
@@ -13,6 +13,26 @@ export const colorVariables = {
     '--color-success': '#5cb85c',
     '--color-warning': '#ffb819',
     '--color-danger': '#f42534',
+
+    '--color-accent-10': '#FEE8D5',
+    '--color-accent-20': '#FCD1AA',
+    '--color-accent-30': '#FBBB80',
+    '--color-accent-40': '#F9A455',
+    '--color-accent-50': '#F88D2B',
+    '--color-accent-60': '#C67122',
+    '--color-accent-70': '#95551A',
+    '--color-accent-80': '#633811',
+    '--color-accent-90': '#321C09',
+
+    '--color-ai-10': '#EEEEFB',
+    '--color-ai-20': '#CDCCF3',
+    '--color-ai-30': '#ACABEB',
+    '--color-ai-40': '#8A89E2',
+    '--color-ai-50': '#5856D6',
+    '--color-ai-60': '#4645AB',
+    '--color-ai-70': '#353480',
+    '--color-ai-80': '#232256',
+    '--color-ai-90': '#12112B',
 };
 
 export const computedColorVariables = (baseVariables: CssVars): CssVars => {
