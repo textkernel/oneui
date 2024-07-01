3b83c40021c0c0a5be0b86790d23b059b65f775e
Eszter Hofmann <hofmann@textkernel.nl>
2024-07-01 12:12:25 +0200

    fix story and spacing

--- stories/molecules/ExpandableText.stories.tsx
+++ stories/molecules/ExpandableText.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { ExpandableText } from '@textkernel/oneui';
+import { ExpandableText, Text } from '@textkernel/oneui';
 
 const meta: Meta<typeof ExpandableText> = {
     title: 'Molecules/ExpandableText',
@@ -19,20 +19,24 @@ export const ExpandableLongText: Story = {
     },
     render: (args) => (
         <ExpandableText {...args}>
-            03/05 - 11/2010 Magellan Health Services/Icore Healthcare Division , Clinical Sales
-            Representative * Managed outside sales of New York&apos;s tri-state territory for
-            Injectable Medications. * Consulted Hospital Staff; Endocrinologists, and
-            Gastroenterologists on injectable Growth Hormone, and Hepatitis C Medications. * Worked
-            together with Oxford, HIP, and Horizon Blue Cross Blue Shield of N.Schlage. on targeting
-            the formulary * spending on injectable medications.Maintained relationships with pharma
-            <br />
-            <br />
-            03/05 - 11/2010 Magellan Health Services/Icore Healthcare Division , Clinical Sales
-            Representative * Managed outside sales of New York&apos;s tri-state territory for
-            Injectable Medications. * Consulted Hospital Staff; Endocrinologists, and
-            Gastroenterologists on injectable Growth Hormone, and Hepatitis C Medications. * Worked
-            together with Oxford, HIP, and Horizon Blue Cross Blue Shield of N.Schlage. on targeting
-            the formulary * spending on injectable medications.Maintained relationships with pharma
+            <Text>
+                03/05 - 11/2010 Magellan Health Services/Icore Healthcare Division , Clinical Sales
+                Representative * Managed outside sales of New York&apos;s tri-state territory for
+                Injectable Medications. * Consulted Hospital Staff; Endocrinologists, and
+                Gastroenterologists on injectable Growth Hormone, and Hepatitis C Medications. *
+                Worked together with Oxford, HIP, and Horizon Blue Cross Blue Shield of N.Schlage.
+                on targeting the formulary * spending on injectable medications.Maintained
+                relationships with pharma
+            </Text>
+            <Text>
+                03/05 - 11/2010 Magellan Health Services/Icore Healthcare Division , Clinical Sales
+                Representative * Managed outside sales of New York&apos;s tri-state territory for
+                Injectable Medications. * Consulted Hospital Staff; Endocrinologists, and
+                Gastroenterologists on injectable Growth Hormone, and Hepatitis C Medications. *
+                Worked together with Oxford, HIP, and Horizon Blue Cross Blue Shield of N.Schlage.
+                on targeting the formulary * spending on injectable medications.Maintained
+                relationships with pharma
+            </Text>
         </ExpandableText>
     ),
 };
@@ -43,5 +47,9 @@ export const ExpandableShortText: Story = {
         showMoreButtonLabel: 'Show more',
         showLessButtonLabel: 'Collapse',
     },
-    render: (args) => <ExpandableText {...args}>Some short text</ExpandableText>,
+    render: (args) => (
+        <ExpandableText {...args}>
+            Some short text - here we shouldn&apos;t show the toggle button
+        </ExpandableText>
+    ),
 };
