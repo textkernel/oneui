1263f9d15ddb04994987fd1e1b932376deffd00e
Eszter <hofmann@textkernel.nl>
2024-07-30 12:09:07 +0200

    feat(RadioButton): migrate to new design (#1217)

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -2,7 +2,6 @@ import React, { forwardRef } from 'react';
 import { bem } from '../../utils';
 import { Text } from '../Text';
 import styles from './RadioButton.scss';
-import { CHECKBOX_VIEWBOX } from '../../constants';
 
 export interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
     /** A unique id to reference this radio button */
@@ -30,11 +29,7 @@ export const RadioButton = forwardRef<HTMLInputElement, Props>(
                 disabled={disabled}
             />
             <label {...elem('label')} htmlFor={id}>
-                <span {...elem('box')}>
-                    <svg {...elem('svg')} width="12px" height="10px" viewBox={CHECKBOX_VIEWBOX}>
-                        <circle cx="5" cy="6" r="3" />
-                    </svg>
-                </span>
+                <span {...elem('box')} />
                 {!!children && (
                     <Text {...elem('text')} inline context={disabled ? 'muted' : 'default'}>
                         {children}
--- src/constants/style-related.ts
+++ src/constants/style-related.ts
@@ -4,7 +4,6 @@ export const CONTEXTS = ['info', ...VALIDATION_CONTEXTS] as const;
 
 export const SIZES = ['small', 'normal', 'large'] as const;
 export const HEADING_SIZES = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] as const;
-export const CHECKBOX_VIEWBOX = [0, 0, 10, 10] as const;
 export const COLORS = [
     '#AFF5E2',
     '#D6F481',
