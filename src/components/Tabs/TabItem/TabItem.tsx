ac37e2fdf495b97efe9ae777c332eceaa135e113
André Nogueira <nogueira@textkernel.nl>
2024-08-02 16:07:16 +0100

    refactor(TabsBar): TabsBar

--- src/components/Tabs/TabItem/TabItem.tsx
+++ src/components/Tabs/TabItem/TabItem.tsx
@@ -45,7 +45,7 @@ export function TabItem<T = string | number>({
         <div
             tabIndex={0}
             {...rest}
-            {...block({ isActive, isBlock, ...rest })}
+            {...block({ isActive, isBlock, disabled, ...rest })}
             role="tab"
             aria-selected={isActive}
             onClick={handleClick}
