3749df340d31bbfa6b221d43216ee55f186a953d
Eszter <hofmann@textkernel.nl>
2024-09-10 11:37:40 +0200

    feat(Link): implement new design (#1249)
    BREAKING CHANGE: `context` and `doNotDecorateOnHover` props are no longer supported for Link component

--- src/__mocks__/svgMock.js
+++ src/__mocks__/svgMock.js
@@ -1,4 +1,4 @@
 /* eslint-disable react/display-name, react/destructuring-assignment */
 import React from 'react';
 
-module.exports = (props) => <svg data-testid={props['data-testid'] || 'default-icon'} />;
+module.exports = (props) => <svg {...props} data-testid={props['data-testid'] || 'default-icon'} />;
--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,26 +1,34 @@
 import * as React from 'react';
+import OpenInNew from '@material-design-icons/svg/round/open_in_new.svg';
 import { bem } from '../../utils';
 import styles from './Link.scss';
 
-export interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
-    /** Link content */
-    children?: React.ReactNode;
-    /** Color context */
-    context?: 'brand' | 'neutral';
-    /** Do not underline text on hover */
-    dontDecorateOnHover?: boolean;
+export interface ExternalLinkProps {
+    /** Indicate external links - an icon will be added */
+    isExternal: true;
+    /** aria label for the external link icon */
+    externalLinkIconLabel: string;
 }
 
-const { block } = bem('Link', styles);
+export interface BaseProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+    /** Link content */
+    children?: React.ReactNode;
+}
+
+export type Props = BaseProps &
+    (ExternalLinkProps | { isExternal?: false; externalLinkIconLabel?: never });
+
+const { block, elem } = bem('Link', styles);
 
 export const Link = React.forwardRef<HTMLAnchorElement, Props>(
-    ({ children, context = 'brand', dontDecorateOnHover = false, ...rest }, ref) => {
+    ({ children, isExternal = false, externalLinkIconLabel, ...rest }, ref) => {
         if (typeof children !== 'number' && !children) {
             return null;
         }
 
         return (
-            <a ref={ref} {...rest} {...block({ context, dontDecorateOnHover, ...rest })}>
+            <a ref={ref} {...rest} {...block({ ...rest })}>
+                {isExternal && <OpenInNew {...elem('icon')} aria-label={externalLinkIconLabel} />}
                 {children}
             </a>
         );
