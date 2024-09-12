685ee4b1dee303b34cd3cb402e55c84b697797f5
Eszter Hofmann <hofmann@textkernel.nl>
2024-09-12 12:23:39 +0200

    fix story file

--- stories/atoms/SelectAllSuggestions.stories.tsx
+++ stories/atoms/SelectAllSuggestions.stories.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { SelectAllSuggestions } from '@textkernel/oneui/components/SelectAllSuggestions';
+import { SelectAllSuggestions } from '@textkernel/oneui';
 
 export default {
     title: 'Atoms/SelectAllSuggestions',
     component: SelectAllSuggestions,
 } as Meta<typeof SelectAllSuggestions>;
 
-type StorySuggestionItem = StoryObj<typeof SelectAllSuggestions>;
+type StorySelectAllSuggestions = StoryObj<typeof SelectAllSuggestions>;
 
-export const _SelectAllSuggestions: StorySuggestionItem = {
+export const _SelectAllSuggestions: StorySelectAllSuggestions = {
     name: 'SelectAllSuggestions',
     args: {
         disabled: false,
         isSelected: false,
-        onClick: () => console.log('Select all has been clicked'),
+        onChange: () => console.log('Select all has been clicked'),
     },
     render: (args) => (
         <ul
