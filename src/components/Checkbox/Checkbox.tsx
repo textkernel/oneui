7abef4257dcb47a05d89052a0097ce4836828be8
Anastasiia Mishchenko <mishchenko@textkernel.nl>
2024-09-16 13:48:46 +0100

    refactor: remove al changes from checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -59,10 +59,10 @@ export const Checkbox = React.forwardRef<HTMLDivElement, Props>(
         return (
             <div ref={ref} style={style} {...block({ className, asFlexbox, disabled, ...rest })}>
                 <input
-                    id={id}
                     {...rest}
                     {...elem('input', { asFlexbox, indeterminate })}
                     type="checkbox"
+                    id={id}
                     disabled={disabled}
                     checked={checked}
                     ref={inputRef}
