d40b4b7c2b4dcbdc04037167d6c9f5d8a23ae954
janseke10 <janneke.vanhulten@home.nl>
2024-10-02 18:38:50 +0200

    feat(Separator): implement feedback

--- src/components/List/ListItem/Variants/Separator/Separator.tsx
+++ src/components/List/ListItem/Variants/Separator/Separator.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import classnames from './Separators.scss';
-import { bem } from '../../../utils/bem';
+import { bem } from '../../../../../utils/bem';
 
 export interface Props<> {
     /** title of the section: optional */
@@ -11,12 +11,7 @@ const titleBlock = bem('SectionTitle', classnames);
 const noTitleBlock = bem('Divider', classnames);
 
 const Separator = React.forwardRef<HTMLLIElement, Props>(({ title }, ref) => {
-    let block;
-    if (title) {
-        block = titleBlock.block;
-    } else {
-        block = noTitleBlock.block;
-    }
+    const block = title ? titleBlock.block : noTitleBlock.block;
     return (
         <li ref={ref} {...block()}>
             {title}
--- src/components/List/index.ts
+++ src/components/List/index.ts
@@ -2,4 +2,3 @@ export { List, Props as ListProps } from './List';
 export * from './ListItem';
 export { ListActions, ListActionsProps } from './ListActions';
 export { ListOptimizer, ListOptimizerProps } from './ListOptimizer';
-export { Separator } from './Separator';
