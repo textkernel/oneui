69819495fe7aaa8fa03d567219aa69520b139e7e
AlsasaAmmar <zarosho@gmail.com>
2024-10-02 11:56:01 +0200

    feat(tooltip.stories): fix the order of the imports, remove an item from .gitignore

--- stories/molecules/Tooltip.stories.tsx
+++ stories/molecules/Tooltip.stories.tsx
@@ -1,6 +1,6 @@
+import * as React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Tooltip } from '@textkernel/oneui';
-import * as React from 'react';
 
 const meta: Meta<typeof Tooltip> = {
     title: 'Molecules/Tooltip',
