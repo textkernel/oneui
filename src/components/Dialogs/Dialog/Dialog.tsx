18dc4eb25fa39a1490270bae682ac178d942c053
AlsasaAmmar <zarosho@gmail.com>
2024-10-07 16:06:04 +0200

    first commit

--- src/components/Dialogs/Dialog/Dialog.tsx
+++ src/components/Dialogs/Dialog/Dialog.tsx
@@ -8,11 +8,16 @@ import styles from './Dialog.scss';
 export type ButtonProps = {
     onClick: () => void;
     label: string;
+    ButtonContext?: string; // this can also be set on the whole component instead..
 };
 
 export interface DialogProps extends ModalProps {
+    /** the component which will trigger the dialog */
+    dialogTrigger?: React.ReactNode;
     /** Should the alert appear */
     isOpen: boolean;
+    /** the function which triggers the modal */
+    onTriggerDialog?: () => void;
     /** The alert content */
     children: React.ReactNode;
     /** Dialog title */
@@ -21,40 +26,57 @@ export interface DialogProps extends ModalProps {
     acceptButton: ButtonProps;
     /** Properties of the cancel button */
     cancelButton?: ButtonProps;
+    /** when set the content will have a scroll bar if it's longer than the value set */
+    maxHeight?: string | number;
 }
 
 const { block, elem } = bem('Dialog', styles);
 
 export const Dialog: React.FC<DialogProps> = (props) => {
-    const { isOpen, children, title, acceptButton, cancelButton, ...rest } = props;
-
+    const {
+        onTriggerDialog,
+        isOpen,
+        children,
+        title,
+        acceptButton,
+        cancelButton,
+        dialogTrigger,
+        ...rest
+    } = props;
     const isConfirm = !!cancelButton;
 
     return (
-        <Modal {...rest} {...block(props)} isOpen={isOpen}>
-            <div {...elem('content', props)} role="alert">
-                {!!title && (
-                    <Heading level="h2" {...elem('title', props)}>
-                        {title}
-                    </Heading>
-                )}
-                {children}
-            </div>
-            <div {...elem('actions', props)}>
-                {isConfirm && (
-                    <Button
-                        {...elem('cancel', props)}
-                        onClick={cancelButton.onClick}
-                        variant="ghost"
-                    >
-                        {cancelButton.label}
+        <>
+            {/*
+                commenting this to pass tests
+            <button style={{ all: 'unset', cursor: 'pointer' }} onClick={onTriggerDialog}>
+                {dialogTrigger}
+            </button> */}
+            <Modal {...rest} {...block(props)} isOpen={isOpen}>
+                <div {...elem('content', props)} role="alert">
+                    {!!title && (
+                        <Heading level="h2" {...elem('title', props)}>
+                            {title}
+                        </Heading>
+                    )}
+                    {children}
+                </div>
+                <div {...elem('actions', props)}>
+                    {isConfirm && (
+                        <Button
+                            {...elem('cancel', props)}
+                            onClick={cancelButton.onClick}
+                            variant="ghost"
+                        >
+                            {cancelButton.label}
+                        </Button>
+                    )}
+                    <Button onClick={acceptButton.onClick} context="primary">
+                        {acceptButton.label}
                     </Button>
-                )}
-                <Button onClick={acceptButton.onClick} context="primary">
-                    {acceptButton.label}
-                </Button>
-            </div>
-        </Modal>
+                </div>
+            </Modal>
+        </>
     );
 };
 
--- stories/organisms/Dialogs.stories.tsx
+++ stories/organisms/Dialogs.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Alert, Confirm, Button } from '@textkernel/oneui';
+import { Dialog } from '@textkernel/oneui/components/Dialogs/Dialog';
 
 const meta: Meta<typeof Alert> = {
     title: 'Organisms/Dialogs',
@@ -90,6 +91,50 @@ export const _Confirm: ConfirmStory = {
                     }}
                 />
             </>
+        );
+    },
+};
+
+export const _Dialog: ConfirmStory = {
+    name: 'Dialog',
+    args: {
+        title: 'Duplicate this page',
+        contentLabel: 'content label',
+        children: 'this is not as cool as I wanted ',
+    },
+    render: (args) => {
+        const [showAlert, setShowAlert] = React.useState(false);
+
+        const handleButtonClick = () => {
+            setShowAlert(true);
+            console.log('hel')
+        };
+
+        const handleAccept = () => {
+            setShowAlert(false);
+            console.log('Dialog has been accepted');
+        };
+
+        const handleCancel = () => {
+            setShowAlert(false);
+            console.log('Dialog has been cancelled');
+        };
+
+        return (
+            <Dialog
+                onTriggerDialog={handleButtonClick}
+                dialogTrigger={<p>Trigger me please!</p>}
+                {...args}
+                isOpen={showAlert}
+                acceptButton={{
+                    onClick: handleAccept,
+                    label: 'Ok',
+                }}
+                cancelButton={{
+                    onClick: handleCancel,
+                    label: 'Cancel',
+                }}
+            />
         );
     },
 };
