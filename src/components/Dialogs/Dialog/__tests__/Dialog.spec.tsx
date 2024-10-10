e85105e091291f601053a3c6547b33908a7844fe
AlsasaAmmar <zarosho@gmail.com>
2024-10-10 14:39:35 +0200

    made a new branch

---
+++ src/components/Dialogs/Dialog/__tests__/Dialog.spec.tsx
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+
+describe('Dialog', () => {
+    it.todo('should render correctly');
+    it.todo('should show a scrollbar when the content is too long');
+    it.todo('should render with a title ');
+    it.todo('should call onClose when x is clicked');
+});
