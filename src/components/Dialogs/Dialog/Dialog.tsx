8d6f3a1ca9d20c557e398bcdf851f4a23a8293ba
André Nogueira <nogueira@textkernel.nl>
2024-11-20 16:08:55 +0000

    fix: Dialog position and shadows

--- src/components/Dialogs/Dialog/Dialog.tsx
+++ src/components/Dialogs/Dialog/Dialog.tsx
@@ -4,9 +4,9 @@ import useResizeObserver from 'use-resize-observer';
 import { bem } from '../../../utils';
 import { Button } from '../../Buttons';
 import { Modal, ModalProps } from '../../Modal';
-import styles from './Dialog.scss';
 import { Heading } from '../../Heading';
 import { ButtonContext } from '../../../constants/component-specific';
+import styles from './Dialog.scss';
 
 export type ButtonProps = {
     onClick: () => void;
@@ -48,34 +48,36 @@ export const Dialog: React.FC<DialogProps> = (props) => {
 
     return (
         <Modal {...rest} {...block(props)} isOpen={isOpen}>
-            {!!title && (
-                <div {...elem('header')} role="alert">
-                    <Heading level="h3" {...elem('title', props)}>
-                        {title}
-                    </Heading>
-                    <button {...elem('closeButton')} type="button" onClick={onClose}>
-                        <MdClose />
-                    </button>
+            <div {...elem('container')}>
+                {!!title && (
+                    <div {...elem('header')} role="alert">
+                        <Heading level="h3" {...elem('title', props)}>
+                            {title}
+                        </Heading>
+                        <button {...elem('closeButton')} type="button" onClick={onClose}>
+                            <MdClose />
+                        </button>
+                    </div>
+                )}
+                <div ref={ref} {...elem('content')}>
+                    {children}
                 </div>
-            )}
-            <div ref={ref} {...elem('content')}>
-                {children}
-            </div>
-            {showButtons && (
-                <div {...elem('actions', { ...props, borderTop: height === 412 })}>
-                    <Button
-                        onClick={acceptButton.onClick}
-                        context={variant === 'primary' ? 'primary' : 'critical'}
-                    >
-                        {acceptButton.label}
-                    </Button>
-                    {!!cancelButton && (
-                        <Button onClick={cancelButton.onClick} variant="ghost">
-                            {cancelButton.label}
+                {showButtons && (
+                    <div {...elem('actions', { ...props, borderTop: height === 412 })}>
+                        <Button
+                            onClick={acceptButton.onClick}
+                            context={variant === 'primary' ? 'primary' : 'critical'}
+                        >
+                            {acceptButton.label}
                         </Button>
-                    )}
-                </div>
-            )}
+                        {!!cancelButton && (
+                            <Button onClick={cancelButton.onClick} variant="ghost">
+                                {cancelButton.label}
+                            </Button>
+                        )}
+                    </div>
+                )}
+            </div>
         </Modal>
     );
 };
