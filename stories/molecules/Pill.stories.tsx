3f4a661ab480db42638ba9e431c524677a3c5706
André Nogueira <nogueira@textkernel.nl>
2024-11-21 16:46:02 +0000

    fix: max height scroll

--- stories/molecules/Pill.stories.tsx
+++ stories/molecules/Pill.stories.tsx
@@ -12,10 +12,10 @@ import {
 
 const DummyComponent = () => (
     <>
-        <p>
+        <span>
             This is some content dropdown has 440px max height so it will show scroll bars after
             that
-        </p>
+        </span>
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas
             vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum
@@ -27,6 +27,19 @@ const DummyComponent = () => (
             sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias
             error harum maxime adipisci amet laborum.
         </p>
+        <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas
+            vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum
+            quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident
+            similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+            molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit
+            sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid.
+            Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias
+            error harum maxime adipisci amet laborum. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Id maiores, quaerat optio ullam eligendi dicta quia ipsam hic
+            exercitationem? Ex exercitationem, ad earum est quod architecto facere debitis quia et!
+        </span>
     </>
 );
 
@@ -79,7 +92,7 @@ export const _Pill: Story = {
         };
 
         return (
-            <div style={{ position: 'relative', display: 'flex', gap: '4px' }}>
+            <div style={{ display: 'flex', alignItems: 'center', gap: 16 }}>
                 <Pill
                     {...args}
                     onClear={() => {
@@ -89,6 +102,9 @@ export const _Pill: Story = {
                         console.log('onClose called');
                     }}
                 >
+                    <DummyComponent />
+                </Pill>
+                <Pill {...args} content={undefined}>
                     <DummyComponent />
                 </Pill>
                 <Pill
@@ -127,10 +143,25 @@ export const _Pill: Story = {
                                 </SingleSelectItem>
                             ))}
                         </SelectedItemBadge>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam a fuga
+                            beatae reiciendis earum, omnis eius accusantium eveniet quam
+                            exercitationem praesentium sint odit harum. Quisquam eos mollitia
+                            veritatis eligendi magnam.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam a fuga
+                            beatae reiciendis earum, omnis eius accusantium eveniet quam
+                            exercitationem praesentium sint odit harum. Quisquam eos mollitia
+                            veritatis eligendi magnam.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam a fuga
+                            beatae reiciendis earum, omnis eius accusantium eveniet quam
+                            exercitationem praesentium sint odit harum. Quisquam eos mollitia
+                            veritatis eligendi magnam.
+                        </p>
                     </div>
-                </Pill>
-                <Pill {...args} content={undefined}>
-                    <DummyComponent />
                 </Pill>
             </div>
         );
@@ -233,15 +264,4 @@ export const _PillButtonEnhancedWithPriority: PillButtonEnhancedStory = {
             </div>
         );
     },
-};
-
-type PillDropdownStory = StoryObj<typeof PillDropdown>;
-
-export const _PillDropdown: PillDropdownStory = {
-    name: 'PillDropdown',
-    render: (args) => (
-        <PillDropdown {...args}>
-            <DummyComponent />
-        </PillDropdown>
-    ),
 };
